f8eee95ab3c3fcc171e20b22b2a4574353d2df13
MElborolossy <mohammedelborolossy@gmail.com>
2021-08-10 14:09:39 +0200

    Formating

--- taiga/taiga_user.v
+++ taiga/taiga_user.v
@@ -1,23 +1,24 @@
 module taiga
+
 import json
 
 struct User {
 pub mut:
-	id int
-	is_active bool
-	username string
-	full_name string
+	id                int
+	is_active         bool
+	username          string
+	full_name         string
 	full_name_display string
-	bio string
-	photo string
-	roles []string
-	email string
-	public_key string
-	date_joined string
-	client TaigaConnection
+	bio               string
+	photo             string
+	roles             []string
+	email             string
+	public_key        string
+	date_joined       string
+	client            TaigaConnection
 }
 
 fn (mut h TaigaConnection) users() ?[]User {
-	data := h.get_json_str("users","",true)?
-	return json.decode([]User, data)
+	data := h.get_json_str('users', '', true) ?
+	return json.decode([]User, data) or {}
 }
