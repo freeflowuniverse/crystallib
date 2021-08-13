5f795ab0b3bbaafdf7148f789fde832faed687e8
despiegk <kristof@incubaid.com>
2021-08-13 07:50:47 +0200

    time.Time fix

--- planner/model_checklist.v
+++ planner/model_checklist.v
@@ -2,15 +2,13 @@ module planner
 
 pub struct Checklist {
 pub mut:
-	title string
+	title  string
 	checks []ChecklistItem
 }
 
 pub struct ChecklistItem {
 pub mut:
 	description string
-	name string
-	deadline time.time
-	
+	name        string
+	deadline    time.Time
 }
-
