222baf3ad5a117906b42ba597e87b6300ed9410c
despiegk <kristof@incubaid.com>
2021-09-15 10:18:11 +0300

    liquid

---
+++ liquid/liquid_test.v
@@ -0,0 +1,19 @@
+module liquid
+module gittools
+
+import os
+
+fn test_1() {
+
+	// key = "92be9b29-7f6c-48e4-9ef2-d6aa0550f620"
+
+	mut c := new({secret:key})
+
+	//need to fetch USD/TFT price
+	//see how we did for taiga how to use the connection
+
+
+
+	panic("sss")
+
+}
