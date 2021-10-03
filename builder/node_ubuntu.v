d1def5b225ce533cdd0734c5f055b6a8b2a5cd5c
despiegk <kristof@incubaid.com>
2021-10-03 17:29:01 +0400

    ...

---
+++ builder/node_ubuntu.v
@@ -0,0 +1,22 @@
+
+
+module builder
+
+
+// pub struct SwarmArgs{
+// 	reset bool
+// }
+
+// //installs docker & swarm
+// fn (mut node Node) ubuntu_update(args SwarmArgs) ?{
+
+// 	cmd := '
+// 		set -ex
+// 		apt update
+// 		apt upgrade -y
+// 		apt autoremove -y
+// 		'
+
+// 	node.exec({cmd:cmd,cache:3600*12})?
+
+// }
