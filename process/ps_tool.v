960d41fc755a99d3ad150b9da83c36a1f33b2397
despiegk <kristof@incubaid.com>
2021-12-27 13:06:28 +0200

    ...

--- process/ps_tool.v
+++ process/ps_tool.v
@@ -17,7 +17,7 @@ pub mut:
 	state		PMState
 }
 
-
+[heap]
 pub struct ProcessInfo {
 pub mut:
 	cpu_perc	f32
@@ -31,9 +31,9 @@ pub mut:
 
 
 //needed to get singleton
-fn init3() ProcessMap {
+fn init3() &ProcessMap {
 	mut f := process.ProcessMap{}	
-	return f
+	return &f
 }
 
 
@@ -45,7 +45,33 @@ const processmap = init3()
 pub fn processmap_get() ?&ProcessMap {
 	mut pm := process.processmap
 	pm.scan()?
-	return &pm
+	return pm
+}
+
+
+//get process info from 1 specific process
+//returns
+//```
+// pub struct ProcessInfo {
+// pub mut:
+// 	cpu_perc	f32
+// 	mem_perc	f32
+// 	cmd 		string
+// 	pid 		int
+// 	ppid 		int
+// 	//resident memory
+// 	rss			int
+// }
+//```
+pub fn procesinfo_get(pid int) ?&ProcessInfo {
+	mut pm := processmap_get()?
+	for pi in pm.processes{
+		if pi.pid == pid{
+			return pi
+		}
+	}
+	return error("Cannot find process with pid: $pid, to get process info from.")
+
 }
 
 
@@ -59,24 +85,40 @@ pub fn (mut pm ProcessMap) scan()? {
 		if pm.state == PMState.ok { return }
 	}
 
-	// cmd := "ps ax -o pid,ppid,stat,%cpu,%mem,rss,command"
-	// res := execute_silent(cmd) or {
-	// 	return error("Cannot get process info \n$cmd\n$err")
-	// }
-	// println("DID SCAN")
-	// for line in res.split_into_lines() {
-	// 	if ! line.contains('PPID') {
-	// 		mut fields:=line.fields()
-	// 		mut pi := ProcessInfo{}
-	// 		pi.ppid=fields[0].int()
-	// 		// pi.stat=fields[1]
-	// 		pi.cpu_perc=fields[2].f32()
-	// 		pi.mem_perc=fields[3].f32()
-	// 		pi.rss=fields[4].int()
-	// 		fields.delete_many(0,6)			
-	// 		println(fields)
-	// 	}
-	// }
+	cmd := "ps ax -o pid,ppid,stat,%cpu,%mem,rss,command"
+	res := execute_silent(cmd) or {
+		return error("Cannot get process info \n$cmd\n$err")
+	}
 
+	pm.processes = []&ProcessInfo{}
+
+	// println("DID SCAN")
+	for line in res.split_into_lines() {
+		if ! line.contains('PPID') {
+			mut fields:=line.fields()
+			if fields.len<6{
+				// println(res)
+				// println("SSS")
+				// println(line)
+				// panic("ss")
+				continue
+			}
+			mut pi := ProcessInfo{}
+			pi.ppid=fields[0].int()
+			pi.pid=fields[1].int()
+			pi.cpu_perc=fields[3].f32()
+			pi.mem_perc=fields[4].f32()
+			pi.rss=fields[5].int()
+			fields.delete_many(0,6)			
+			pi.cmd = fields.join(" ")
+			// println(pi.cmd)
+			pm.processes << &pi
+		}
+	}
+
+	pm.lastscan = time.now()
+	pm.state = PMState.ok
+
+	// println(pm)
 
 }
