3b12685e3032b2ca73ced357baa43803e359ce40
despiegk <kristof@incubaid.com>
2022-01-21 06:11:44 +0400

    init

--- texttools/clean.v
+++ texttools/clean.v
@@ -6,17 +6,6 @@ import os
 const ignore_for_name="\\/[]()?!@#$%^&*<>:;{}|~"
 const keep_ascii='abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_-+={}[]"\':;?/>.<,|\\~` '
 
-pub fn name_fix(name string) string {
-	pagename := name_fix_keepext(name)
-
-	if pagename.ends_with('.md') {
-		fixed_pagename := pagename[0..pagename.len - 3]
-		return fixed_pagename
-	}
-	res := pagename.clone()
-	return res
-}
-
 pub fn name_clean(r string) string {
 	mut res := []string{}
 	for ch in r {
