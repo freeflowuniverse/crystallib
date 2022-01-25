7541364a7a05179ab7115b5aadede025ac40c1c3
MElborolossy <mohammedelborolossy@gmail.com>
2022-01-25 15:51:46 +0200

    - Move cache methods to taiga_caching.v - Rename models to taiga_models.v

---
+++ taiga/taiga_caching.v
@@ -0,0 +1,78 @@
+module taiga
+
+import crypto.md5
+// calculate the key for the cache starting from data and prefix
+
+fn cache_key(prefix string, postdata string) string {
+	/*
+	Create Cache Key
+	Inputs:
+		prefix: Taiga elements types, ex (projects, issues, tasks, ...).
+
+	Output:
+		cache_key: key that will be used in redis
+	*/
+	mut data2 := postdata
+	if data2.len > 16 {
+		data2 = md5.hexhash(data2)
+	}
+	if data2.len > 0 {
+		return 'taiga:' + prefix + ':' + data2
+	}
+	return 'taiga:' + prefix
+}
+
+fn (mut h TaigaConnection) cache_get(prefix string, postdata string, cache bool) string {
+	/*
+	Get from Cache
+	Inputs:
+		prefix: Taiga elements types, ex (projects, issues, tasks, ...).
+		reqdata: data used in the request.
+		cache: Flag to enable caching.
+	Output:
+		result: If cache ture and no thing stored or cache false will return empty string
+	*/
+	mut text := ''
+	if cache {
+		text = h.redis.get(cache_key(prefix, postdata)) or { '' }
+	}
+	return text
+}
+
+fn (mut h TaigaConnection) cache_set(prefix string, postdata string, data string, cache bool) ? {
+	/*
+	Set Cache
+	Inputs:
+		prefix: Taiga elements types, ex (projects, issues, tasks, ...).
+		reqdata: data used in the request.
+		data: Json encoded data.
+		cache: Flag to enable caching.
+	*/
+	if cache {
+		key := cache_key(prefix, postdata)
+		h.redis.set(key, data) ?
+		h.redis.expire(key, h.cache_timeout) or {
+			panic('should never get here, if redis worked expire should also work.$err')
+		}
+	}
+}
+
+pub fn (mut h TaigaConnection) cache_drop_all() ? {
+	/*
+	Drop all cache related to taiga
+	*/
+	all_keys := h.redis.keys('taiga:*') ?
+	for key in all_keys {
+		h.redis.del(key) ?
+	}
+}
+
+pub fn (mut h TaigaConnection) cache_drop(prefix string) ? {
+	/*
+	Drop specific key cache related to taiga
+	*/
+	all_keys := h.redis.keys('taiga:$prefix*') ?
+	for key in all_keys {
+		h.redis.del(key) ?
+	}
+}
--- taiga/taiga_connect.v
+++ taiga/taiga_connect.v
@@ -5,7 +5,6 @@ import json
 import net.http
 import despiegk.crystallib.redisclient
 import despiegk.crystallib.crystaljson
-import crypto.md5
 
 struct TaigaConnectionSettings {
 	comments_story bool = true
@@ -100,81 +99,6 @@ fn (mut h TaigaConnection) header() http.Header {
 		http.CommonHeader.authorization: 'Bearer $h.auth.auth_token'
 	})
 	return header
-}
-
-// calculate the key for the cache starting from data and prefix
-fn cache_key(prefix string, postdata string) string {
-	/*
-	Create Cache Key
-	Inputs:
-		prefix: Taiga elements types, ex (projects, issues, tasks, ...).
-
-	Output:
-		cache_key: key that will be used in redis
-	*/
-	mut data2 := postdata
-	if data2.len > 16 {
-		data2 = md5.hexhash(data2)
-	}
-	if data2.len > 0 {
-		return 'taiga:' + prefix + ':' + data2
-	}
-	return 'taiga:' + prefix
-}
-
-fn (mut h TaigaConnection) cache_get(prefix string, postdata string, cache bool) string {
-	/*
-	Get from Cache
-	Inputs:
-		prefix: Taiga elements types, ex (projects, issues, tasks, ...).
-		reqdata: data used in the request.
-		cache: Flag to enable caching.
-	Output:
-		result: If cache ture and no thing stored or cache false will return empty string
-	*/
-	mut text := ''
-	if cache {
-		text = h.redis.get(cache_key(prefix, postdata)) or { '' }
-	}
-	return text
-}
-
-fn (mut h TaigaConnection) cache_set(prefix string, postdata string, data string, cache bool) ? {
-	/*
-	Set Cache
-	Inputs:
-		prefix: Taiga elements types, ex (projects, issues, tasks, ...).
-		reqdata: data used in the request.
-		data: Json encoded data.
-		cache: Flag to enable caching.
-	*/
-	if cache {
-		key := cache_key(prefix, postdata)
-		h.redis.set(key, data) ?
-		h.redis.expire(key, h.cache_timeout) or {
-			panic('should never get here, if redis worked expire should also work.$err')
-		}
-	}
-}
-
-pub fn (mut h TaigaConnection) cache_drop_all() ? {
-	/*
-	Drop all cache related to taiga
-	*/
-	all_keys := h.redis.keys('taiga:*') ?
-	for key in all_keys {
-		h.redis.del(key) ?
-	}
-}
-
-pub fn (mut h TaigaConnection) cache_drop(prefix string) ? {
-	/*
-	Drop specific key cache related to taiga
-	*/
-	all_keys := h.redis.keys('taiga:$prefix*') ?
-	for key in all_keys {
-		h.redis.del(key) ?
-	}
 }
 
 // fn (mut h TaigaConnection) post_json_dict_any(_amyprefix string, postdata string, cache bool) ?map[string]json2.Any {
