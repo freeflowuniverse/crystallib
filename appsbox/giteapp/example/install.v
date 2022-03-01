dd2c760e463dbd25612c8a2ee148647ec4309eba
Maxime Daniel <root@maxux.net>
2022-03-01 16:17:13 +0100

    appsbox: add gitea support

---
+++ appsbox/giteapp/example/install.v
@@ -0,0 +1,13 @@
+module main
+
+import freeflowuniverse.crystallib.appsbox.giteapp
+
+fn do() ? {
+	mut app := giteapp.get(name: "maingit")
+	app.start()?
+}
+
+
+fn main() {
+	do() or {panic(err)}
+}
