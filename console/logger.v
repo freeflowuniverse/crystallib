2565f0d5b5c3ddc1219d371c0f525d6306ae2033
MElborolossy <mohammedelborolossy@gmail.com>
2022-03-22 13:14:14 +0200

    Update logger

--- console/logger.v
+++ console/logger.v
@@ -2,26 +2,45 @@ module console
 
 import time
 
-pub fn info(msg string) {
-	println('${time.now()} | ' + 'INFO' + '\t| '+ msg)
+pub enum LogLevel {
+	error
+	warning
+	info
+	debug
 }
 
-pub fn debug(msg string) {
-	println('${time.now()} | ' + color_fg('DEBUG', 'blue') + '\t| ' + color_fg(msg, 'light_blue'))
+pub struct Logger{
+pub mut:
+	level LogLevel
 }
 
-pub fn success(msg string) {
+fn (log Logger) output(msg string, t LogLevel) {
+	if int(log.level) >= int(t) {
+		println(msg)
+	}
+}
+
+pub fn (log Logger) info(msg string) {
+	log.output('${time.now()} | ' + 'INFO' + '\t| '+ msg, .info)
+}
+
+[if debug]
+pub fn (log Logger) debug(msg string) {
+	log.output('${time.now()} | ' + color_fg('DEBUG', 'blue') + '\t| ' + color_fg(msg, 'light_blue'), .debug)
+}
+
+pub fn (log Logger) warning(msg string) {
+	log.output('${time.now()} | ' + color_fg('WARNING', 'yellow') + '\t| ' + color_fg(msg, 'light_yellow'), .warning)
+}
+
+pub fn (log Logger) success(msg string) {
 	println('${time.now()} | ' + color_fg('SUCCESS', 'green') + '\t| ' + color_fg(msg, 'light_green'))
 }
 
-pub fn warning(msg string) {
-	println('${time.now()} | ' + color_fg('WARNING', 'yellow') + '\t| ' + color_fg(msg, 'light_yellow'))
-}
-
-pub fn error(msg string) {
+pub fn (log Logger) error(msg string) {
 	eprintln('${time.now()} | ' + style(color_fg('ERROR', 'red'), 'bold') + '\t| ' + color_fg(msg, 'light_red'))
 }
 
-pub fn critical(msg string) {
+pub fn (log Logger) critical(msg string) {
 	eprintln('${time.now()} | ' + style(color_bg('CRITICAL', 'red'),'bold') + '\t| ' + style(color_bg(msg, 'light_red'),'bold'))
 }
