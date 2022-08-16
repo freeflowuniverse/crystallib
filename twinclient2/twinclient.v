946eb634d0f6602439df1e11329db18871480350
Sameh-farouk <samehabouelsaad@gmail.com>
2022-08-16 13:25:09 +0200

    fix typos

--- twinclient2/twinclient.v
+++ twinclient2/twinclient.v
@@ -3,36 +3,35 @@ module twinclient2
 import net.websocket
 import rand
 
-type MessageChannel chan websocket.Message{}
 
 struct TwinClient {
 	pub:
 		ws websocket.Client
 	pub mut:
-		channels  []MessageChannel
+		channels  map[string]chan websocket.Message
 }
 
 pub fn init_client (ws websocket.Client) TwinClient {
-	TwinClient {
+	return TwinClient {
 		ws: ws
-		channels: map[string]MessageChannel{}
+		channels: map[string]chan websocket.Message{}
 	}
 }
 
-fn (tcl GridClient) invoke(functionPath string, args map[string]string) string {  
-       id = rand.uuid_v4()
-       channel = MessageChannel{}
+fn (mut tcl TwinClient) invoke(functionPath string, args map[string]string) string {  
+       id := rand.uuid_v4()
+       channel := chan websocket.Message{}
        tcl.channels[id] = channel
-	   payload = {id:id, fn: "invoke", invokeargs:{functionPath, args}}
-	   // construct the message `msg`
+	   payload := {"id":id, "fn": "invoke", "invokeargs":{functionPath, args}}
+	   // construct the message `msg` from the payload
        tcl.ws.send_message(msg)
-       res =  tcl.wait(id)   
+       res :=  tcl.wait(id)   
        return res
 }
 
-fn (tcl GridClient) wait(id string) string {
-		channel = tcl.channels[id]
-		res <-channel
+fn (mut tcl TwinClient) wait(id string) websocket.Message {
+		channel := tcl.channels[id]
+		res := <-channel
 		channel.close()
 		tcl.channels.delete(id)
 		return res
