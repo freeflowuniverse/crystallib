3f14dbb49ff6605b69902b75f9075530b11ead2b
Mahmoud-Emad <mahmmoud.hassanein@gmail.com>
2022-08-23 15:58:30 +0200

    WIP: Update the code with review condtions

--- twinclient2/tfchain.v
+++ twinclient2/tfchain.v
@@ -13,7 +13,7 @@ pub fn (mut c TfChain) import_(name string, mnemonics string)?{
 	c.client.send(
 		'tfchain.import',
 		json.encode({"name": name, "mnemonics": mnemonics})
-	)?
+	) or { panic(err) }
 }
 
 pub fn (mut c TfChain) get(name string)? TfchainWalletAddressModel{
@@ -21,42 +21,36 @@ pub fn (mut c TfChain) get(name string)? TfchainWalletAddressModel{
 	response := c.client.send(
 		'tfchain.get',
 		json.encode({"name": name})
-	)?
-	return json.decode(
-		TfchainWalletAddressModel,
-		response.data
-	) or {}
+	) or { panic(err) }
+	return json.decode(TfchainWalletAddressModel,response.data)
 }
 
 pub fn (mut c TfChain) update(name string, mnemonics string)?{
 	c.client.send(
 		'tfchain.update',
 		json.encode({"name": name, "mnemonics": mnemonics})
-	)?
+	) or { panic(err) }
 }
 
-pub fn (mut c TfChain) exist(name string)? string{
-	// Returns bool as string.
-	response := c.client.send(
-		'tfchain.exist',
-		json.encode({"name": name})
-	)?
-	return response.data
+pub fn (mut c TfChain) exist(name string)? bool{
+	response := c.client.send('tfchain.exist', json.encode(
+		{"name": name}
+	)) or { panic(err) }
+	return response.data.bool()
 }
 
 pub fn (mut c TfChain) list()? []string {
 	// Returns array of string.
-	response := c.client.send('tfchain.list', "{}")?
+	response := c.client.send('tfchain.list', "{}") or { panic(err) }
 	return json.decode([]string, response.data)
 }
 
 pub fn (mut c TfChain) balance_by_name(name string)? BalanceResult {
 	// Returns BalanceResult{free, feeFrozen, miscFrozen, reserved}
-	options_encoded := json.encode(options)
 	response := c.client.send(
 		'tfchain.balanceByName',
 		json.encode({"name": name})
-	)?
+	) or { panic(err) }
 	return json.decode(BalanceResult, response.data)
 }
 
@@ -64,19 +58,19 @@ pub fn (mut c TfChain) balance_by_address(address string)? BalanceResult {
 	// Returns BalanceResult{free, feeFrozen, miscFrozen, reserved}
 	response := c.client.send(
 		'tfchain.balanceByAddress', json.encode({"address": address})
-	)?
+	) or { panic(err) }
 	return json.decode(BalanceResult, response.data)
 }
 
-pub fn (mut c TfChain) transfer(name string, target_address string, amount f64)? {
+pub fn (mut c TfChain) transfer(name string, target_address string, amount f64)?{
 	c.client.send('tfchain.transfer', json.encode({
 		"name": name,
 		"target_address": target_address,
 		"amount": amount,
 		})
-	)?
+	) or { panic(err) }
 }
 
-pub fn (mut c TfChain) delete(name string)? {
-	c.client.send('tfchain.delete', json.encode({"name": name}))?
+pub fn (mut c TfChain) delete(name string)?{
+	c.client.send('tfchain.delete', json.encode({"name": name})) or { panic(err) }
 }
