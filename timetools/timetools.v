2cc6d07d5eeda2611657ae6b4aba8beda527f26c
Jonathan Ouwerx <jonathanouwerx@me.com>
2022-10-05 09:20:56 +0300

    testing suite and relative time

--- timetools/timetools.v
+++ timetools/timetools.v
@@ -8,10 +8,12 @@ pub mut:
 	expiration i64
 }
 
+//? function purpose?
 fn (mut exp Expiration) epoch() i64 {
 	return exp.expiration
 }
 
+//? function purpose?
 fn (mut exp Expiration) time() time.Time {
 	return time.unix(exp.expiration)
 }
@@ -21,40 +23,70 @@ fn (mut exp Expiration) time() time.Time {
 // 0 means nothing
 // h = hour, s=sec, d=day, m=month
 // TODO: need to support absolute days
+// TODO: do error handling
 pub fn expiration_new(exp_ string) ?Expiration {
-	nnow := time.now().unix_time()
-	mut exp := exp_.trim(' ')
-	if exp == '' || exp.trim(' ') == '0' {
+	relative_time := true // TESTING PLACEHOLDER
+	// TODO: function to determine if relative or absolute time
+	if relative_time == true{
+		// removes all spaces from the string
+		mut full_exp:= exp_.replace(' ', '')
+
+		// If input is empty or contains just a 0
+		if full_exp == '' || full_exp.trim(' ') == '0' {
 		return Expiration{
-			expiration: nnow
+			expiration: time.now().unix_time()
+		}
+		}
+
+		// duplicates the + and - signs
+		full_exp = full_exp.replace('+', '£+')
+		full_exp = full_exp.replace('-', '£-')
+		// create an array of periods
+		mut exps := full_exp.split_any('£')
+		exps = exps.filter(it.len > 0)
+		mut total := 0
+
+		for mut exp in exps {
+			mut mult := 0
+			if exp.ends_with('s') {
+				mult = 1
+			} else if exp.ends_with('m') {
+				mult = 60
+			} else if exp.ends_with('h') {
+				mult = 60*60
+			} else if exp.ends_with('d') {
+				mult = 60 * 60 * 24
+			} else if exp.ends_with('w') {
+				mult = 60 * 60 * 24 * 7	
+			} else if exp.ends_with('M') {
+				mult = 60 * 60 * 24 * 30	
+			} else if exp.ends_with('Q') {
+				mult = 60 * 60 * 24 * 30 * 3	
+			} else if exp.ends_with('Y') {
+				mult = 60 * 60 * 24 * 365	
+			} else {
+				return error('could not parse time suffix for: $exp')
+			}
+			if exp.starts_with('-') {
+				mult *= -1
+			}
+			// remove +/- and period
+			exp = exp[1..(exp.len - 1)]
+			// multiplies the value by the multiplier
+			exp_int := exp.int() * mult
+			total += exp_int
+
+		}
+		expir := total + time.now().unix_time()
+		return Expiration{
+		expiration: expir
 		}
 	}
-	if exp.starts_with('+') {
-		mut mult := 0
-		if exp.ends_with('h') {
-			mult = 60 * 60
-		} else if exp.ends_with('s') {
-			mult = 1
-		} else if exp.ends_with('d') {
-			mult = 60 * 60 * 24
-		} else if exp.ends_with('M') {
-			mult = 60 * 60 * 24 * 30	
-		} else if exp.ends_with('W') {
-			mult = 60 * 60 * 24 * 7	
-		} else if exp.ends_with('Y') {
-			mult = 60 * 60 * 24 * 365	
-		} else {
-			return error('could not parse time:$exp')
-		}
-		// remove + and perdiod
-		exp = exp[1..(exp.len - 2)]
-		exp_int := exp.int() * mult + nnow
-		return Expiration{
-			expiration: exp_int
-		}
-	}
-	return error('could not parse time:$exp')
+	// handling for absolute inputs
+	// else {}
+	return error('could not parse time')
 }
+	
 
 
 //get vlang time object but based on string
@@ -77,3 +109,4 @@ pub fn get(timestr string) ?time.Time{
 	}
 	return time.parse(timestr)
 }
+
--- timetools/timetools_test.v
+++ timetools/timetools_test.v
@@ -1,9 +1,89 @@
 import freeflowuniverse.crystallib.timetools
+import time
 
-fn test_base() {
-	e := timetools.expiration_new('+1h') or { panic('cannot do expiration') }
-
-	println(e)
-
-	// panic("s")
+fn check_input(input_string string, seconds int) {
+	nnow := time.now().unix_time()
+	e := timetools.expiration_new(input_string) or { panic('cannot get expiration for $input_string') }
+	assert e.expiration == (nnow + seconds), 'expiration was incorrect for $input_string'
 }
+
+// check every period
+fn test_every_period () {
+	input_strings := {
+	'+5s': 5
+	'+3m': 180
+	'+2h': 7200
+	'+1d': 86_400
+	'+1w': 604_800
+	'+1M': 2_592_000
+	'+1Q': 7_776_000
+	'+1Y': 31_536_000
+	}
+
+	for key, value in input_strings {
+		check_input(key, value)
+	}
+}
+
+// check multiple periods input
+fn test_combined_periods () {
+
+	input_strings := {
+	'+5s +1h +1d': 90_005
+	'+1h +2s +1Y': 31_539_602
+	}
+
+	for key, value in input_strings {
+		check_input(key, value)
+	}
+
+}
+
+// check negative inputs
+fn test_negative_periods () {
+	input_strings := {
+	'-15s': -15
+	'-5m': -300
+	'-2h': -7200
+	'-1d': -86_400
+	'-1w': -604_800
+	'-1M': -2_592_000
+	'-1Q': -7_776_000
+	'-1Y': -31_536_000
+	}
+
+	for key, value in input_strings {
+		check_input(key, value)
+	}
+}
+
+// check positive and negative combinations
+fn test_combined_signs() {
+	input_strings := {
+	'+1h -10s': 3_590
+	'+1d -2h': 79_200
+	'+1Y -2Q +2M +4h -60s': 21_182_340
+	}
+
+	for key, value in input_strings {
+		check_input(key, value)
+	}
+}
+
+// check varied input styles 
+fn test_input_variations () {
+	input_strings := {
+	'   +1s   ': 1
+	' - 1 s ': -1
+	'+    1s-1h ': -3599
+	'- 1s+   1   h': 3599
+	}
+
+	for key, value in input_strings {
+		check_input(key, value)
+	}
+}
+
+// check that standard formats can be inputted
+
+
