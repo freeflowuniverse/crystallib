c5bc6b2809ae1fc799af4063d85d30a445d6e328
timurgordon <timurgordon@gmail.com>
2022-11-02 12:13:31 +0300

    add actionparser test

---
+++ actionparser/actionparser_parser_test.v
@@ -0,0 +1,24 @@
+module actionparser
+
+import os 
+import freeflowuniverse.crystallib.texttools
+
+const testpath = os.dir(@FILE) + '/testdata'
+
+fn test_parse_into_blocks() {
+	text := "!!git.link
+source:'https://github.com/ourworld-tsc/ourworld_books/tree/development/content/feasibility_study/Capabilities'
+dest:'https://github.com/threefoldfoundation/books/tree/main/books/feasibility_study_internet/src/capabilities'"
+	blocks := parse_into_blocks(text) or { panic('cant parse') }
+	assert blocks.blocks.len == 1
+	assert blocks.blocks[0].name == 'git.link'
+	content_lines := blocks.blocks[0].content.split('\n')
+	assert content_lines[1] == "source:'https://github.com/ourworld-tsc/ourworld_books/tree/development/content/feasibility_study/Capabilities'"
+	assert content_lines[2] == "dest:'https://github.com/threefoldfoundation/books/tree/main/books/feasibility_study_internet/src/capabilities'"
+}
+
+fn test_file_parse() {
+	mut parser := actionparser.get()
+	parser.file_parse('$testpath/testfile.md')?
+	assert parser.actions.len == 10
+}
