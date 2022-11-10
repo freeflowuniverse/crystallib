810f113fb46216dd0fabe7a9519cdfec69bfb16b
Jonathan Ouwerx <jonathanouwerx@me.com>
2022-11-10 14:37:41 +0300

    params_resp en/decode

--- params/params_to_text.v
+++ params/params_to_text.v
@@ -1,8 +1,88 @@
+module params
+
+import freeflowuniverse.crystallib.resp
 
 
+pub fn (mut result Params) str() string {
+	mut string_array := []string
+	string_array << "params:"
+	for param in result.params {
+		string_array << '${param.key}: "${param.value}",'
+	}
+	string_array << "args:"
+	for arg in result.args {
+		string_array << '"${arg.value}",'
+	}
 
-pub fn (mut result Params) str()string {
-	//TODO: walk over params fill in a serialized string
-	//TODO: Jonathan
-	return ""
+	return string_array.join('\n')
+}
+
+
+pub fn (mut p Params) to_resp () []u8 {
+
+	mut param_array := resp.RArray{}
+	for param in p.params {
+		param_array.values << resp.RArray {
+			values: [
+				resp.r_string(param.key),
+				resp.r_string(param.value)
+			]
+		}
+	}
+
+	mut arg_array := resp.RArray{}
+	for arg in p.args {
+		arg_array.values << resp.r_string(arg.value)
+	}
+
+	mut final_array := resp.RArray{
+		values: [
+			param_array,
+			arg_array
+		]
+	}
+
+	return resp.encode([final_array])
+}
+
+pub fn from_resp (bytes []u8) Params {
+	mut params := Params{}
+	mut char_array := bytes.bytestr().replace('\r\n', '').split('')
+
+	char_array = char_array[1..char_array.len]
+	for char_array.first() != '*' {
+		char_array = char_array[1..char_array.len]
+	}
+	char_array = char_array[1..char_array.len]
+
+	mut num_string := ''
+	for i in char_array {
+		if i == '*' || i == '\r' {
+			break
+		} else {
+			num_string = num_string + i
+		}
+	}
+	mut items_left := num_string.int()
+	mut params_array := char_array.join('').split_nth('*', items_left+3)
+	args_string := params_array.pop()
+	params_array = params_array[1..params_array.len]
+	params_array = params_array.map(it[1..it.len])
+	mut args := args_string.split('+')
+	args = args[1..args.len]
+
+	for param_pair in params_array {
+		param := Param{
+		key : param_pair[1..param_pair.len].all_before('+')
+		value : param_pair.all_after_last('+')
+		}
+		params.params << param
+	}
+	for arg in args {
+		params.args << Arg{value: arg}
+	}
+
+	println(params)
+
+	return params
 }
---
+++ playground.v
@@ -0,0 +1,110 @@
+module main
+
+import freeflowuniverse.crystallib.params {Params, Param, Arg}
+import freeflowuniverse.crystallib.resp
+
+fn main () {
+
+	mut input := Params{
+		params: [Param{
+			key: 'id'
+			value: 'a1'
+		}, Param{
+			key: 'name6'
+			value: 'aaaaa'
+		}, Param{
+			key: 'name'
+			value: 'need to do something 1'
+		}, Param{
+			key: 'description'
+			value: '## markdown works in it
+
+	description can be multiline
+	lets see what happens
+
+	- a
+	- something else
+
+	### subtitle
+
+	```python
+	#even code block in the other block, crazy parsing for sure
+	def test():
+	print()
+	```
+	'
+		}, Param{
+			key: 'name2'
+			value: 'test'
+		}, Param{
+			key: 'name3'
+			value: 'hi'
+		}, Param{
+			key: 'name10'
+			value: 'this is with space'
+		}, Param{
+			key: 'name11'
+			value: 'aaa11'
+		}, Param{
+			key: 'name4'
+			value: 'aaa'
+		}, Param{
+			key: 'name5'
+			value: 'aab'
+		}],
+		args: [Arg{
+			value: '111'
+		}, Arg {
+			value: '222'
+		}, Arg {
+			value: '333'
+		}
+		]
+	}
+
+	mut bytes := input.to_resp()
+
+	params.from_resp(bytes)
+}
+
+
+
+
+/*
+expected_result := "
+		id:a1 name6:aaaaa
+		name:'need to do something 1' 
+		description:
+			## markdown works in it
+
+			description can be multiline
+			lets see what happens
+
+			- a
+			- something else
+
+			### subtitle
+
+			```python
+			#even code block in the other block, crazy parsing for sure
+			def test():
+				print()
+			```
+
+		name2:   test
+		name3: hi name10:'this is with space'  name11:aaa11
+
+		#some comment
+
+		name4: 'aaa'
+
+		//somecomment
+		name5:   'aab' 
+
+		args:
+
+		'111'
+		'222'
+		'333'
+	"
+*/
