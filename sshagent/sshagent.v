94b4135b8b1c228c4472b99ed8e00e7b2e0dd42b
timurgordon <timurgordon@gmail.com>
2022-12-10 03:35:22 +0300

    sshagent pubkey find fix

--- sshagent/sshagent.v
+++ sshagent/sshagent.v
@@ -51,8 +51,10 @@ pub fn load_interactive() !string {
 
 	pubkeys = []string{}
 
-	for p in sshdirpath.file_list(tofind: '.pub', recursive: false)! {
-		pubkeys << p.path.replace('.pub', '')
+	for p in sshdirpath.file_list(mut recursive: false)! {
+		if p.path.ends_with('.pub') {
+			pubkeys << p.path.replace('.pub', '')
+		}
 	}
 	// println(keypaths)
 
@@ -120,7 +122,9 @@ pub fn pubkey_guess() !string {
 	// now means nothing in ssh-agent, lets see if we find 1 key in .ssh directory
 	mut sshdirpath := pathlib.get_dir('$os.home_dir()/.ssh', true)!
 
-	mut keypaths := sshdirpath.file_list(tofind: '.pub')!
+	// todo: use ourregex field to nly list .pub files
+	mut sshfiles := sshdirpath.file_list(mut pathlib.ListArgs{})!
+	mut keypaths := sshfiles.filter(it.path.ends_with('.pub'))
 	// println(keypaths)
 
 	if keypaths.len == 1 {
