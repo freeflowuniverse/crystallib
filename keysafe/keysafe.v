2fbe4dfbe33e413885217f45602776048c877b13
Maxime Daniel <root@maxux.net>
2023-01-25 21:35:43 +0100

    keysafe: add code documentation

--- keysafe/keysafe.v
+++ keysafe/keysafe.v
@@ -6,6 +6,27 @@ import libsodium
 import json
 import os
 
+
+/*
+ * KeysSafe
+ *
+ * This module implement a secure keys manager.
+ *
+ * When loading a keysafe object, you can specify a directory and a secret.
+ * In that directory, a file called '.keys' will be created and encrypted using
+ * the 'secret' provided (AES-CBC).
+ * 
+ * Content of that file is a JSON dictionnary of key-name and it's mnemonic,
+ * a single mnemonic is enough to derivate ed25519 and x25519 keys.
+ *
+ * When loaded, private/public signing key and public/private encryption keys
+ * are loaded and ready to be used.
+ *
+ * key_generate_add() generate a new key and store is as specified name
+ * key_import_add() import an existing key based on it's seed and specified name
+ * 
+ */
+
 pub struct KeysSafe {
 pub mut:
 	path    pathlib.Path          // file path of keys
