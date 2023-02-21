d4fb1f77a9cfd6c556bbe22f47170a8aadd82c07
Maxime Daniel <root@maxux.net>
2023-02-22 00:51:00 +0100

    secp256k1: improve v build, fix includes

--- keysafe/secp256k1/secp256k1.h
+++
@@ -1,5 +0,0 @@
-#ifndef SECP256K1_V_MOD
-    #define SECP256K1_V_MOD
-
-
-#endif
--- keysafe/secp256k1/secp256k1mod.c
+++ keysafe/secp256k1/secp256k1mod.c
@@ -1,48 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <assert.h>
 #include <string.h>
 #include <sys/random.h>
-
 #include <stddef.h>
 #include <limits.h>
 #include <stdio.h>
 #include <secp256k1.h>
+#include "secp256k1mod.h"
 
 static int fill_random(unsigned char* data, size_t size) {
 #if defined(__linux__) || defined(__FreeBSD__)
-    /* If `getrandom(2)` is not available you should fallback to /dev/urandom */
     ssize_t res = getrandom(data, size, 0);
-    if (res < 0 || (size_t)res != size ) {
+    if(res < 0 || (size_t) res != size) {
         return 0;
     } else {
         return 1;
     }
+
 #elif defined(__APPLE__) || defined(__OpenBSD__)
-    /* If `getentropy(2)` is not available you should fallback to either
-     * `SecRandomCopyBytes` or /dev/urandom */
     int res = getentropy(data, size);
-    if (res == 0) {
+    if(res == 0) {
         return 1;
     } else {
         return 0;
     }
 #endif
+
     return 0;
 }
 
-int init() {
-    secp256k1_context* ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
+secp256k1_t *secp256k1_new() {
+    secp256k1_t *secp = malloc(sizeof(secp256k1_t));
     unsigned char randomize[32];
 
-    if (!fill_random(randomize, sizeof(randomize))) {
+    secp->kntxt = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
+
+    if(!fill_random(randomize, sizeof(randomize))) {
         printf("Failed to generate randomness\n");
-        return 1;
+        return NULL;
     }
 
     /* Randomizing the context is recommended to protect against side-channel
      * leakage See `secp256k1_context_randomize` in secp256k1.h for more
      * information about it. This should never fail. */
-    int return_val = secp256k1_context_randomize(ctx, randomize);
+    int val = secp256k1_context_randomize(secp->kntxt, randomize);
 
+    return secp;
+}
+
+/*
+int main() {
+    secp256k1_new();
     return 0;
 }
+*/
