d61050e96c8ac9efc0bfe0498a65f5558b39073b
Maxime Daniel <root@maxux.net>
2023-03-11 13:43:08 +0100

    secp256k1: generate shared key with no leaks

--- keysafe/secp256k1/secp256k1mod.c
+++ keysafe/secp256k1/secp256k1mod.c
@@ -7,6 +7,7 @@
 #include <limits.h>
 #include <stdio.h>
 #include <secp256k1.h>
+#include <secp256k1_ecdh.h>
 #include "secp256k1mod.h"
 
 static int fill_random(unsigned char* data, size_t size) {
@@ -30,6 +31,35 @@ static int fill_random(unsigned char* data, size_t size) {
     return 0;
 }
 
+static void dumphex(unsigned char *data, size_t size) {
+    size_t i;
+
+    printf("0x");
+
+    for(i = 0; i < size; i++) {
+        printf("%02x", data[i]);
+    }
+
+    printf("\n");
+}
+
+static void secp256k1_erase(unsigned char *target, size_t length) {
+#if defined(__GNUC__)
+    // memory barrier to avoid memset optimization
+    memset(target, 0, length);
+    __asm__ __volatile__("" : : "r"(target) : "memory");
+#else
+    // if we can't, fill with random, still better than
+    // risking avoid memset
+    fill_random(target, length);
+#endif
+}
+
+static void secp256k1_erase_free(unsigned char *target, size_t length) {
+    secp256k1_erase(target, length);
+    free(target);
+}
+
 secp256k1_t *secp256k1_new() {
     secp256k1_t *secp = malloc(sizeof(secp256k1_t));
     unsigned char randomize[32];
@@ -37,21 +67,89 @@ secp256k1_t *secp256k1_new() {
     secp->kntxt = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
 
     if(!fill_random(randomize, sizeof(randomize))) {
-        printf("Failed to generate randomness\n");
+        printf("[-] failed to generate randomness\n");
         return NULL;
     }
 
-    /* Randomizing the context is recommended to protect against side-channel
-     * leakage See `secp256k1_context_randomize` in secp256k1.h for more
-     * information about it. This should never fail. */
+    // side-channel protection
     int val = secp256k1_context_randomize(secp->kntxt, randomize);
 
     return secp;
 }
 
-/*
+void secp256k1_free(secp256k1_t *secp) {
+    secp256k1_context_destroy(secp->kntxt);
+    secp256k1_erase_free(secp->seckey, SECKEY_SIZE);
+    secp256k1_erase_free(secp->compressed, COMPPUB_SIZE);
+    free(secp);
+}
+
+int secp256k1_generate_key(secp256k1_t *secp) {
+    secp->seckey = malloc(sizeof(char) * SECKEY_SIZE);
+    secp->compressed = malloc(sizeof(char) * COMPPUB_SIZE);
+
+    while(1) {
+        if(!fill_random(secp->seckey, SECKEY_SIZE)) {
+            printf("[-] failed to generate randomness\n");
+            return 1;
+        }
+
+        if(secp256k1_ec_seckey_verify(secp->kntxt, secp->seckey) == 0) {
+            // try again
+            continue;
+        }
+
+        int r = secp256k1_ec_pubkey_create(secp->kntxt, &secp->pubkey, secp->seckey);
+        assert(r);
+
+        size_t len = COMPPUB_SIZE;
+        int val = secp256k1_ec_pubkey_serialize(secp->kntxt, secp->compressed, &len, &secp->pubkey, SECP256K1_EC_COMPRESSED);
+
+        return 0;
+    }
+
+    return 1;
+}
+
+unsigned char *secp265k1_shared_key(secp256k1_t *private, secp256k1_t *public) {
+    unsigned char *shared = malloc(sizeof(unsigned char) * SHARED_SIZE);
+
+    int val = secp256k1_ecdh(private->kntxt, shared, &public->pubkey, private->seckey, NULL, NULL);
+
+    return shared;
+}
+
+
 int main() {
-    secp256k1_new();
+    secp256k1_t *bob = secp256k1_new();
+    secp256k1_generate_key(bob);
+
+    printf("Bob:\n");
+    dumphex(bob->seckey, SECKEY_SIZE);
+    dumphex(bob->compressed, COMPPUB_SIZE);
+
+    secp256k1_t *alice = secp256k1_new();
+    secp256k1_generate_key(alice);
+
+    printf("\n");
+    printf("Alice:\n");
+    dumphex(alice->seckey, SECKEY_SIZE);
+    dumphex(alice->compressed, COMPPUB_SIZE);
+
+    unsigned char *shared1 = secp265k1_shared_key(bob, alice);
+    unsigned char *shared2 = secp265k1_shared_key(alice, bob);
+
+    printf("\n");
+    printf("Shared Key:\n");
+    dumphex(shared1, SHARED_SIZE);
+    dumphex(shared2, SHARED_SIZE);
+
+    secp256k1_erase_free(shared1, SHARED_SIZE);
+    secp256k1_erase_free(shared2, SHARED_SIZE);
+
+    secp256k1_free(bob);
+    secp256k1_free(alice);
+
     return 0;
 }
-*/
+
--- keysafe/secp256k1/secp256k1mod.h
+++ keysafe/secp256k1/secp256k1mod.h
@@ -5,11 +5,20 @@
 
     typedef struct secp256k1_t {
         secp256k1_context *kntxt;
+        unsigned char *seckey;
+        unsigned char *compressed;
+        secp256k1_pubkey pubkey;
+        secp256k1_ecdsa_signature sig;
+
 
     } secp256k1_t;
 
+    #define SECKEY_SIZE    32
+    #define SHARED_SIZE    32
+    #define COMPPUB_SIZE   33
 
     secp256k1_t *secp256k1_new();
+    int secp256k1_generate_key(secp256k1_t *secp);
 
 #endif
 
