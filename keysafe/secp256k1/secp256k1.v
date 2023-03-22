3c060d432332f8c64f2dd0be41d6a8825d1e586d
Maxime Daniel <root@maxux.net>
2023-03-22 22:20:48 +0100

    secp256k1: improve v module rewrite

--- keysafe/secp256k1/secp256k1.v
+++ keysafe/secp256k1/secp256k1.v
@@ -4,8 +4,11 @@ module secp256k1
 #include "@VMODROOT/secp256k1mod.h"
 
 #flag @VMODROOT/secp256k1mod.o
-#flag -lsecp256k1
+#flag -lsecp256k1 -DNO_SECP_MAIN
 
+//
+// struct definitions
+//
 struct Secp256k1_pubkey { 
 	data [64]u8
 }
@@ -23,6 +26,7 @@ struct Secp256k1_keypair {
 }
 
 struct Secp256k1_t { 
+pub: // FIXME
 	kntxt &C.secp256k1_context
 	seckey &u8
 	compressed &u8
@@ -37,73 +41,67 @@ struct Secp256k1_sign_t {
 	serialized &u8
 	length usize
 }
+
+struct Secp256k1 {
+	cctx &Secp256k1_t
+}
+
+
+//
+// prototypes
+//
 fn C.secp256k1_new() &Secp256k1_t
-
-pub fn new() &Secp256k1_t {
-	return C.secp256k1_new()
-}
-
-fn C.secp256k1_free(secp &Secp256k1_t) 
-
-pub fn free(secp &Secp256k1_t)  {
-	C.secp256k1_free(secp)
-}
-
-fn C.secp256k1_generate_key(secp &Secp256k1_t) int
-
-pub fn generate_key(secp &Secp256k1_t) int {
-	return C.secp256k1_generate_key(secp)
-}
-
-fn C.secp256k1_load_key(secp &Secp256k1_t, key &u8) int
-
-pub fn load_key(secp &Secp256k1_t, key &u8) int {
-	return C.secp256k1_load_key(secp, key)
-}
-
-
-fn C.secp265k1_shared_key(private &Secp256k1_t, public &Secp256k1_t) &u8
-
-pub fn secp265k1_shared_key(private &Secp256k1_t, public &Secp256k1_t) &u8 {
-	return C.secp265k1_shared_key(private, public)
-}
-
-fn C.secp256k1_sign_hash(secp &Secp256k1_t, hash &u8, length usize) &u8
-
-pub fn sign_hash(secp &Secp256k1_t, hash &u8, length usize) &u8 {
-	return C.secp256k1_sign_hash(secp, hash, length)
-}
-
-fn C.secp256k1_load_signature(secp &Secp256k1_t, serialized &u8, length usize) &Secp256k1_sign_t
-
-pub fn load_signature(secp &Secp256k1_t, serialized &u8, length usize) &Secp256k1_sign_t {
-	return C.secp256k1_load_signature(secp, serialized, length)
-}
-
-fn C.secp256k1_sign_free(signature &Secp256k1_sign_t) 
-
-pub fn sign_free(signature &Secp256k1_sign_t)  {
-	C.secp256k1_sign_free(signature)
-}
-
-fn C.secp256k1_sign_verify(secp &Secp256k1_t, signature &Secp256k1_sign_t, hash &u8, length usize) int
-
-pub fn sign_verify(secp &Secp256k1_t, signature &Secp256k1_sign_t, hash &u8, length usize) int {
-	return C.secp256k1_sign_verify(secp, signature, hash, length)
-}
-
-fn C.secp256k1_schnorr_sign_hash(secp &Secp256k1_t, hash &u8, length usize) &u8
-
-pub fn schnorr_sign_hash(secp &Secp256k1_t, hash &u8, length usize) &u8 {
-	return C.secp256k1_schnorr_sign_hash(secp, hash, length)
-}
 
 fn C.secp256k1_schnorr_verify(secp &Secp256k1_t, signature &u8, siglen usize, hash &u8, hashlen usize) int
 
-pub fn schnorr_verify(secp &Secp256k1_t, signature &u8, siglen usize, hash &u8, hashlen usize) int {
-	return C.secp256k1_schnorr_verify(secp, signature, siglen, hash, hashlen)
+fn C.secp256k1_schnorr_sign_hash(secp &Secp256k1_t, hash &u8, length usize) &u8
+
+fn C.secp256k1_sign_verify(secp &Secp256k1_t, signature &Secp256k1_sign_t, hash &u8, length usize) int
+
+fn C.secp256k1_sign_free(signature &Secp256k1_sign_t) 
+
+fn C.secp256k1_load_signature(secp &Secp256k1_t, serialized &u8, length usize) &Secp256k1_sign_t
+
+fn C.secp256k1_sign_hash(secp &Secp256k1_t, hash &u8, length usize) &u8
+
+fn C.secp265k1_shared_key(private &Secp256k1_t, public &Secp256k1_t) &u8
+
+fn C.secp256k1_load_key(secp &Secp256k1_t, key &u8) int
+
+fn C.secp256k1_free(secp &Secp256k1_t)
+
+fn C.secp256k1_export(secp &Secp256k1_t) &u8
+
+fn C.secp256k1_generate_key(secp &Secp256k1_t) int
+
+//
+// v implementation
+//
+pub fn new() Secp256k1 {
+	secp := Secp256k1{}
+	secp.cctx = C.secp256k1_new()
+
+	return secp
 }
 
-pub fn test() {
-	x := "hello"
+pub fn (s Secp256k1) load(key string) {
+	println("load")
 }
+
+pub fn (s Secp256k1) generate() {
+	C.secp256k1_generate_key(s.cctx)
+}
+
+pub fn (s Secp256k1) keys() {
+    println(s.cctx.seckey)
+    println(s.cctx.compressed)
+    println(s.cctx.xcompressed)
+}
+
+pub fn (s Secp256k1) export() string {
+	key := C.secp256k1_export(s.cctx)
+	println(key)
+	return key.vstring()
+}
+
+
