d374c353f276f5160b09481029688b72b7e8ed8d
timurgordon <timurgordon@gmail.com>
2023-04-12 11:24:41 +0300

    add openrpc encode and test

---
+++ openrpc/encode_test.v
@@ -0,0 +1,40 @@
+module openrpc
+
+import jsonschema {Schema, SchemaRef}
+
+fn test_encode() ! {
+	doc := OpenRPC{
+		info: Info{
+			title: ''
+			version: '1.0.0'
+		}
+		methods: []Method{}
+	}
+	encoded := doc.encode()!
+	assert encoded == '{"openrpc":"2.0.0","info":{"version":"1.0.0"}}'
+}
+
+fn test_encode_2() ! {
+
+	doc := OpenRPC{
+		info: Info{
+			title: ''
+			version: '1.0.0'
+		}
+		methods: [
+			Method{
+				name: 'method_name'
+				summary: 'summary'
+				description: 'description for this method'
+				deprecated: true
+				params: [
+					ContentDescriptor{
+						name: 'sample descriptor'
+					}
+				]
+			}
+		]
+	}
+	encoded := doc.encode()!
+	assert encoded == '{"openrpc":"2.0.0","info":{"version":"1.0.0"},"methods":[{"name":"method_name","summary":"summary","description":"description for this method","params":[{"name":"sample descriptor"}],"paramStructure":"default"}]}'
+}
