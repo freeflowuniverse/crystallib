7147e7be1719bf0233b2ba477ef2d82a57cefac7
timurgordon <timurgordon@gmail.com>
2023-04-12 11:25:47 +0300

    create openrpc examples

---
+++ openrpc/examples/openrpc_client/methods.v
@@ -0,0 +1,26 @@
+module openrpc_client 
+
+pub fn (client Client) hello() {
+	println('hello')
+}
+
+pub fn (client Client) ping() string {
+	return 'pong'
+}
+
+[params]
+pub struct AnimalArgs {
+	name string
+	species string
+}
+
+// create_animal adds an animal with the provided arguments to the database
+pub fn (client Client) create_animal(args AnimalArgs) {
+	println('Creating animal `$args.name`')
+}
+
+// get_animal finds an animal in the database with the provided name
+// returns the animal: an animal in the db with a matching name
+pub fn (client Client) get_animal(name string) Animal {
+	return Animal{}
+}
---
+++ openrpc/examples/openrpc_client/model.v
@@ -0,0 +1,7 @@
+module openrpc_client
+
+pub struct Animal {
+	name string
+	species string
+	created_at string
+}
