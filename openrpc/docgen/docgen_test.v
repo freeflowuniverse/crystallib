87466dc04a47aae75d847e47bb6b31719048e41f
timurgordon <timurgordon@gmail.com>
2023-04-12 11:27:29 +0300

    add document generator tests

---
+++ openrpc/docgen/docgen_test.v
@@ -0,0 +1,23 @@
+module docgen
+
+import json
+import os
+
+fn test_docgen_simple() ! {
+	client_path := os.dir(@FILE).all_before_last('/') + '/examples/openrpc_client'
+	doc := docgen(
+		title: 'test'
+		source: client_path
+	)!
+	panic(doc.encode()!)
+}
+
+fn test_docgen() ! {
+
+	client_path := os.dir(@FILE).all_before_last('/') + '/examples/openrpc_client'
+	doc := docgen(
+		title: 'test'
+		source: client_path
+	)!
+	panic(doc.encode()!)
+}
