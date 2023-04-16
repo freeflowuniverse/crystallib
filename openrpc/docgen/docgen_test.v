ebbcce09f3408dd74ef2846ab772391f67c780db
timurgordon <timurgordon@gmail.com>
2023-04-16 15:22:32 +0300

    update codegen test

--- openrpc/docgen/docgen_test.v
+++ openrpc/docgen/docgen_test.v
@@ -4,10 +4,11 @@ import json
 import os
 
 fn test_docgen_simple() ! {
-	client_path := os.dir(@FILE).all_before_last('/') + '/examples/openrpc_client'
+	client_path := os.dir(@FILE).all_before_last('/') + '/examples/petstore_client'
 	doc := docgen(
 		title: 'test'
 		source: client_path
+		exclude_files: ['client.v']
 	)!
 	panic(doc.encode()!)
 }
