be1c02fdff874ab9060b9b5be6609eb514bb49bc
timurgordon <timurgordon@gmail.com>
2023-04-18 13:45:54 +0300

    test chapter.fix in chapter_test

--- books/library/chapter_test.v
+++ books/library/chapter_test.v
@@ -9,21 +9,13 @@ const testpath = os.dir(@FILE) + '/example/chapter1'
 
 fn test_fix() ! {
 	mut lib := new()
-	mut book := book_new(name: 'testbook')!
+	mut book := lib.book_new(name: 'testbook')!
 	mut test_chapter := book.chapter_new(
 		name: 'Chapter1'
 		path: library.testpath
 	) or { panic('Cannot create new chapter: ${err}') }
 
-	mut page_path := pathlib.get('${library.testpath}/decentralized_cloud/decentralized_cloud.md')
-	mut test_page := test_chapter.page_new(mut page_path) or { panic('Cannot create page: ${err}') }
-
-	println(test_page)
-
-	assert test_page.pathrel == 'decentralized_cloud/decentralized_cloud.md'
-	assert test_page.name == 'decentralized_cloud'
-
-	test_page.fix() or { panic('Cannot fix page: ${err}') }
+	test_chapter.fix() or { panic('Cannot fix page: ${err}') }
 }
 
 fn test_scan() {
