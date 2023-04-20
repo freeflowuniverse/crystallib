b2d96a7c0043bd79fd4f6004a1d53bfd43c328f7
timurgordon <timurgordon@gmail.com>
2023-04-20 18:01:03 +0300

    schema improvements and handling more types

--- jsonschema/schema.v
+++ jsonschema/schema.v
@@ -5,19 +5,41 @@ import freeflowuniverse.crystallib.codemodel
 pub fn struct_to_schema(struct_ codemodel.Struct) SchemaRef {
 	mut properties := map[string]SchemaRef{}
 	for field in struct_.fields {
-		properties[field.name] = typesymbol_to_schema(field.typ.symbol)
+		mut property_schema := SchemaRef(Schema{})
+		if field.typ.symbol.starts_with('_VAnonStruct') {
+			property_schema = struct_to_schema(field.anon_struct)
+		} else {
+			property_schema = typesymbol_to_schema(field.typ.symbol)
+		}
+		if mut property_schema is Schema {
+			properties[field.name] = SchemaRef(
+				Schema {
+					typ: property_schema.typ
+					title: property_schema.title
+					description: field.description
+				}
+			)
+		} else {
+			properties[field.name] = property_schema
+		}
 	}
+
+	title := if struct_.name.starts_with('_VAnonStruct') {
+		''} else {
+			struct_.name
+		}
 	
 	return SchemaRef(Schema{
-		title: struct_.name
+		title: title
+		description: struct_.description
 		properties: properties
 	})
 }
 
-// get schema_from_typesymbol receives a typesymbol, if the typesymbol belongs to a user defined struct
+// get typesymbol_to_schema receives a typesymbol, if the typesymbol belongs to a user defined struct
 // it returns a reference to the schema, else it returns a schema for the typesymbol
 pub fn typesymbol_to_schema(symbol_ string) SchemaRef {
-	mut symbol := symbol_.trim_string_left('!')
+	mut symbol := symbol_.trim_string_left('!').trim_string_left('?')
 	if symbol == '' {
 		return SchemaRef(Schema{typ: 'null'})
 	} else if symbol.starts_with('[]') {
@@ -33,6 +55,8 @@ pub fn typesymbol_to_schema(symbol_ string) SchemaRef {
 			additional_properties: typesymbol_to_schema(map_type)
 		})
 	} else if symbol[0].is_capital() {
+		return SchemaRef(Reference{ref: '#/components/schemas/$symbol'})
+	} else if symbol.starts_with('_VAnonStruct') {
 		return SchemaRef(Reference{ref: '#/components/schemas/$symbol'})
 	} else {
 		if symbol == 'void' {
@@ -57,6 +81,9 @@ pub fn typesymbol_to_schema(symbol_ string) SchemaRef {
 			return SchemaRef(Schema{typ: 'null'})
 		}
 		if symbol == 'i64' {
+			return SchemaRef(Schema{typ: 'string'})
+		}
+		if symbol == 'byte' {
 			return SchemaRef(Schema{typ: 'string'})
 		}
 		return SchemaRef(Schema{typ: symbol})
