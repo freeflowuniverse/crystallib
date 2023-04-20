3c3982305cebe580c57aeaf3f2cf825527a6f5da
timurgordon <timurgordon@gmail.com>
2023-04-20 18:05:17 +0300

    add annotations to petstore_client example

--- openrpc/examples/petstore_client/model.v
+++ openrpc/examples/petstore_client/model.v
@@ -1,7 +1,8 @@
 module petstore_client
 
+// a pet struct that represents a pet
 struct Pet {
-	name string
-	tag string
-	id int
+	name string // name of the pet
+	tag string // a tag of the pet, helps finding pet 
+	id int // unique indentifier
 }
