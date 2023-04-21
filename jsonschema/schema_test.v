4e7a64f7a161ae38707572177b34d958e5461353
timurgordon <timurgordon@gmail.com>
2023-04-21 12:49:24 +0300

    improve tests

--- jsonschema/schema_test.v
+++ jsonschema/schema_test.v
@@ -1,6 +1,23 @@
 module jsonschema
 
-fn test_define() {
-	mut schema := Schema{}
-	// schema.define()
+import freeflowuniverse.crystallib.codemodel
+
+fn test_struct_to_schema() {
+	struct_ := codemodel.Struct {
+		name: 'test_name'
+		description: 'a codemodel struct to test struct to schema serialization'
+		fields: [
+			codemodel.StructField {
+				name: 'test_field'
+				description: 'a field of the test struct to test fields serialization into schema'
+				typ: codemodel.Type {
+					symbol: 'string'
+				}
+			},
+		]
+	}
+
+	schema := struct_to_schema(struct_)
+	println(schema)
+	panic('as')
 }
