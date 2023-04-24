c535a4eb607b483487b4dd26c1f98d9f16edb82d
timurgordon <timurgordon@gmail.com>
2023-04-24 11:16:47 +0300

    improve page fix testing

--- books/library/page_test.v
+++ books/library/page_test.v
@@ -7,6 +7,7 @@ import freeflowuniverse.crystallib.pathlib
 const testpath = os.dir(@FILE) + '/testdata/broken_chapter'
 
 fn testsuite_end() {
+	// reset testdata changes after running tests
 	os.execute('git checkout $testpath')
 }
 
@@ -25,9 +26,19 @@ fn test_fix() ! {
 	mut page_path := pathlib.get('${library.testpath}/wrong_links/page_with_wrong_links.md')
 	mut test_page := test_chapter.page_new(mut page_path) or { panic('Cannot create page: ${err}') }
 
+	doc_before := *((*test_page).doc)
 	test_page.fix() or { panic('Cannot fix page: ${err}') }
+	
+	assert !test_page.changed // should be set to false after fix
+	assert test_page.doc != doc_before // page was actually modified
 
-	assert test_page.changed
+	paragraph := test_page.doc.items[1] as Paragraph
+	wrong_link := paragraph.items[1] as Link
+	right_link := paragraph.items[3] as Link
+
+	assert wrong_link.path := 
+	println(test_page.doc)
+	panic('s')
 }
 
 fn test_fix_links() ! {}
