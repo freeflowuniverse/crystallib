1a74777fa199625be15939737c5bc61a662d81e9
timurgordon <timurgordon@gmail.com>
2023-04-24 12:49:50 +0300

    more rigorous testing and make more concise

--- codeparser/vparser_test.v
+++ codeparser/vparser_test.v
@@ -1,7 +1,6 @@
 module codeparser
 
-import freeflowuniverse.crystallib.codemodel {CodeItem, Function, Struct}
-
+import freeflowuniverse.crystallib.codemodel { CodeItem, Function, Struct }
 import os
 
 const testpath = os.dir(@FILE) + '/testdata'
@@ -10,16 +9,17 @@ const testpath = os.dir(@FILE) + '/testdata'
 // used to make assertions and verify test outputs
 const testcode = {
 	'anotherfile.v': [
-		codemodel.CodeItem(codemodel.Function{
+		CodeItem(Function{
 			name: 'anotherfile_func0'
 			receiver: codemodel.Param{
+				required: false
 				description: ''
 				name: ''
 				typ: codemodel.Type{
 					symbol: ''
 				}
 			}
-			description: ''
+			description: ' is the first function of file'
 			params: []
 			body: ''
 			result: codemodel.Result{
@@ -30,23 +30,28 @@ const testcode = {
 				name: ''
 			}
 			has_return: false
-		}), codemodel.CodeItem(codemodel.Function{
+		}),
+		CodeItem(Function{
 			name: 'anotherfile_func1'
 			receiver: codemodel.Param{
+				required: false
 				description: ''
 				name: ''
 				typ: codemodel.Type{
 					symbol: ''
 				}
 			}
-			description: ''
-			params: [codemodel.Param{
-				description: 'a name that the function will do nothing with'
-				name: 'name'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}]
+			description: ' is the second function of file'
+			params: [
+				codemodel.Param{
+					required: false
+					description: 'a name that the function will do nothing with'
+					name: 'name'
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+			]
 			body: ''
 			result: codemodel.Result{
 				typ: codemodel.Type{
@@ -56,40 +61,62 @@ const testcode = {
 				name: ''
 			}
 			has_return: false
-		}), codemodel.CodeItem(codemodel.Struct{
+		}),
+		CodeItem(Struct{
 			name: 'AnotherfileStruct0'
-			fields: [codemodel.StructField{
-				comments: []
-				attrs: []
-				name: 'param1'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}, codemodel.StructField{
-				comments: []
-				attrs: []
-				name: 'param2'
-				typ: codemodel.Type{
-					symbol: 'int'
-				}
-			}]
-		}), codemodel.CodeItem(codemodel.Function{
+			description: 'AnotherfileStruct0 defines the configuration params of anotherfile_func2'
+			fields: [
+				codemodel.StructField{
+					comments: []
+					attrs: []
+					name: 'param1'
+					description: ''
+					anon_struct: Struct{
+						name: ''
+						description: ''
+						fields: []
+					}
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+				codemodel.StructField{
+					comments: []
+					attrs: []
+					name: 'param2'
+					description: ''
+					anon_struct: Struct{
+						name: ''
+						description: ''
+						fields: []
+					}
+					typ: codemodel.Type{
+						symbol: 'int'
+					}
+				},
+			]
+		}),
+		CodeItem(Function{
 			name: 'anotherfile_func2'
 			receiver: codemodel.Param{
+				required: false
 				description: ''
 				name: ''
 				typ: codemodel.Type{
 					symbol: ''
 				}
 			}
-			description: ''
-			params: [codemodel.Param{
-				description: 'configuration for anotherfile_func2'
-				name: 'config'
-				typ: codemodel.Type{
-					symbol: 'AnotherfileStruct0'
-				}
-			}]
+			description: ' is the third function of the file'
+			params: [
+				codemodel.Param{
+					required: false
+					description: 'configuration for anotherfile_func2'
+					name: 'config'
+					typ: codemodel.Type{
+						symbol: 'AnotherfileStruct0'
+					}
+				},
+			]
 			body: ''
 			result: codemodel.Result{
 				typ: codemodel.Type{
@@ -99,39 +126,56 @@ const testcode = {
 				name: ''
 			}
 			has_return: false
-		}), codemodel.CodeItem(codemodel.Struct{
+		}),
+		CodeItem(Struct{
 			name: 'AnotherfileStruct1'
-			fields: [codemodel.StructField{
-				comments: []
-				attrs: []
-				name: 'param'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}]
-		}), codemodel.CodeItem(codemodel.Function{
+			description: ''
+			fields: [
+				codemodel.StructField{
+					comments: []
+					attrs: []
+					name: 'param'
+					description: ''
+					anon_struct: Struct{
+						name: ''
+						description: ''
+						fields: []
+					}
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+			]
+		}),
+		CodeItem(Function{
 			name: 'anotherfile_func3'
 			receiver: codemodel.Param{
+				required: false
 				description: ''
 				name: ''
 				typ: codemodel.Type{
 					symbol: ''
 				}
 			}
-			description: ''
-			params: [codemodel.Param{
-				description: ''
-				name: 'param1'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}, codemodel.Param{
-				description: ''
-				name: 'param2'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}]
+			description: ' is the fourth function of the file is does something with param1 and param2 and creates AnotherfileStruct1'
+			params: [
+				codemodel.Param{
+					required: false
+					description: ''
+					name: 'param1'
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+				codemodel.Param{
+					required: false
+					description: ''
+					name: 'param2'
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+			]
 			body: ''
 			result: codemodel.Result{
 				typ: codemodel.Type{
@@ -141,19 +185,20 @@ const testcode = {
 				name: 'the created filestruct1'
 			}
 			has_return: false
-		})
-	],
-	'subfile.v': [
-		codemodel.CodeItem(codemodel.Function{
+		}),
+	]
+	'subfile.v':     [
+		CodeItem(Function{
 			name: 'subfile_func0'
 			receiver: codemodel.Param{
+				required: false
 				description: ''
 				name: ''
 				typ: codemodel.Type{
 					symbol: ''
 				}
 			}
-			description: ''
+			description: ' is the first function of file'
 			params: []
 			body: ''
 			result: codemodel.Result{
@@ -164,23 +209,28 @@ const testcode = {
 				name: ''
 			}
 			has_return: false
-		}), codemodel.CodeItem(codemodel.Function{
+		}),
+		CodeItem(Function{
 			name: 'subfile_func1'
 			receiver: codemodel.Param{
+				required: false
 				description: ''
 				name: ''
 				typ: codemodel.Type{
 					symbol: ''
 				}
 			}
-			description: ''
-			params: [codemodel.Param{
-				description: 'a name that the function will do nothing with'
-				name: 'name'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}]
+			description: ' is the second function of file'
+			params: [
+				codemodel.Param{
+					required: false
+					description: 'a name that the function will do nothing with'
+					name: 'name'
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+			]
 			body: ''
 			result: codemodel.Result{
 				typ: codemodel.Type{
@@ -190,40 +240,62 @@ const testcode = {
 				name: ''
 			}
 			has_return: false
-		}), codemodel.CodeItem(codemodel.Struct{
+		}),
+		CodeItem(Struct{
 			name: 'SubfileStruct0'
-			fields: [codemodel.StructField{
-				comments: []
-				attrs: []
-				name: 'param1'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}, codemodel.StructField{
-				comments: []
-				attrs: []
-				name: 'param2'
-				typ: codemodel.Type{
-					symbol: 'int'
-				}
-			}]
-		}), codemodel.CodeItem(codemodel.Function{
+			description: 'SubfileStruct0 defines the configuration params of subfile_func2'
+			fields: [
+				codemodel.StructField{
+					comments: []
+					attrs: []
+					name: 'param1'
+					description: ''
+					anon_struct: Struct{
+						name: ''
+						description: ''
+						fields: []
+					}
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+				codemodel.StructField{
+					comments: []
+					attrs: []
+					name: 'param2'
+					description: ''
+					anon_struct: Struct{
+						name: ''
+						description: ''
+						fields: []
+					}
+					typ: codemodel.Type{
+						symbol: 'int'
+					}
+				},
+			]
+		}),
+		CodeItem(Function{
 			name: 'subfile_func2'
 			receiver: codemodel.Param{
+				required: false
 				description: ''
 				name: ''
 				typ: codemodel.Type{
 					symbol: ''
 				}
 			}
-			description: ''
-			params: [codemodel.Param{
-				description: 'configuration for subfile_func2'
-				name: 'config'
-				typ: codemodel.Type{
-					symbol: 'SubfileStruct0'
-				}
-			}]
+			description: ' is the third function of the file'
+			params: [
+				codemodel.Param{
+					required: false
+					description: 'configuration for subfile_func2'
+					name: 'config'
+					typ: codemodel.Type{
+						symbol: 'SubfileStruct0'
+					}
+				},
+			]
 			body: ''
 			result: codemodel.Result{
 				typ: codemodel.Type{
@@ -233,39 +305,56 @@ const testcode = {
 				name: ''
 			}
 			has_return: false
-		}), codemodel.CodeItem(codemodel.Struct{
+		}),
+		CodeItem(Struct{
 			name: 'SubfileStruct1'
-			fields: [codemodel.StructField{
-				comments: []
-				attrs: []
-				name: 'param'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}]
-		}), codemodel.CodeItem(codemodel.Function{
+			description: ''
+			fields: [
+				codemodel.StructField{
+					comments: []
+					attrs: []
+					name: 'param'
+					description: ''
+					anon_struct: Struct{
+						name: ''
+						description: ''
+						fields: []
+					}
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+			]
+		}),
+		CodeItem(Function{
 			name: 'subfile_func3'
 			receiver: codemodel.Param{
+				required: false
 				description: ''
 				name: ''
 				typ: codemodel.Type{
 					symbol: ''
 				}
 			}
-			description: ''
-			params: [codemodel.Param{
-				description: ''
-				name: 'param1'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}, codemodel.Param{
-				description: ''
-				name: 'param2'
-				typ: codemodel.Type{
-					symbol: 'string'
-				}
-			}]
+			description: ' is the fourth function of the file is does something with param1 and param2 and creates SubfileStruct1'
+			params: [
+				codemodel.Param{
+					required: false
+					description: ''
+					name: 'param1'
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+				codemodel.Param{
+					required: false
+					description: ''
+					name: 'param2'
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+			]
 			body: ''
 			result: codemodel.Result{
 				typ: codemodel.Type{
@@ -275,144 +364,231 @@ const testcode = {
 				name: 'the created filestruct1'
 			}
 			has_return: false
-		})
+		}),
 	]
-	'file.v': []CodeItem{}
+	'file.v':        [
+		CodeItem(Function{
+			name: 'file_func0'
+			receiver: codemodel.Param{
+				required: false
+				description: ''
+				name: ''
+				typ: codemodel.Type{
+					symbol: ''
+				}
+			}
+			description: ' is the first function of file'
+			params: []
+			body: ''
+			result: codemodel.Result{
+				typ: codemodel.Type{
+					symbol: 'void'
+				}
+				description: ''
+				name: ''
+			}
+			has_return: false
+		}),
+		CodeItem(Function{
+			name: 'file_func1'
+			receiver: codemodel.Param{
+				required: false
+				description: ''
+				name: ''
+				typ: codemodel.Type{
+					symbol: ''
+				}
+			}
+			description: ' is the second function of file'
+			params: [
+				codemodel.Param{
+					required: false
+					description: 'a name that the function will do nothing with'
+					name: 'name'
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+			]
+			body: ''
+			result: codemodel.Result{
+				typ: codemodel.Type{
+					symbol: 'void'
+				}
+				description: ''
+				name: ''
+			}
+			has_return: false
+		}),
+		CodeItem(Struct{
+			name: 'FileStruct0'
+			description: 'FileStruct0 defines the configuration params of file_func2'
+			fields: [
+				codemodel.StructField{
+					comments: []
+					attrs: []
+					name: 'param1'
+					description: ''
+					anon_struct: Struct{
+						name: ''
+						description: ''
+						fields: []
+					}
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+				codemodel.StructField{
+					comments: []
+					attrs: []
+					name: 'param2'
+					description: ''
+					anon_struct: Struct{
+						name: ''
+						description: ''
+						fields: []
+					}
+					typ: codemodel.Type{
+						symbol: 'int'
+					}
+				},
+			]
+		}),
+		CodeItem(Function{
+			name: 'file_func2'
+			receiver: codemodel.Param{
+				required: false
+				description: ''
+				name: ''
+				typ: codemodel.Type{
+					symbol: ''
+				}
+			}
+			description: ' is the third function of the file'
+			params: [
+				codemodel.Param{
+					required: false
+					description: 'configuration for file_func2'
+					name: 'config'
+					typ: codemodel.Type{
+						symbol: 'FileStruct0'
+					}
+				},
+			]
+			body: ''
+			result: codemodel.Result{
+				typ: codemodel.Type{
+					symbol: 'void'
+				}
+				description: ''
+				name: ''
+			}
+			has_return: false
+		}),
+		CodeItem(Struct{
+			name: 'FileStruct1'
+			description: ''
+			fields: []
+		}),
+		CodeItem(Function{
+			name: 'file_func3'
+			receiver: codemodel.Param{
+				required: false
+				description: ''
+				name: ''
+				typ: codemodel.Type{
+					symbol: ''
+				}
+			}
+			description: ' is the fourth function of the file is does something with param1 and param2 and creates FileStruct1'
+			params: [
+				codemodel.Param{
+					required: false
+					description: ''
+					name: 'param1'
+					typ: codemodel.Type{
+						symbol: 'string'
+					}
+				},
+				codemodel.Param{
+					required: false
+					description: ''
+					name: 'param2'
+					typ: codemodel.Type{
+						symbol: 'int'
+					}
+				},
+			]
+			body: ''
+			result: codemodel.Result{
+				typ: codemodel.Type{
+					symbol: 'FileStruct1'
+				}
+				description: 'a FileStruc1 struct filled in with params 1 and 2'
+				name: 'the created filestruct1'
+			}
+			has_return: false
+		}),
+	]
 }
 
-// tests parsing blank directory, should return empty codeitem array.
 fn test_vparse_blankdir() ! {
-	$if debug {
-		eprintln('\nTEST: test_vparse_blankdir\n')
-	}
-	code := parse_v('$testpath/blankdir')!
+	code := parse_v('${codeparser.testpath}/blankdir')!
 	assert code.len == 0
 }
 
-// tests whether can parse a flat directory
-// with no subdirectories but multiple files
-fn test_vparse_flatdir() ! {
-	$if debug {
-		eprintln('\nTEST: test_vparse_flatdir\n')
-	}
-	code := parse_v('$testpath/flatdir')!
+fn test_vparse_flat_directory() ! {
+	code := parse_v('${codeparser.testpath}/flatdir')!
 	assert code.len == 12
-
-	assert code[0] is Function
-	function0 := code[0] as Function
-	assert function0.name == 'anotherfile_func0'
-
-	assert code[1] is Function
-	function1 := code[1] as Function
-	assert function1.name == 'anotherfile_func1'
+	assert code[0..6] == codeparser.testcode['anotherfile.v'][0..6]
+	assert code[6..12] == codeparser.testcode['subfile.v'][0..6]
 }
 
-// tests wheter can parse a directory with subdirectories
-// without recursing into subdirectories
 fn test_vparse_non_recursive() ! {
-	$if debug {
-		eprintln('\nTEST: test_vparse_non_recursive\n')
-	}
-	code := parse_v(testpath)!
-	// should be only 6 since 
+	code := parse_v(codeparser.testpath)!
 	assert code.len == 6
-
-	assert code[0] is Function
-	function0 := code[0] as Function
-	assert function0.name == 'file_func0'
-
-	assert code[1] is Function
-	function1 := code[1] as Function
-	assert function1.name == 'file_func1'
+	assert code[0..6] == codeparser.testcode['file.v'][0..6]
 }
 
-// tests wheter can parse a directory with subdirectories
-// with recursing into subdirectories
 fn test_vparse_recursive() ! {
 	$if debug {
 		eprintln('\nTEST: test_vparse_recursive\n')
 	}
-	code := parse_v(testpath, recursive: true)!
-	// should be 18 since there are 3 files with 6 CodeItems each
+	code := parse_v(codeparser.testpath, recursive: true)!
 	assert code.len == 18
-
-	assert code[0] is Function
-	function0 := code[0] as Function
-	assert function0.name == 'anotherfile_func0'
-
-	assert code[1] is Function
-	function1 := code[1] as Function
-	assert function1.name == 'anotherfile_func1'
+	assert code[0..6] == codeparser.testcode['anotherfile.v'][0..6]
+	assert code[6..12] == codeparser.testcode['subfile.v'][0..6]
+	assert code[12..18] == codeparser.testcode['file.v'][0..6]
 }
 
-// tests wheter can parse a directory with subdirectories
-// and exclude a directory
-fn test_vparse_exclude_dirs() ! {
-	$if debug {
-		eprintln('\nTEST: test_vparse_exclude_dirs\n')
-	}
-	code := parse_v(testpath, 
-	recursive: true
-	exclude_dirs: ['flatdir']
+fn test_vparse_exclude_directories() ! {
+	code := parse_v(codeparser.testpath,
+		recursive: true
+		exclude_dirs: ['flatdir']
 	)!
-
-	// should be 6 since there is only 1 file with 6 CodeItems excluding flatdir
 	assert code.len == 6
-
-	// codeitems should have prefix file_func since anotherfile and subfile are excluded
-	assert code[0] is Function
-	function0 := code[0] as Function
-	assert function0.name == 'file_func0'
-
-	assert code[1] is Function
-	function1 := code[1] as Function
-	assert function1.name == 'file_func1'
+	assert code[0..6] == codeparser.testcode['file.v'][0..6]
 }
 
-// tests wheter can parse a directory with subdirectories
-// and exclude a file
 fn test_vparse_exclude_files() ! {
-	$if debug {
-		eprintln('\nTEST: test_vparse_exclude_files\n')
-	}
-	code := parse_v(testpath, 
-	recursive: true
-	exclude_files: ['flatdir/anotherfile.v']
+	code := parse_v(codeparser.testpath,
+		recursive: true
+		exclude_files: ['flatdir/anotherfile.v']
 	)!
-
-	// should be 12 since there are 2 files with 6 CodeItems excluding anotherfile.v
 	assert code.len == 12
-
-	// codeitems should have prefix file_func since anotherfile and subfile are excluded
-	assert code[0] is Function
-	function0 := code[0] as Function
-	assert function0.name == 'subfile_func0'
-
-	assert code[1] is Function
-	function1 := code[1] as Function
-	assert function1.name == 'subfile_func1'
+	assert code[0..6] == codeparser.testcode['subfile.v'][0..6]
+	assert code[6..12] == codeparser.testcode['file.v'][0..6]
 }
 
-// tests wheter can parse a directory with subdirectories
-// and exclude a file
-fn test_vparse_only_pub() ! {
-	$if debug {
-		eprintln('\nTEST: test_vparse_only_pub\n')
-	}
-	code := parse_v(testpath, 
+fn test_vparse_only_public() ! {
+	code := parse_v(codeparser.testpath,
 		recursive: true
 		only_pub: true
 	)!
 
-	// should be 12 since there are 2 files with 6 CodeItems excluding anotherfile.v
+	// first function of each code file is private so should skip those
 	assert code.len == 15
-
-	// codeitems should have prefix file_func since anotherfile and subfile are excluded
-	assert code[0] is Function
-	function0 := code[0] as Function
-	assert function0.name == 'anotherfile_func1'
-
-	assert code[1] is Struct
-	function1 := code[1] as Struct
-	assert function1.name == 'AnotherfileStruct0'
+	assert code[0..5] == codeparser.testcode['anotherfile.v'][1..6]
+	assert code[5..10] == codeparser.testcode['subfile.v'][1..6]
+	assert code[10..15] == codeparser.testcode['file.v'][1..6]
 }
