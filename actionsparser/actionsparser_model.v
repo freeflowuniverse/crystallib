c8ec8d2c4d83f71861aaf2dc48e51662dd438bb4
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2023-06-22 12:05:42 +0300

    change limits

--- actionsparser/actionsparser_model.v
+++ actionsparser/actionsparser_model.v
@@ -29,8 +29,8 @@ fn book_check(c string, block string) ! {
 	if c.len < 3 {
 		return error("book bad specified (len min 3), found '${c}'.\nFor block: ${block}")
 	}
-	if c.len > 10 {
-		return error("book bad specified (len max 10), found '${c}'.\nFor block: ${block}")
+	if c.len > 20 {
+		return error("book bad specified (len max 20), found '${c}'.\nFor block: ${block}")
 	}
 }
 
@@ -39,8 +39,8 @@ fn actor_check(c string, block string) ! {
 		return error('actor not specified\nFor block: ${block}')
 	}
 
-	if c.len < 4 {
-		return error("actor bad specified (len min 4), found '${c}'.\nFor block: ${block}")
+	if c.len < 2 {
+		return error("actor bad specified (len min 2), found '${c}'.\nFor block: ${block}")
 	}
 	if c.len > 20 {
 		return error("actor bad specified (len max 20), found '${c}'.\nFor block: ${block}")
