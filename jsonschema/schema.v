4a280b7c67cf174034bd410aed4213e859eee76d
timurgordon <timurgordon@gmail.com>
2023-07-03 16:27:19 +0300

    add number validation fields to JSON schema model

--- jsonschema/schema.v
+++ jsonschema/schema.v
@@ -9,6 +9,8 @@ pub:
 	ref string [json: '\$ref'; required]
 }
 
+type Number = int
+
 // https://json-schema.org/draft-07/json-schema-release-notes.html
 pub struct Schema {
 pub mut:
@@ -24,4 +26,12 @@ pub mut:
 	items                 Items
 	defs                  map[string]Schema
 	one_of	?[]SchemaRef [json: 'oneOf']
+
+	// from https://git.sr.ht/~emersion/go-jsonschema/tree/master/item/schema.go
+	// Validation for numbers
+	multiple_of      Number [json: "multipleOf"]
+	maximum          Number [json_as_number]
+	exclusive_maximum Number [json: "exclusiveMaximum"]
+	minimum          Number
+	exclusive_minimum Number [json: "exclusiveMinimum"]
 }
