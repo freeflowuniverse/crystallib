04bd45695390facb90682f93397498f29b1839f7
brandonpille <brandon@threefold.tech>
2023-07-07 15:32:51 +0200

    Fixed bug

--- params/params_getresource.v
+++ params/params_getresource.v
@@ -7,6 +7,7 @@ import strconv
 pub fn (params &Params) get_storagecapacity_in_bytes(key string) !u64 {
 	valuestr := params.get(key)!
 	mut times := 1
+	mut units_string_size := 0
 	if valuestr.len > 2 && !valuestr[valuestr.len - 2].is_digit()
 		&& !valuestr[valuestr.len - 1].is_digit() {
 		times = match valuestr[valuestr.len - 2..].to_upper() {
@@ -26,9 +27,9 @@ pub fn (params &Params) get_storagecapacity_in_bytes(key string) !u64 {
 		if times == 0 {
 			return error('not valid: should end with kb, mb or gb')
 		}
+		units_string_size = 2
 	}
-
-	val := strconv.parse_uint(valuestr[0..valuestr.len - 2], 10, 64) or {
+	val := strconv.parse_uint(valuestr[0..valuestr.len - units_string_size], 10, 64) or {
 		return error('Parameter ${key} = ${valuestr} does not include a valid unsigned 64-bit integer')
 	}
 	return val * u64(times)
@@ -45,6 +46,7 @@ pub fn (params &Params) get_storagecapacity_in_bytes_default(key string, defval 
 pub fn (params &Params) get_storagecapacity_in_gigabytes(key string) !u64 {
 	valuestr := params.get(key)!
 	mut units := 1
+	mut units_string_size := 0
 	if valuestr.len > 2 && !valuestr[valuestr.len - 2].is_digit()
 		&& !valuestr[valuestr.len - 1].is_digit() {
 		units = match valuestr[valuestr.len - 2..].to_upper() {
@@ -64,9 +66,10 @@ pub fn (params &Params) get_storagecapacity_in_gigabytes(key string) !u64 {
 		if units == 0 {
 			return error('not valid: should end with kb, mb or gb')
 		}
+		units_string_size = 2
 	}
 
-	val := strconv.parse_uint(valuestr[0..valuestr.len - 2], 10, 64) or {
+	val := strconv.parse_uint(valuestr[0..valuestr.len - units_string_size], 10, 64) or {
 		return error('Parameter ${key} = ${valuestr} does not include a valid unsigned 64-bit integer')
 	}
 	mut ret := val / u64(units)
