fa644b2e9319e7d68bc738435975e4aba97da106
Ali Chaddad <chaddada@incubaid.com>
2023-08-02 12:24:01 +0300

    add doc for open ai client

--- clients/openai/model_enums.v
+++ clients/openai/model_enums.v
@@ -9,6 +9,7 @@ pub enum ModelType {
 	gpt_3_5_turbo_0613
 	gpt_3_5_turbo_16k
 	gpt_3_5_turbo_16k_0613
+	whisper_1
 }
 
 fn modelname_str(e ModelType) string {
@@ -18,9 +19,35 @@ fn modelname_str(e ModelType) string {
 	if e == .gpt_3_5_turbo {
 		return 'gpt-3.5-turbo'
 	}
-
-	// TODO: https://platform.openai.com/docs/models/how-we-use-your-data implement remaining
-	panic('could not find right type')
+	return match e {
+		.gpt_4 {
+			'gpt-4'
+		}
+		.gpt_3_5_turbo {
+			'gpt-3.5-turbo'
+		}
+		.gpt_4_0613 {
+			'gpt-4-0613'
+		}
+		.gpt_4_32k {
+			'gpt-4-32k'
+		}
+		.gpt_4_32k_0613 {
+			'gpt-4-32k-0613'
+		}
+		.gpt_3_5_turbo_0613 {
+			'gpt-3.5-turbo-0613'
+		}
+		.gpt_3_5_turbo_16k {
+			'gpt-3.5-turbo-16k'
+		}
+		.gpt_3_5_turbo_16k_0613 {
+			'gpt-3.5-turbo-16k-0613'
+		}
+		.whisper_1 {
+			'whisper-1'
+		}
+	}
 }
 
 pub enum RoleType {
