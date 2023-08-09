bfeefd55f8ce4a4f91821a19c705d427c7beb9aa
Rob Van Mieghem <robvanmieghem@gmail.com>
2023-08-09 07:40:45 +0200

    Fix docker contains_ssh_port
    Solves #207

--- docker/docker_tools.v
+++ docker/docker_tools.v
@@ -102,10 +102,8 @@ fn clear_str(s string) string {
 fn contains_ssh_port(forwarded_ports []string) bool {
 	for port in forwarded_ports {
 		splitted := port.split(':')
-		if splitted.contains(':') {
-			if splitted[1] == '22' || splitted[1] == '22/tcp' {
+			if splitted.last() == '22' || splitted.last() == '22/tcp' {
 				return true
-			}
 		}
 	}
 	return false
