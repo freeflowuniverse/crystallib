fdcfe127ac96ac3dd0418ec384b7cee19fd6c3fe
kassem <omarksm09@gmail.com>
2023-08-29 17:54:03 +0300

    add simple email example

--- sendgrid/example/main.v
+++ sendgrid/example/main.v
@@ -1,24 +1,25 @@
 module main
+
 import os
-import  freeflowuniverse.crystallib.sendgrid {new_sendgrid_client}
+import freeflowuniverse.crystallib.sendgrid { new_sendgrid_client }
 
-fn main(){
+fn main() {
 	cred := sendgrid.Credintials{
-			token:  os.getenv('SENDGRID_AUTH_TOKEN')
-			source: os.getenv("SENDGRID_EMAIL")
-			api: "https://api.sendgrid.com/v3/mail/send"
-		}
+		token: os.getenv('SENDGRID_AUTH_TOKEN')
+		source: os.getenv('SENDGRID_EMAIL')
+		api: 'https://api.sendgrid.com/v3/mail/send'
+	}
 
-	mut client := new_sendgrid_client(cred)or{ 
+	mut client := new_sendgrid_client(cred) or {
 		println('something went wrong')
 		return
-	 }
-	email := client.new_email([sendgrid.Personalizations{to:[sendgrid.Recipiant{email:'omarksm09@gmail.com'}]}], 'hello', [sendgrid.Content{type_: 'text/html', value:'testing'}])!
-	res:= client.send(email) or { 
-	print(err)
-	return
-		}
+	}
+
+	email := sendgrid.new_email(['mariobassem12@gmail.com', 'omarksm09@gmail.com'], client.source,
+		'finally works', 'done today')
+	res := client.send(email) or {
+		print(err)
+		return
+	}
 	println(res.str())
-	
-	
 }
