550200547154f73073d04eca367a2bd5d376748f
kassem <omarksm09@gmail.com>
2023-08-29 14:30:05 +0300

    WIP: update example

--- sendgrid/example/main.v
+++ sendgrid/example/main.v
@@ -3,15 +3,22 @@ import os
 import  freeflowuniverse.crystallib.sendgrid {new_sendgrid_client}
 
 fn main(){
-		cred := sendgrid.Credintials{
-		token:  os.getenv('SENDGRID_AUTH_TOKEN')
-		source: os.getenv("SENDGRID_EMAIL")
-		api: "https://api.sendgrid.com/v3/mail/send"
-	}
+	cred := sendgrid.Credintials{
+			token:  os.getenv('SENDGRID_AUTH_TOKEN')
+			source: os.getenv("SENDGRID_EMAIL")
+			api: "https://api.sendgrid.com/v3/mail/send"
+		}
 
 	mut client := new_sendgrid_client(cred)or{ 
 		println('something went wrong')
 		return
 	 }
+	email := client.new_email([sendgrid.Personalizations{to:[sendgrid.Recipiant{email:'omarksm09@gmail.com'}]}], 'hello', [sendgrid.Content{type_: 'text/html', value:'testing'}])!
+	res:= client.send(email) or { 
+	print(err)
+	return
+		}
+	println(res.str())
+	
 	
 }
