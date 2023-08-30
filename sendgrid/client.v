02b39f0d597589f77964fc1040382c5b77e4adc1
kassem <omarksm09@gmail.com>
2023-08-30 13:15:24 +0300

    update clint

--- sendgrid/client.v
+++ sendgrid/client.v
@@ -1,49 +1,37 @@
 module sendgrid
+
 import net.http
 import json
 
 pub struct Client {
 pub:
 	token   string
-	source  string
-	api_url string
 }
 
-pub struct Content {
-	type_ string [json: 'type'] = 'text/html' 
-	value string
-}
+const send_api_endpoint = 'https://api.sendgrid.com/v3/mail/send'
 
+pub fn new_client(token string) !Client {
+	if token.len == 0 {
+		return error('empty token')
+	}
 
-
-[params]
-pub struct Credintials {
-pub:
-	source string
-	token  string
-	api    string
-}
-
-pub fn new_sendgrid_client(cred Credintials) !Client {
 	return Client{
-		token: cred.token
-		api_url: cred.api
-		source: cred.source
+		token: token
 	}
 }
 
 fn (c Client) get_headers() !http.Header {
-	mut headers_map := map[string]string{}
-	headers_map['Authorization'] = 'Bearer ${c.token}'
-	headers_map['Content-Type'] = 'application/json'
+	headers_map := {
+		'Authorization': 'Bearer ${c.token}'
+		'Content-Type':  'application/json'
+	}
 	headers := http.new_custom_header_from_map(headers_map)!
 
 	return headers
 }
 
-
-pub fn (mut c Client) send(email Email) !http.Response {
-	mut request := http.new_request(http.Method.post, c.api_url, json.encode(email))
+pub fn (c Client) send(email Email) !http.Response {
+	mut request := http.new_request(http.Method.post, sendgrid.send_api_endpoint, json.encode(email))
 	request.header = c.get_headers()!
 
 	res := request.do()!
