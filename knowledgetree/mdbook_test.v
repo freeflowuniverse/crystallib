b3a876c0b40ec28016e5ddc5da5700f62dfcd0f9
timurgordon <timurgordon@gmail.com>
2023-09-04 15:56:11 +0300

    fix mdbook test

--- knowledgetree/mdbook_test.v
+++ knowledgetree/mdbook_test.v
@@ -1,13 +1,16 @@
 module knowledgetree
 
+import freeflowuniverse.crystallib.baobab.spawner
 import os
 
 const collections_path = os.dir(@FILE) + '/testdata/collections'
 
 const book1_path = os.dir(@FILE) + '/testdata/book1'
+const book1_dest = os.dir(@FILE) + '/testdata/_book1'
 
 fn test_book_new() {
-	mut tree := new()!
+	mut s := spawner.new()
+	mut tree := new(mut s)!
 
 	mut col_examples := tree.collection_new(
 		name: 'examples'
@@ -30,6 +33,7 @@ fn test_book_new() {
 		heal: true
 	)!
 
-	mut book := tree.book_new(name: 'book1', path: knowledgetree.book1_path, dest: '_book1')!
+	mut book := tree.book_new(name: 'book1', path: knowledgetree.book1_path, dest: book1_dest)!
 	book.export()!
+	panic('sh')
 }
