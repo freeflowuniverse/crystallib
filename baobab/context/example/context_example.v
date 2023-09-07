ebf70ba47306b73fba44179401fea0c4244c15d6
despiegk <>
2023-09-07 06:53:39 +0300

    biog refactor

--- baobab/context/example/context_example.v
+++ baobab/context/example/context_example.v
@@ -1,1 +1,25 @@
 
+module main
+
+import freeflowuniverse.crystallib.baobab.context
+
+fn do() ! {
+
+	mut c:=context.new()!
+
+
+	tree.scan(
+		git_root: '~/code6'
+		git_url: 'https://github.com/threefoldfoundation/books/tree/main/content'
+		load: true
+		heal: false
+		git_reset: reset
+	)!
+
+
+	println(h)
+}
+
+fn main() {
+	do() or { panic(err) }
+}
--- baobab/context/knowledgetree.v
+++ baobab/context/knowledgetree.v
@@ -2,12 +2,33 @@ module context
 
 import freeflowuniverse.crystallib.knowledgetree
 
+
+
 pub fn (mut c Context) knowledgetree() !knowledgetree.Tree {
-	if c.knowledgetree == none {
-		c.knowledgetree = knowledgetree.new()!
+	if c.knowledgetree == none{
+		c.knowledgetree = knowledgetree.new(mut c)!	
 	}
-	return c.knowledgetree or { panic(err) }
+	return c.knowledgetree or {panic(err)}
 }
 
+
+
 fn (mut c Context) knowledgetree_init(mut actions Actions, action Action) ! {
+
+
+	if action.name == "init"{
+
+		filter:=action.params.get_string_default(filter,"")!
+		root:=action.params.get_string_default(root,"")!
+		pull:=action.params.get_default_false(pull)!
+		reset:=action.params.get_default_false(reset)!
+		light:=action.params.get_default_true(light)!
+		log:=action.params.get_default_false(log)!
+
+		c.gitstructure:=gittools.new(filter:filter,root:root,pull:pull,reset:reset,light:light,log:log)!
+
+	}
+
+
 }
+
