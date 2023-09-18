e3f18994ce1a17789f1683dac3758b74ffd23727
timurgordon <timurgordon@gmail.com>
2023-09-18 15:51:24 +0200

    fix redisclientcore.core_get by avoinding nested locks

--- redisclient/redisclientcore.v
+++ redisclient/redisclientcore.v
@@ -7,13 +7,16 @@ pub struct RedisURL {
 }
 
 pub fn core_get(url RedisURL) !Redis {
-	lock redis_connections{		
-		println("lock redis")
-		if redis_connections.len==0{
-			new(['${url.address}:${url.port}'])!
+	mut connection_exists := false // if any redis connection exists that can be used
+	lock redis_connections {
+		if redis_connections.len > 0 {
+			connection_exists = true
 		}
 	}
-	println("unlock redis")
+	if !connection_exists {
+		new(['${url.address}:${url.port}'])!
+	}
+	println('unlock redis')
 	mut r := Redis{}
 	return r
 }
