59d90ff75a6a10ffe59305d869f54e4fc2abb8f5
timurgordon <timurgordon@gmail.com>
2023-10-06 15:21:04 +0300

    circles code generation improvements

--- circles/gen/generate_definers_test.v
+++ circles/gen/generate_definers_test.v
@@ -5,9 +5,9 @@ import os
 
 const (
 	circle_model = '${os.dir(@FILE)}/testdata/test_model/model.v'
-	test_model = '${os.dir(@FILE)}/testdata/test_model/organization_model.v'
-	// actor_target   = '${os.dir(os.dir(@FILE))}/actor2.v'
-	// methods_target = '${os.dir(os.dir(@FILE))}/methods2.v'
+	test_model   = '${os.dir(@FILE)}/testdata/test_model/organization_model.v'
+		// actor_target   = '${os.dir(os.dir(@FILE))}/actor2.v'
+		// methods_target = '${os.dir(os.dir(@FILE))}/methods2.v'
 )
 
 fn test_generate_definers() {
