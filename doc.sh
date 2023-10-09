e662b31aa9c77e1b140e3df9eb3754a3d0b47d90
despiegk <>
2023-10-09 10:48:13 +0300

    examples moved

--- doc.sh
+++ doc.sh
@@ -1,13 +1,13 @@
 set -ex
 rm -rf _docs
 rm -rf docs
-v fmt -w src
-#pushd src
-v doc -m -f html src/. -readme -comments -no-timestamp 
-#-o ../
-#popd
+
+cd src
+
+v fmt -w .
+v doc -m -f html . -readme -comments -no-timestamp -o ../
+
+cd ..
 
 mv _docs docs
 open docs/index.html
-
-# open https://threefoldfoundation.github.io/dao_research/liqpool/docs/liquidity.html
