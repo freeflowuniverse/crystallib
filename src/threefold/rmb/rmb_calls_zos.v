0b72d40bf95554f93eea1f67939d0ab874eee24e
despiegk <>
2023-10-09 09:48:29 +0300

    rmb fix

--- src/threefold/rmb/rmb_calls_zos.v
+++ src/threefold/rmb/rmb_calls_zos.v
@@ -5,7 +5,7 @@ import json
 
 // if true the ZOS has a public ip address
 pub fn (mut z RMBClient) zos_has_public_ipaddr(dst u32) !bool {
-	response := z.rmb_client_request('zos.network.public_config_get', dst)!
+	response := z.rmb_request('zos.network.public_config_get', dst, '')!
 	if response.err.message != '' {
 		return false
 	}
@@ -13,7 +13,7 @@ pub fn (mut z RMBClient) zos_has_public_ipaddr(dst u32) !bool {
 }
 
 pub fn (mut z RMBClient) get_zos_system_version(dst u32) !string {
-	response := z.rmb_client_request('zos.system.version', dst)!
+	response := z.rmb_request('zos.system.version', dst, '')!
 	if response.err.message != '' {
 		return error('${response.err.message}')
 	}
@@ -22,7 +22,7 @@ pub fn (mut z RMBClient) get_zos_system_version(dst u32) !string {
 
 // TODO: point to documentation where it explains what this means, what is zos_wg_port and why do we need it
 pub fn (mut z RMBClient) get_zos_wg_ports(dst u32) ![]u16 {
-	response := z.rmb_client_request('zos.network.list_wg_ports', dst)!
+	response := z.rmb_request('zos.network.list_wg_ports', dst, '')!
 	if response.err.message != '' {
 		return error('${response.err.message}')
 	}
--- src/threefold/rmb/rmb_request.v
+++ src/threefold/rmb/rmb_request.v
@@ -5,12 +5,12 @@ import time
 import json
 
 // cmd is e.g.
-pub fn (mut z RMBClient) rmb_request(cmd string, dst u32) !RmbResponse {
+pub fn (mut z RMBClient) rmb_request(cmd string, dst u32, payload string) !RmbResponse {
 	msg := RmbMessage{
 		ver: 1
 		cmd: cmd
 		exp: 5
-		dat: base64.encode_str('')
+		dat: base64.encode_str(payload)
 		dst: [dst]
 		ret: rand.uuid_v4()
 		now: u64(time.now().unix_time())
@@ -18,6 +18,6 @@ pub fn (mut z RMBClient) rmb_request(cmd string, dst u32) !RmbResponse {
 	request := json.encode_pretty(msg)
 	z.redis.lpush('msgbus.system.local', request)!
 	response_json := z.redis.blpop(msg.ret, 5)!
-	response := json.decode(RmbResponse, response_json)!
+	response := json.decode(RmbResponse, response_json[1])!
 	return response
 }
