12f17d18b8321152d96a93f948c77822b95986c1
root <omarabdul3ziz@gmail.com>
2023-10-12 09:17:09 +0000

    update dev_tool example to use add_sshserver, add_package
    Co-authored-by: Omar Abdulaziz <omarabdul3ziz@gmail.com>

--- crystallib/osal/docker/examples/dev_tools.v
+++ crystallib/osal/docker/examples/dev_tools.v
@@ -4,26 +4,15 @@ fn main() {
 
   mut engine := docker.new(prefix: "", localonly: true)!
 
-  mut r := engine.recipe_new(name: 'dev_tools', platform: .ubuntu)
+  mut r := engine.recipe_new(name: 'dev_tools', platform: .alpine)
 
-  r.add_from(image: 'ubuntu', tag: 'latest')!
+  r.add_from(image: 'alpine', tag: 'latest')!
 
-  r.add_run(cmd: 'apt update -y 
-  apt install -y git vim wget openssh-server')!
+  r.add_package(name:"git,vim")!
 
   r.add_zinit()!
 
-  r.add_zinit_cmd(name: "sshd", exec: "/usr/bin/sshd -D")!
+  r.add_sshserver()!
 
-	r.add_zinit_cmd(name: "sshkey", exec:'if [ ! -z "\$SSH_KEY" ]; then
-		mkdir -p /var/run/sshd
-		mkdir -p /root/.ssh
-		touch /root/.ssh/authorized_keys
-		
-		chmod 700 /root/.ssh
-		chmod 600 /root/.ssh/authorized_keys
-
-		echo "\$SSH_KEY" >> /root/.ssh/authorized_keys
-fi')!
   r.build(true)!
 }
