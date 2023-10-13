db95d52122e01c922e991b48b7cae1dac9a3968d
timurgordon <timurgordon@gmail.com>
2023-10-13 19:49:29 +0300

    crpgp example and module  fixes

--- crystallib/algo/crpgp/pgp.v
+++ crystallib/algo/crpgp/pgp.v
@@ -26,7 +26,7 @@ fn (self KeyParams) id() string {
 	return '${self.name} ${comment}<${self.email}>'
 }
 
-pub fn generate_key(k KeyParams) ?SignedSecretKey {
+pub fn generate_key(k KeyParams) !SignedSecretKey {
 	mut primarykey_type := C.KeyType{}
 	mut subkey_type := C.KeyType{}
 	match k.key_type {
@@ -39,39 +39,39 @@ pub fn generate_key(k KeyParams) ?SignedSecretKey {
 			subkey_type = C.KeyType{KeyType_Tag.rsa, k.length}
 		}
 	}
-	builder := new_secret_key_param_builder()?
-	builder.primary_key_id(k.id())?
-	builder.key_type(primarykey_type)?
+	builder := new_secret_key_param_builder()!
+	builder.primary_key_id(k.id())!
+	builder.key_type(primarykey_type)!
 	subkey_builder := new_subkey_params_builder()
-	subkey_builder.key_type(subkey_type)?
-	subkey := subkey_builder.build()?
-	builder.subkey(subkey)?
-	params := builder.build()?
-	sk := params.generate_and_free()?
-	ssk := sk.sign()?
+	subkey_builder.key_type(subkey_type)!
+	subkey := subkey_builder.build()!
+	builder.subkey(subkey)!
+	params := builder.build()!
+	sk := params.generate_and_free()!
+	ssk := sk.sign()!
 	return ssk
 }
 
 // import functions
 // import public key from ASCII armor text format
-pub fn import_publickey(data string) ?SignedPublicKey {
+pub fn import_publickey(data string) !SignedPublicKey {
 	return signed_public_key_from_armored(data)
 }
 
 // import public key from ASCII armor file format
-pub fn import_publickey_from_file(path string) ?SignedPublicKey {
-	content := os.read_file(path)?
+pub fn import_publickey_from_file(path string) !SignedPublicKey {
+	content := os.read_file(path)!
 	return import_publickey(content)
 }
 
 // import secret key from ASCII armor text format
-pub fn import_secretkey(data string) ?SignedSecretKey {
+pub fn import_secretkey(data string) !SignedSecretKey {
 	return signed_secret_key_from_armored(data)
 }
 
 // import secret key from ASCII armor file format
-pub fn import_secretkey_from_file(path string) ?SignedSecretKey {
-	content := os.read_file(path)?
+pub fn import_secretkey_from_file(path string) !SignedSecretKey {
+	content := os.read_file(path)!
 	return import_secretkey(content)
 }
 
@@ -81,33 +81,33 @@ pub fn (spk &SignedPublicKey) verify_signature(sig &Signature, message string) b
 	return true
 }
 
-pub fn (spk &SignedPublicKey) encrypt_from_text(message string) ?[]byte {
+pub fn (spk &SignedPublicKey) encrypt_from_text(message string) ![]byte {
 	return spk.encrypt_with_any(message.bytes())
 }
 
 // Secret Key Fucntions
-pub fn (ssk &SignedSecretKey) sign_message(message string) ?Signature {
+pub fn (ssk &SignedSecretKey) sign_message(message string) !Signature {
 	return ssk.create_signature(message.bytes())
 }
 
-pub fn (ssk &SignedSecretKey) decrypt_to_text(encrypted_message []byte) ?string {
-	return ssk.decrypt(encrypted_message)?.bytestr()
+pub fn (ssk &SignedSecretKey) decrypt_to_text(encrypted_message []byte) !string {
+	return ssk.decrypt(encrypted_message)!.bytestr()
 }
 
-pub fn (ssk &SignedSecretKey) get_signed_public_key() ?SignedPublicKey {
-	return ssk.public_key()?.sign_and_free(ssk)
+pub fn (ssk &SignedSecretKey) get_signed_public_key() !SignedPublicKey {
+	return ssk.public_key()!.sign_and_free(ssk)
 }
 
 // Signature Fucntions
-fn (sig &Signature) to_bytes() ?[]byte {
+fn (sig &Signature) to_bytes() ![]byte {
 	return sig.serialize()
 }
 
-pub fn (sig &Signature) to_hex() ?string {
-	return hex.encode(sig.to_bytes()?)
+pub fn (sig &Signature) to_hex() !string {
+	return hex.encode(sig.to_bytes()!)
 }
 
-pub fn signature_from_hex(sig string) ?Signature {
-	sig_bytes := hex.decode(sig)?
+pub fn signature_from_hex(sig string) !Signature {
+	sig_bytes := hex.decode(sig)!
 	return deserialize_signature(sig_bytes)
 }
