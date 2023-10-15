1f22bc829b323d4667016718075cfbf1efe7d1cc
Omar Abdulaziz <omarabdul3ziz@gmail.com>
2023-10-15 17:29:45 +0300

    integrate griddriver for rmb and other calls
    Co-authored-by: Mario Bassem <mariobassem12@gmail.com>

---
+++ crystallib/threefold/griddriver/rmb.v
@@ -0,0 +1,24 @@
+module griddriver
+
+import os
+import x.json2
+
+pub fn (mut c Client) rmb_call(dst u32, cmd string, payload string) !string {
+	res := os.execute('griddriver rmb --cmd \"${cmd}\" --dst ${dst} --payload \"${payload}\" --substrate \"${c.substrate}\" --mnemonics \"${c.mnemonic}\" --relay \"${c.relay}\"')
+	if res.exit_code != 0 {
+		return error(res.output)
+	}
+	return res.output
+} 
+
+pub struct Version {
+	zinit string
+	zos string
+}
+
+pub fn (mut c Client) get_zos_version() !Version {
+	res := c.rmb_call(21, "zos.system.version", "")! 
+	ver := json2.decode[Version](res)!
+	return ver
+}
+
