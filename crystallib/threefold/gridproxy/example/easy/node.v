1cd67eeb7b5c2cc52443c01f218efdbfcbc8561b
kassem <omarksm09@gmail.com>
2023-10-15 14:14:59 +0300

    add node related examples

---
+++ crystallib/threefold/gridproxy/example/easy/node.v
@@ -0,0 +1,60 @@
+import freeflowuniverse.crystallib.threefold.gridproxy
+import freeflowuniverse.crystallib.threefold.gridproxy.model
+import json 
+fn main(){
+
+	// init gridproxy client on devnet with redis cash
+	mut gp_client := gridproxy.get(.dev, true)!
+
+	mut node_filter := model.NodeFilter{}
+
+	node_filter.status = "up"
+	node_filter.country = "egypt"
+	// node_filter.free_ips = u64(4)
+	// node_filter.dedicated= true
+	// node_filter.free_sru = u64(9000) //bytes
+
+	nodes := gp_client.get_nodes(node_filter)!
+	// println(nodes)
+
+	
+	node := nodes[0] or { gp_client.get_node_by_id(11)!}
+	node_id := node.node_id
+
+	// get node available resources
+	node_available_resources :=  node.calc_available_resources()
+	// println(node_available_resources)
+	
+	// get node status
+	node_stats := gp_client.get_node_stats_by_id(node_id)!
+	// println(node_stats)
+  
+
+
+	// using resource filter
+
+	resources := model.ResourceFilter{
+		free_sru_gb: 0
+		free_hru_gb:0
+		free_mru_gb: 0
+		free_ips:0
+	}
+
+	nodes_has_resources := gp_client.get_nodes_has_resources(resources)
+	
+	// this returns all nodes that have the provided resources
+	all_nodes_with_resources := nodes_has_resources.get_func()!
+	// println(all_nodes_with_resources)
+
+	// you can maintain the returned filter 
+	mut has_resources_filter := nodes_has_resources.filter
+	// to get the available nodes (Up), that have the provided resources
+	has_resources_filter.status ="up"
+	available_nodes :=  gp_client.get_nodes(has_resources_filter)!
+	// println(available_nodes)
+
+	return
+
+	
+
+}
