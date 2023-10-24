6e035e92a4d65f6b5f95381d0f75a8ab8e620aec
timurgordon <timurgordon@gmail.com>
2023-10-24 11:56:40 +0300

    get_file function fix creating parent dirs with create flag

--- crystallib/core/pathlib/factory.v
+++ crystallib/core/pathlib/factory.v
@@ -4,7 +4,7 @@ import os
 
 // gets Path object, will check if it exists, is dir_file, ...
 pub fn get(path_ string) Path {
-	mut p2:=get_no_check(path_)
+	mut p2 := get_no_check(path_)
 	p2.check()
 	return p2
 }
@@ -27,26 +27,26 @@ pub fn get_no_check(path_ string) Path {
 }
 
 [params]
-pub struct GetArgs{
+pub struct GetArgs {
 pub mut:
-	path string
+	path   string
 	create bool
-	check bool = true //means will check the dir or file exists
-	empty bool //will empty the dir or the file
+	check  bool = true // means will check the dir or file exists
+	empty  bool // will empty the dir or the file
 }
 
 // get a directory, or needs to be created
 // if the dir doesn't exist and is not created, then there will be an error
 pub fn get_dir(args_ GetArgs) !Path {
-	mut args:=args_
-	if args.empty{
-		args.create=true
-	}		
-	if args.create{
-		args.check=true
+	mut args := args_
+	if args.empty {
+		args.create = true
+	}
+	if args.create {
+		args.check = true
 	}
 	mut p2 := get_no_check(args.path)
-	if args.check{
+	if args.check {
 		p2.check()
 		p2.absolute()
 		if p2.exist == .no {
@@ -56,7 +56,7 @@ pub fn get_dir(args_ GetArgs) !Path {
 		if !p2.is_dir() {
 			return error('Path ${args.path} is not a dir.')
 		}
-		if args.empty{
+		if args.empty {
 			p2.empty()!
 		}
 	}
@@ -64,29 +64,32 @@ pub fn get_dir(args_ GetArgs) !Path {
 }
 
 pub fn get_file(args_ GetArgs) !Path {
-	mut args:=args_
-	if args.empty{
-		args.create=true
-	}		
-	if args.create{
-		args.check=true
+	mut args := args_
+	if args.empty {
+		args.create = true
+	}
+	if args.create {
+		args.check = true
 	}
 	mut p2 := get_no_check(args.path)
-	if args.check{
+	if args.check {
 		p2.check()
-		if args.create{
-			parent_ := p2.parent()!
+		if args.create {
+			mut parent_ := p2.parent()!
+			parent_.check()
 			if parent_.exist == .no {
 				os.mkdir_all(parent_.path) or { return error('cannot create path:${args.path}') }
 			}
-			if p2.exist == .no ||  args.empty{
-				os.write_file(args.path, '') or { return error('cannot create empty file:${args.path}') }
+			if p2.exist == .no || args.empty {
+				os.write_file(args.path, '') or {
+					return error('cannot create empty file:${args.path} ${err}')
+				}
 				p2.check()
 			}
 		}
 		if !p2.is_file() {
 			return error('Path ${args.path} is not a file.')
-		}		
+		}
 	}
 	return p2
 }
