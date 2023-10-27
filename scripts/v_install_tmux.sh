83f04b6e4b8498514df282f4ede2cd34bc1ebacf
despiegk <>
2023-10-27 07:56:31 +0300

    ...

--- scripts/v_install_tmux.sh
+++ scripts/v_install_tmux.sh
@@ -15,6 +15,26 @@ if [ -z "$TERM" ]; then
     export TERM=xterm
 fi
 
+#!/bin/bash
+
+# Check if the /etc/os-release file exists
+if [ -e /etc/os-release ]; then
+    # Read the ID field from the /etc/os-release file
+    os_id=$(grep '^ID=' /etc/os-release | cut -d= -f2)
+    
+    # Check if the ID is "ubuntu" (case-insensitive)
+    if [ "${os_id,,}" == "ubuntu" ]; then
+        echo "This system is running Ubuntu."
+    else
+        echo "This system is not running Ubuntu."
+        exit 1
+    fi
+else
+    echo "The /etc/os-release file does not exist. Unable to determine the operating system."
+    exit 1
+fi
+
+
 function os_package_install {
     apt -o Dpkg::Options::="--force-confold" -o Dpkg::Options::="--force-confdef" install $1 -q -y --allow-downgrades --allow-remove-essential 
 }
