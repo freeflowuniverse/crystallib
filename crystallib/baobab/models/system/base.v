ccae755da6a6866f83ac5e9b9dcfe57c518a14e3
mariobassem <mariobassem12@gmail.com>
2023-11-02 13:54:37 +0200

    add/fix tests for models/system
    Signed-off-by: mariobassem <mariobassem12@gmail.com>

--- crystallib/baobab/models/system/base.v
+++ crystallib/baobab/models/system/base.v
@@ -8,16 +8,16 @@ import freeflowuniverse.crystallib.baobab.smartid
 [heap]
 pub struct Base {
 pub mut:
-	gid     smartid.GID
-	params      paramsparser.Params
-	version_base u8 = 1 //so we know how to do the serialize or unserialize
+	gid                smartid.GID
+	params             paramsparser.Params
+	version_base       u8 = 1 // so we know how to do the serialize or unserialize
 	serialization_type SerializationType
-	name string
-	description string
-	remarks     Remarks
+	name               string
+	description        string
+	remarks            Remarks
 }
 
-pub enum SerializationType{
+pub enum SerializationType {
 	bin
 	json
 	tscript
@@ -27,11 +27,11 @@ pub fn (mut o Base) json() string {
 	return json.encode(o)
 }
 
-pub fn serializationtype_u8(t SerializationType) u8{
+pub fn serializationtype_u8(t SerializationType) u8 {
 	match t {
-		.bin {return 0 }
-		.json {return 1 }
-		.tscript {return 2 }
+		.bin { return 0 }
+		.json { return 1 }
+		.tscript { return 2 }
 	}
 	return 255
 }
@@ -40,7 +40,7 @@ pub fn (mut o Base) params_set(text string) ! {
 	o.params = paramsparser.new(text)!
 }
 
-//will merge the params
+// will merge the params
 pub fn (mut o Base) params_add(text string) ! {
 	o.params.merge(text)!
 }
@@ -49,7 +49,7 @@ pub fn (mut o Base) params_add(text string) ! {
 pub fn (o Base) bin_encoder() !encoder.Encoder {
 	mut b := encoder.new()
 	b.add_u8(o.version_base) // remember which version this is	
-	b.add_u32(o.gid.oid()) // remember which version this is
+	b.add_string(o.gid.str())
 	b.add_string(o.params.str())
 	b.add_string(o.name)
 	b.add_string(o.description)
@@ -58,17 +58,16 @@ pub fn (o Base) bin_encoder() !encoder.Encoder {
 	return b
 }
 
-
-pub fn base_decoder(data []u8) !(encoder.Decoder,Base) {
-	mut o:=Base{}
-	mut d := encoder.decoder_new(data) 
-	assert d.get_u8()==1
-	o.gid=smartid.GID{id:d.get_u32()}
-	o.params=paramsparser.new(d.get_string())!
-	o.name=d.get_string()
-	o.description=d.get_string()
-	remarksbytes:=d.get_bytes()
+pub fn base_decoder(data []u8) !(encoder.Decoder, Base) {
+	mut o := Base{}
+	mut d := encoder.decoder_new(data)
+	assert d.get_u8() == 1
+	o.gid = smartid.gid(gid_str: d.get_string())!
+	o.params = paramsparser.new(d.get_string())!
+	o.name = d.get_string()
+	o.description = d.get_string()
+	remarksbytes := d.get_bytes()
 	o.remarks = remarks_unserialize_binary(remarksbytes)!
-	o.serialization_type = .bin	
-	return d,o
+	o.serialization_type = .bin
+	return d, o
 }
