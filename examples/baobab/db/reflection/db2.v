ddf43f45df0c096ff709fd275fbcfe719e3d7c2c
timurgordon <timurgordon@gmail.com>
2023-11-06 15:03:31 +0300

    complete reflection example for db

--- examples/baobab/db/reflection/db2.v
+++ examples/baobab/db/reflection/db2.v
@@ -1,7 +1,8 @@
 module main
 
-import mystruct
+import mystruct {MyStruct}
 import dbreflection
+import freeflowuniverse.crystallib.baobab.db
 import freeflowuniverse.crystallib.data.ourtime
 import time
 
@@ -9,11 +10,12 @@ fn do() ! {
 	mydb := dbreflection.db_new(circlename: 'testcircle')!
 	println(mydb)
 
-	mydb.delete_all() or { println('failed to delete: ${err}') }
+	mydb.delete_all()!
 
-	mydb.find() or { println('failed to query: ${err}') }
+	objs_start := mydb.find[MyStruct](dbreflection.FindArgs[MyStruct]{})!
+	assert objs_start.len == 0
 
-	mut o1 := mystruct.MyStruct{
+	mut o1 := MyStruct{
 		gid: mydb.cid.gid()!
 		name: 'my name'
 		nr: 2
@@ -22,28 +24,35 @@ fn do() ! {
 		nr2: 10
 		listu32: [u32(2), u32(3), u32(4)]
 	}
-	mydb.set[mystruct.MyStruct](o1)!
+	mydb.set[MyStruct](o1)!
 
-	// objs_4 := mydb.find()!
-	// assert objs_4.len == 1
+	objs_4 := mydb.find[MyStruct](dbreflection.FindArgs[MyStruct]{})!
+	assert objs_4.len == 1
 
-	// mut o2 := mystruct.MyStruct{
-	// 	gid: mydb.cid.gid()!
-	// 	name: 'my second name'
-	// 	nr: 2
-	// 	color: 'blue'
-	// 	nr2: 11
-	// 	listu32: [u32(5), u32(6), u32(7)]
-	// }
-	// mydb.set(o2)!
+	mut o2 := mydb.new[MyStruct](
+		object: MyStruct {
+			name: 'my second name'
+			nr: 2
+			color: 'blue'
+			nr2: 11
+			listu32: [u32(5), u32(6), u32(7)]
+		}
+		
+	)!
+	mydb.set(o2)!
 
-	// objs_5 := mydb.find()!
-	// assert objs_5.len == 2
+	objs_5 := mydb.find[MyStruct](dbreflection.FindArgs[MyStruct]{})!
+	assert objs_5.len == 2
 
-	// o3 := mydb.get(o1.gid)!
-	// o1data := mydb.serialize(o1)!
-	// o3data := mydb.serialize(o3)!
-	// assert o1data == o3data
+	o3 := mydb.get[MyStruct](o1.gid)!
+	println(o1)
+	println(o3)
+	o1data := mydb.serialize(o1)!
+	o3data := mydb.serialize(o3)!
+	assert o1data == o3data
+
+	script3:=mydb.serialize_3script(o1)!
+	println(script3)
 
 	// objs := mydb.find(
 	// 	nr: 1
@@ -68,6 +77,7 @@ fn do() ! {
 	mydb.delete_all()!
 }
 
+
 fn perf_write(mydb dbreflection.MyDB) ! {
 	now := time.now()
 	for i in 0 .. 1000 {
@@ -87,7 +97,7 @@ fn perf_write(mydb dbreflection.MyDB) ! {
 
 fn perf_find(mydb dbreflection.MyDB) ! {
 	now := time.now()
-	mydb.find(nr: 1)!
+	mydb.find[MyStruct](object: MyStruct{nr: 1})!
 	diff := time.since(now)
 	println('querying 1000 objects took ${diff.seconds()} seconds.\n')
 }
--- examples/baobab/db/reflection/dbreflection/db_reflection.v
+++ examples/baobab/db/reflection/dbreflection/db_reflection.v
@@ -3,6 +3,7 @@ module dbreflection
 import mystruct {MyStruct}
 import freeflowuniverse.crystallib.baobab.db
 import freeflowuniverse.crystallib.baobab.smartid
+import freeflowuniverse.crystallib.data.ourtime
 import freeflowuniverse.crystallib.algo.encoder
 import freeflowuniverse.crystallib.data.paramsparser
 
@@ -26,6 +27,30 @@ pub fn db_new(args DBArgs) !MyDB {
 	}
 	mydb.init()!
 	return mydb
+}
+
+[params]
+pub struct NewArgs[T] {
+pub mut:
+	params             string
+	name               string
+	description        string
+	mtime              string// modification time
+	ctime              string // creation time
+	object T
+}
+
+pub fn (mydb MyDB) new[T](args NewArgs[T]) !T {
+	base:=mydb.new_base(
+		params:args.params
+		name:args.name
+		description:args.description
+		mtime:args.mtime
+		ctime:args.ctime
+	)!
+	mut o:=args.object{}
+	o.Base = base
+	return o
 }
 
 pub fn (mydb MyDB) set[T](object T) ! {
@@ -53,19 +78,20 @@ pub fn (mydb MyDB) set[T](object T) ! {
 	)!
 }
 
-pub fn (mydb MyDB) get(gid smartid.GID) !MyStruct {
+pub fn (mydb MyDB) get[T](gid smartid.GID) !T {
 	data := mydb.get_data(gid)!
-	return mydb.unserialize(data)
+	return mydb.unserialize[T](data)
 }
 
 [params]
-pub struct FindArgs {
+pub struct FindArgs[T] {
 	db.BaseFindArgs
 pub mut:
-	name  string
-	color string
-	nr    int
-	nr2   int
+	object T
+	mtime_from ?ourtime.OurTime
+	mtime_to   ?ourtime.OurTime
+	ctime_from ?ourtime.OurTime
+	ctime_to   ?ourtime.OurTime
 }
 
 //find on our database
@@ -74,27 +100,33 @@ pub mut:
 // color string
 // nr    int
 // nr2   int
-// mtime_from ?ourtime.OurTime
-// mtime_to   ?ourtime.OurTime
-// ctime_from ?ourtime.OurTime
-// ctime_to   ?ourtime.OurTime
+
 // name       string
 //```
-pub fn (mydb MyDB) find(args FindArgs) ![]MyStruct {	
+pub fn (mydb MyDB) find[T](args FindArgs[T]) ![]T {	
+	mut query_int := map[string]int
+	mut query_string := map[string]string
+
+	$for field in T.fields {
+		if field.attrs.contains('index') {
+			$if field.typ is int {
+				query_int[field.name] = args.object.$(field.name)
+			}
+			$if field.typ is string {
+				query_string[field.name] = args.object.$(field.name)
+			}
+		}
+	}
+	
 	dbfindoargs:=db.DBFindArgs{
-				query_int: {
-					'nr':  args.nr
-					'nr2': args.nr2
-				}
-				query_string: {
-					'name':  args.name
-					'color': args.color
-				}	
-			}	
+		query_int: query_int
+		query_string: query_string
+	}
+
 	data := mydb.find_base(args.BaseFindArgs,dbfindoargs)!
-	mut read_o := []MyStruct{}
+	mut read_o := []T{}
 	for d in data {
-		read_o << mydb.unserialize(d)!
+		read_o << mydb.unserialize[T](d)!
 	}
 	return read_o
 }
@@ -107,7 +139,6 @@ pub fn (mydb MyDB) serialize[T](obj T) ![]u8 {
 	$for field in T.fields {
 		$if field.typ is db.Base {
 			enc = obj.$(field.name).bin_encoder()!
-			println('eureka: ${enc}')
 		}
 	}
 
@@ -119,59 +150,90 @@ pub fn (mydb MyDB) serialize[T](obj T) ![]u8 {
 			enc.add_string(obj.$(field.name))
 		}
 		$if field.typ is []u32 {
-			enc.add_list_u32(obj.listu32)
+			enc.add_list_u32(obj.$(field.name))
 		}
 	}
 	return enc.data
 }
 
 // serialize to 3script
-pub fn (mydb MyDB) serialize_kwargs(o MyStruct) map[string]string {
-	mut kwargs := o.Base.serialize_kwargs()
-	kwargs["nr"]="${o.nr}"
-	kwargs["nr2"]="${o.nr2}"
-	kwargs["color"]=o.color
-	mut listu32:=""
-	for i in listu32{
-		listu32+="${i},"
+pub fn (mydb MyDB) serialize_kwargs[T](obj T) map[string]string {
+	mut kwargs := obj.Base.serialize_kwargs()
+	
+	$for field in T.fields {
+		$if field.typ is int {
+			val_int := obj.$(field.name)
+			kwargs[field.name]="${val_int}"
+		}
+		$if field.typ is string {
+			kwargs[field.name]=obj.$(field.name)
+		}
+		$if field.typ is []u32 {
+			mut listu32:=""
+			for i in obj.$(field.name){
+				listu32+="${i},"
+			}
+			listu32=listu32.trim_string_right(",")
+			kwargs["listu32"]=listu32
+		}
 	}
-	listu32=listu32.trim_string_right(",")
-	kwargs["listu32"]=listu32
 	return kwargs
 }
 
 
 // this is the method to load binary form
-pub fn (mydb MyDB) unserialize(data []u8) !MyStruct {
+pub fn (mydb MyDB) unserialize[T](data []u8) !T {
 	// mut d := encoder.decoder_new(data)
-	mut d, base := db.base_decoder(data)!
-	mut o := MyStruct{
+	
+	mut dec, base := db.base_decoder(data)!
+	mut obj := T{
 		Base: base
 	}
-	o.nr = d.get_int()
-	o.color = d.get_string()
-	o.nr2 = d.get_int()
-	o.listu32 = d.get_list_u32()
-	return o
+	$for field in T.fields {
+		$if field.typ is db.Base {
+			// base already decoded above
+		}
+		$if field.typ is int {
+			obj.$(field.name) = dec.get_int()
+		}
+		$if field.typ is string {
+			obj.$(field.name) = dec.get_string()
+		}
+		$if field.typ is []u32 {
+			obj.$(field.name) = dec.get_list_u32()
+		}
+	}
+	return obj
 }
 
 
 // serialize to 3script
-pub fn (mydb MyDB) serialize_3script(o MyStruct) !string {
-	p:=paramsparser.new_from_dict(mydb.serialize_kwargs(o))!
+pub fn (mydb MyDB) serialize_3script[T](obj T) !string {
+	p:=paramsparser.new_from_dict(mydb.serialize_kwargs[T](obj))!
 	return p.export(pre:"!!${mydb.objtype}.define ")
 }
 
 
-pub fn (mydb MyDB) unserialize_3script(txt string) ![]MyStruct {
-	mut res:=[]MyStruct{}
+pub fn (mydb MyDB) unserialize_3script[T](txt string) ![]T {
+	mut res:=[]T{}
 	for r in mydb.base_decoder_3script(txt)!{
-		mut o := MyStruct{Base: r.base}
+		mut o := T{Base: r.base}
 		p:=r.params
-		o.nr = p.get_int_default("nr",0)!
-		o.color = p.get_default("color","")!
-		o.nr2 = p.get_int_default("nr",0)!
-		o.listu32 = p.get_list_u32("listu32")!
+
+		$for field in T.fields {
+			$if field.typ is db.Base {
+				// base already decoded above
+			}
+			$if field.typ is int {
+				obj.$(field.name) = p.get_int_default(field.name,0)!
+			}
+			$if field.typ is string {
+				obj.$(field.name) = p.get_default(field.name,"")!
+			}
+			$if field.typ is []u32 {
+				obj.$(field.name) = dec.get_list_u32(field.name)!
+			}
+		}
 		res<<o
 	}
 	return res
