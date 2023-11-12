22222eb2b19e3b271ff3e5372577bda9e180e783
despiegk <kristof@incubaid.com>
2023-11-12 07:19:46 +0300

    generator for markdown

--- crystallib/data/markdownparser/generator.v
+++ crystallib/data/markdownparser/generator.v
@@ -1,6 +1,7 @@
 module main
 
 import freeflowuniverse.crystallib.core.pathlib
+import freeflowuniverse.crystallib.core.texttools
 
 const templatespath = os.dir(@FILE) + '/templates'
 const outpath = os.dir(@FILE) + '/elements'
@@ -13,9 +14,14 @@ struct ElementCat{
 
 fn new(args_ ElementCat)ElementCat{
 	mut args:=args_
+	args.name = texttools.name_fix(args.name)
 	if args.classname==""{
-
-	}
+		args.classname=args.name[0].to_upper()
+		args.classname+=args.name[1..]
+		if args.classname.contains("_"){
+			panic("Cannot have _ name if classname not specified.")
+		}
+	}	
 }
 
 
@@ -26,9 +32,23 @@ fn do() ! {
 	elements<<new(name:"html")
 
 
-	content:=$templ("${templatespath}/generated.v")
-	mut outpath:=pathlib.get_file(path:"${outpath}/generated.v",create:true)!
+	// e.g.	type DocElement = Action | CodeBlock | Text | None
+	mut elementtypes:=""
+	for element in elements{
+		elementtypes+= "${element.classname} | "
+	}
+	elementtypes=elementtypes.trim_right(" |")
+
+
+	content:=$tmpl("${templatespath}/generated.vtemplate")
+	mut outpath:=pathlib.get_file(path:"${outpath}/generated.",create:true)!
 	outpath.write(content)!
+
+	for element in elements{
+		content2:=$tmpl("${templatespath}/element_x.vtemplate")
+		mut outpath2:=pathlib.get_file(path:"${outpath}/element_${element.name}.v",create:true)!
+		outpath2.write(content2)!		
+	}
 
 }
 
