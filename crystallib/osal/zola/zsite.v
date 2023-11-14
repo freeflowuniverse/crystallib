b9ee9e8ca5cd1d071c8befdb74db9531c17059d6
timurgordon <timurgordon@gmail.com>
2023-11-14 10:38:55 +0300

    add zola generation function

--- crystallib/osal/zola/zsite.v
+++ crystallib/osal/zola/zsite.v
@@ -4,54 +4,68 @@ module zola
 // import os
 // import freeflowuniverse.crystallib.osal.gittools
 import freeflowuniverse.crystallib.core.pathlib
+import os
 
 [heap]
 pub struct ZSite {
 pub mut:
-	sites &Zola [skip; str: skip]
-	name string
-	path_src pathlib.Path
+	sites       &Zola             [skip; str: skip]
+	url         string // url of site repo
+	name        string
+	path_src    pathlib.Path
 	path_export pathlib.Path
 	collections []ZSiteCollection
-	gitrepokey string	
+	gitrepokey  string
 }
-
 
 [params]
 pub struct ZSiteArgs {
 pub mut:
-	name string [required]
-	url string [required] //url of the summary.md file
+	name  string [required]
+	url   string [required] // url of the summary.md file
+	reset bool
+	pull  bool
 }
 
+pub fn (mut sites Zola) site_new(args ZSiteArgs) !&ZSite {
+	path_src := '/tmp/zolas_builder/${args.name}' // where builds happen
+	path_export := '${sites.path.path}/${args.name}'
 
-pub fn (mut sites Zola) site_new(args ZSiteArgs)!&ZSite{
+	mut gs := sites.gitstructure
+	mut locator := gs.locator_new(args.url)!
+	mut repo := gs.repo_get(locator: locator, reset: args.reset, pull: args.pull)!
+	sites.gitrepos[repo.key()] = repo
+	mut path_site_dir := locator.path_on_fs()!
 
-	path_src:="/tmp/zolas_builder/${args.name}" //where builds happen
-	path_export:="${sites.path.path}/${args.name}"
+	os.mkdir_all('/tmp/mdbooks_build/${args.name}/src')!
 
-	mut site:=ZSite{
-		name:args.name
-		path_src:pathlib.get_dir(path:path_src,create:true)!
-		path_export:pathlib.get_dir(path:path_export,create:true)!
-		sites:&sites
+	mut site := ZSite{
+		name: args.name
+		url: args.url
+		path_src: path_site_dir
+		path_export: pathlib.get_dir(path: path_export, create: true)!
+		sites: &sites
+		gitrepokey: repo.key()
 	}
-
-	sites.sites<<&site
+	sites.sites << &site
 
 	return &site
 }
 
-
-pub fn (mut b ZSite) collection_add(args_ ZSiteCollectionArgs)!{
-	mut args:=args_
-	mut c:=ZSiteCollection{url:args.url,name:args.name,site:&b}
+pub fn (mut b ZSite) collection_add(args_ ZSiteCollectionArgs) ! {
+	mut args := args_
+	mut c := ZSiteCollection{
+		url: args.url
+		name: args.name
+		site: &b
+	}
 	c.get()!
 	b.collections << c
 }
 
-
-pub fn (mut b ZSite) generate()!{
-	println (" - site generate: ${b.name} on ${b.path_src}")
+pub fn (mut b ZSite) generate() ! {
+	println(' - site generate: ${b.name} on ${b.path_src}')
+	println('bash ${b.path_src.path}/build.sh')
+	os.execute('bash ${b.path_src.path}/build.sh')
+	os.mv('${b.path_src.path}/public', b.path_export.path)!
 }
-
