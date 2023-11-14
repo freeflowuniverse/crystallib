00cb36ddf8176981617dc85c2c42f9e38b48b8c5
timurgordon <timurgordon@gmail.com>
2023-11-14 09:17:13 +0300

    fmt

--- crystallib/osal/mdbook/mdbookcollection.v
+++ crystallib/osal/mdbook/mdbookcollection.v
@@ -5,50 +5,49 @@ module mdbook
 // import log
 // import os
 
-
 pub struct MDBookCollection {
 pub mut:
-	book &MDBook [skip; str: skip]
-	name string
-	url string
-	reset bool
-	pull bool
+	book       &MDBook [skip; str: skip]
+	name       string
+	url        string
+	reset      bool
+	pull       bool
 	gitrepokey string
 }
 
 [params]
 pub struct MDBookCollectionArgs {
 pub mut:
-	name string
-	url string
+	name  string
+	url   string
 	reset bool
-	pull bool
+	pull  bool
 }
 
-pub fn (mut book MDBook) collection_add(args_ MDBookCollectionArgs)!{
-	mut args:=args_
-	mut c:=MDBookCollection{url:args.url,name:args.name,book:&book}
+pub fn (mut book MDBook) collection_add(args_ MDBookCollectionArgs) ! {
+	mut args := args_
+	mut c := MDBookCollection{
+		url: args.url
+		name: args.name
+		book: &book
+	}
 	c.get()!
 	book.collections << c
 }
 
-
-pub fn (mut self MDBookCollection) get()!{
-
-	println(" - mdbook collection get: $self.url")
-	mut gs:=self.book.books.gitstructure
+pub fn (mut self MDBookCollection) get() ! {
+	println(' - mdbook collection get: ${self.url}')
+	mut gs := self.book.books.gitstructure
 	mut locator := gs.locator_new(self.url)!
-	mut repo := gs.repo_get(locator: locator,reset:self.reset,pull:self.pull)!
+	mut repo := gs.repo_get(locator: locator, reset: self.reset, pull: self.pull)!
 	self.book.books.gitrepos[repo.key()] = repo
 	self.gitrepokey = repo.key()
-	
-	mut srcpath:=locator.path_on_fs()!
 
-	srcpath.link("${self.book.path_build.path}/src/${self.name}",true)!
+	mut srcpath := locator.path_on_fs()!
+	println('debugzo: ${srcpath}')
+	srcpath.link('${self.book.path_build.path}/src/${self.name}', true)!
 
 	// println(srcpath)
 
-	println(" - mdbook collection: $srcpath")
-
-
+	println(' - mdbook collection: ${srcpath}')
 }
