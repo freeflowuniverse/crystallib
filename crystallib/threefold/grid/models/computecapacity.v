c866e5bab2c9661a08d7291d7a9225e11770a773
despiegk <kristof@incubaid.com>
2023-11-15 16:48:38 +0300

    Merge branch 'development_db' into development-griddriver-test
    * development_db: (107 commits)
      installer quick fix
      dontknow
      dontknow
      dontknow
      remove a binary
      openapi code generation wip
      update example
      Make code block with "```"
      add zola generation function
      zola import & module name fix
      fmt
      better error reporting
      pathlib improvements in linking and relative paths
      openapi
      fix project models
      dontknow
      dontknow
      ...
      ...
      zola add
      ...

--- crystallib/threefold/grid/models/computecapacity.v
+++ crystallib/threefold/grid/models/computecapacity.v
@@ -3,7 +3,7 @@ module models
 pub struct ComputeCapacity {
 pub mut:
 	// cpu cores, minimal 10 cpu_centi_core
-	// always reserved with overprovisioning of about 1/4-1/6
+	// always reserved with overprovisioning of about 1/4-1/6 //TODO: what does it mean
 	cpu u8
 	// memory in bytes, minimal 100 MB
 	// always reserved
