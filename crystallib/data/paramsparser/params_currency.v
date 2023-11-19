dd150388b38614fe1b0caf8f32b77fe1a57fe3ab
mario <mariobassem12@gmail.com>
2023-11-19 10:52:23 +0200

    fixes in params_currency methods and tests
    Signed-off-by: mario <mariobassem12@gmail.com>

--- crystallib/data/paramsparser/params_currency.v
+++ crystallib/data/paramsparser/params_currency.v
@@ -1,42 +1,41 @@
 module paramsparser
 
-// import freeflowuniverse.crystallib.data.currency
-// import freeflowuniverse.crystallib.core.texttools
-// import os
+import freeflowuniverse.crystallib.data.currency
 
 // TODO: fix if necessary
-// // see currency object, gets it from params
-// pub fn (params &Params) get_currencyamount(mut cs currency.Currencies, key string) !currency.Amount {
-// 	valuestr := params.get(key)!
-// 	return cs.amount_get(valuestr)!
-// }
+// see currency object, gets it from params
+pub fn (params &Params) get_currencyamount(key string) !currency.Amount {
+	valuestr := params.get(key)!
+	return currency.amount_get(valuestr)!
+}
 
-// pub fn (params &Params) get_currencyamount_default(mut cs currency.Currencies, key string, defval string) !currency.Amount {
-// 	if params.exists(key) {
-// 		return params.get_currencyamount(mut cs, key)!
-// 	}
-// 	return cs.amount_get(defval)!
-// }
+pub fn (params &Params) get_currencyamount_default(key string, defval string) !currency.Amount {
+	if params.exists(key) {
+		return params.get_currencyamount(key)!
+	}
+	return currency.amount_get(defval)!
+}
 
-// // get currency expressed in float in line to currency passed
-// pub fn (params &Params) get_currencyfloat(mut cs currency.Currencies, key string) !f64 {
-// 	valuestr := params.get(key)!
-// 	a := cs.amount_get(valuestr)!
-// 	return a.val
-// }
+// get currency expressed in float in line to currency passed
+pub fn (params &Params) get_currencyfloat(key string) !f64 {
+	valuestr := params.get(key)!
+	a := currency.amount_get(valuestr)!
+	return a.val
+}
 
-// pub fn (params &Params) get_currencyfloat_default(mut cs currency.Currencies, key string, defval f64) !f64 {
-// 	if params.exists(key) {
-// 		return params.get_currencyfloat(mut cs, key)!
-// 	}
-// 	return defval
-// }
+pub fn (params &Params) get_currencyfloat_default(key string, defval f64) !f64 {
+	if params.exists(key) {
+		return params.get_currencyfloat(key)!
+	}
+	return defval
+}
 
-// fn (mut cs Currencies) default_set(cur string, usdval f64) {
+// TODO: this probably does not belong here
+// fn (mut cs Currency) default_set(cur string, usdval f64) {
 // 	cur2 := cur.trim_space().to_upper()
 // 	mut c1 := Currency{
 // 		name: cur2
 // 		usdval: usdval
 // 	}
-// 	cs.currencies[cur2] = &c1
+// 	cs.currency[cur2] = &c1
 // }
