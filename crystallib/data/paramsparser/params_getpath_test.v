471dd668d23dd25c38865b269f78ace6205284b9
mario <mariobassem12@gmail.com>
2023-11-20 11:29:22 +0200

    tests fixes
    Signed-off-by: mario <mariobassem12@gmail.com>

--- crystallib/data/paramsparser/params_getpath_test.v
+++ crystallib/data/paramsparser/params_getpath_test.v
@@ -3,27 +3,30 @@ module paramsparser
 import os
 
 fn test_get_path() {
-	os.open_file('/tmp/f1', 'wr', 0, 6, 4, 4)!
+	os.create('/tmp/f1')!
 
 	text := '
-		key1: val1
+		key1: v1
 		key2: /tmp/f1
 	'
 
 	params := new(text)!
 
-	params.get_path('key1') or { assert true }
+	if _ := params.get_path('key1') {
+		assert false
+	} else {
+		assert true
+	}
 	assert params.get_path('key2')! == '/tmp/f1'
 }
 
 fn test_get_path_create() {
 	text := '
-		key1: val1
+		key1: val2
 		key2: /tmp/f2
 	'
 
 	params := new(text)!
 
-	params.get_path_create('key1') or { assert true }
 	assert params.get_path_create('key2')! == '/tmp/f2'
 }
