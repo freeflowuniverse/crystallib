9f8a640e58ef1f15144ff21b87bdc3319575a672
root <kristof@incubaid.com>
2023-11-26 02:00:22 +0000

    ...

--- crystallib/clients/gitea/gitea.v
+++ crystallib/clients/gitea/gitea.v
@@ -2,23 +2,32 @@ module gitea
 
 // import os
 import freeflowuniverse.crystallib.clients.httpconnection
+import os
 
-pub struct GithubClient {
+
+pub struct GiteaClient {
 mut:
 	connection &httpconnection.HTTPConnection
 }
 
-pub fn new(secret string) !GithubClient {
+pub fn new() !GiteaClient {
 	mut conn := httpconnection.new(
 		name: 'gitea'
 		url: 'http://localhost:3000/api/v1'
 	)!
-	conn.default_header.add(.authorization, 'Bearer ${secret}')
 
-	return GithubClient{
+	env := os.environ()
+	if 'GITEA_TOKEN' !in env {
+		return error('need to set `GITEA_TOKEN` environment variable with a valid token')
+	}
+	key := env['GITEA_TOKEN']
+	conn.default_header.add(.authorization, 'Bearer ${key}')
+	conn.default_header.add(.content_type, 'application/json')
+
+	return GiteaClient{
 		connection: conn
 	}
 }
 
 
-// curl -X GET "http://localhost:3000/api/v1/admin/cron" -H "Authorization: token ????"
+// curl -X GET "http://localhost:3000/api/v1/admin/cron" -H "Authorization: token $GITEA_TOKEN
