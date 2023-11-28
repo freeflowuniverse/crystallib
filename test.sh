24213a3884f52543b543c9bf1a0a5511dbec7e1e
mario <mariobassem12@gmail.com>
2023-11-28 10:48:05 +0200

    update test.sh
    Signed-off-by: mario <mariobassem12@gmail.com>

--- test.sh
+++ test.sh
@@ -1,37 +1,28 @@
 set -ex
-# pushd ~/code/github/freeflowuniverse/crystallib
+pushd ~/code/github/freeflowuniverse/crystallib
 bash doc.sh
-v -enable-globals -stats test crystallib/data/actionparser
-v -enable-globals -stats test crystallib/algo/encoder
-v -enable-globals -stats test crystallib/core/codeparser
-v -enable-globals -stats test crystallib/clients/coinmarketcap
-v -enable-globals -stats test crystallib/ui/console
-v -enable-globals -stats test crystallib/core/crystaljson
-v -enable-globals -stats test crystallib/data/currency
-v -enable-globals -stats test crystallib/osal/docker
-v -enable-globals -stats test crystallib/algo/encoder
-v -enable-globals -stats test crystallib/tools/imagemagick
-v -enable-globals -stats test crystallib/data/ipaddress
-v -enable-globals -stats test crystallib/data/markdownparser
-v -enable-globals -stats test crystallib/data/mnemonic
-v -enable-globals -stats test crystallib/data/paramsparser
-v -enable-globals -stats test crystallib/core/pathlib
-v -enable-globals -stats test crystallib/clients/redisclient
-v -enable-globals -stats test crystallib/data/resp
-v -enable-globals -stats test crystallib/core/texttools
-v -enable-globals -stats test crystallib/data/ourtime
-v -enable-globals -stats test crystallib/osal/gittools
-v -enable-globals -stats test crystallib/data/jsonrpc
-v -enable-globals -stats test crystallib/core/openrpc
-v -enable-globals -stats test crystallib/data/jsonschema
+v -enable-globals -stats test crystallib/algo
+v -enable-globals -stats test crystallib/baobab
+v -enable-globals -stats test crystallib/biz
+v -enable-globals -stats test crystallib/builder
+v -enable-globals -stats test crystallib/clients
+v -enable-globals -stats test crystallib/core
+v -enable-globals -stats test crystallib/crypt
+v -enable-globals -stats test crystallib/data
+v -enable-globals -stats test crystallib/installers
 v -enable-globals -stats test crystallib/osal
+v -enable-globals -stats test crystallib/servers
+v -enable-globals -stats test crystallib/threefold
+v -enable-globals -stats test crystallib/tools
+v -enable-globals -stats test crystallib/ui
+v -enable-globals -stats test crystallib/web
 
 # OK TO IGNORE FOR NOW
 # v -stats test tmux
 # v -stats test knowledgetree
 # v -stats test crpgp
 
-# popd
+popd
 
 
 echo "TESTS OK"
