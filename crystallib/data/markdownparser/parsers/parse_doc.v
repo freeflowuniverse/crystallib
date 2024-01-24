31eefa0689d92ed59868131c584abd65659f83d2
mario <mariobassem12@gmail.com>
2024-01-24 10:25:24 +0200

    fixes in markdownparser
    Co-authored-by: ashraf fouda <ashraf.m.fouda@gmail.com>

--- crystallib/data/markdownparser/parsers/parse_doc.v
+++ crystallib/data/markdownparser/parsers/parse_doc.v
@@ -63,77 +63,83 @@ pub fn parse_doc(mut doc elements.Doc) ! {
 			continue
 		}
 
-		if line.starts_with('!!include ') {
-			content := line.all_after_first('!!include ').trim_space()
-			doc.include_new(content)
-			parser.next_start()!
-			continue
-		}
-		// parse action
-		if line.starts_with('!!') {
-			doc.action_new(line)
-			parser.next()
-			continue
-		}
-
-		// find codeblock
-		if line.starts_with('```') || line.starts_with("'''") {
-			mut e := doc.codeblock_new('')
-			e.category = line.substr(3, line.len).to_lower().trim_space()
-			parser.next()
-			continue
-		}
-
-		// process headers (# is 35)
-		if line.len > 0 && line[0] == 35 {
-			mut d := 0
-			for d < line.len && line[d] == 35 {
-				d += 1
-			}
-			if d < line.len && line[d] == 32 {
-				if d >= 6 {
-					parser.error_add('header should be max 5 deep')
-					parser.next_start()!
-					continue
-				}
-				mut e := doc.header_new(line.all_after_first(line[..d]).trim_space())
-				e.depth = d
+		if mut llast is elements.Paragraph{
+			if line.starts_with('!!include ') {
+				content := line.all_after_first('!!include ').trim_space()
+				doc.include_new(content)
 				parser.next_start()!
 				continue
 			}
-			parser.next()
-			continue
+			// parse action
+			if line.starts_with('!!') {
+				doc.action_new(line)
+				parser.next()
+				continue
+			}
+
+			// find codeblock
+			if line.starts_with('```') || line.starts_with("'''") {
+				mut e := doc.codeblock_new('')
+				e.category = line.substr(3, line.len).to_lower().trim_space()
+				parser.next()
+				continue
+			}
+
+			// process headers (# is 35)
+			if line.len > 0 && line[0] == 35 {
+				mut d := 0
+				for d < line.len && line[d] == 35 {
+					d += 1
+				}
+				if d < line.len && line[d] == 32 {
+					if d >= 6 {
+						parser.error_add('header should be max 5 deep')
+						parser.next_start()!
+						continue
+					}
+					mut e := doc.header_new(line.all_after_first(line[..d]).trim_space())
+					e.depth = d
+					parser.next_start()!
+					continue
+				}
+				parser.next()
+				continue
+			}
+
+			if trimmed_line.starts_with('|') && trimmed_line.ends_with('|') {
+				doc.table_new('${line}\n')
+				parser.next()
+				continue
+			}
+
+			if trimmed_line.to_lower().starts_with('<html>') {
+				doc.html_new('')
+				parser.next()
+				continue
+			}
+
+			if trimmed_line.starts_with('<!--') && trimmed_line.ends_with('-->') {
+				mut comment := trimmed_line.all_after_first('<!--')
+				comment = comment.all_before('-->')
+				doc.comment_new(comment)
+				parser.next_start()!
+				continue
+			}
 		}
 
-		if trimmed_line.starts_with('|') && trimmed_line.ends_with('|') {
-			doc.table_new('${line}\n')
-			parser.next()
-			continue
+		match mut llast {
+			elements.Paragraph {
+				if parser.endlf == false && parser.next_is_eof() {
+					llast.content += line
+				} else {
+					llast.content += line + '\n'
+				}
+			}
+			else {
+				panic('parser error, means we got element which is not supported')
+			}
 		}
-
-		if trimmed_line.to_lower().starts_with('<html>') {
-			doc.html_new('')
-			parser.next()
-			continue
-		}
-
-		if trimmed_line.starts_with('<!--') && trimmed_line.ends_with('-->') {
-			mut comment := trimmed_line.all_after_first('<!--')
-			comment = comment.all_before('-->')
-			doc.comment_new(comment)
-			parser.next_start()!
-			continue
-		}
-
-		parser.ensure_last_is_paragraph()!
-		llast = parser.lastitem()!
-
-		if parser.endlf == false && parser.next_is_eof() {
-			llast.content += line
-		} else {
-			llast.content += line + '\n'
-		}
-
+		
 		parser.next()
 	}
 
