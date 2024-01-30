dced6e6dedb821c88f573b7b64938ae6d9dd637b
mario <mariobassem12@gmail.com>
2024-01-30 18:44:04 +0200

    add holochain dev script
    Co-authored-by: ashraf fouda <ashraf.m.fouda@gmail.com>

--- examples/threefold/grid/examples/holochain_vm/holochain_vm.v
+++ examples/threefold/grid/examples/holochain_vm/holochain_vm.v
@@ -11,29 +11,40 @@ fn main() {
 		level: .debug
 	}
 	mnemonics := os.getenv('MNEMONICS')
+	
 	chain_network := tfgrid.ChainNetwork.dev // User your desired network
 	mut deployer := tfgrid.new_deployer(mnemonics, chain_network, mut logger)!
-
-	node_id := u32(27)
-	network_name := 'network1'
+	
+	node_id := u32(27) // get from user or use gridproxy to get nodeid
+	network_name := 'network1' // autocreate a network
 	wg_port := deployer.assign_wg_port(node_id)!
 	mut network := models.Znet{
-		ip_range: '10.1.0.0/16'
-		subnet: '10.1.1.0/24'
-		wireguard_private_key: 'GDU+cjKrHNJS9fodzjFDzNFl5su3kJXTZ3ipPgUjOUE='
+		ip_range: '10.1.0.0/16' // auto-assign
+		subnet: '10.1.1.0/24' // auto-assign
+		wireguard_private_key: 'GDU+cjKrHNJS9fodzjFDzNFl5su3kJXTZ3ipPgUjOUE=' // autocreate
 		wireguard_listen_port: wg_port
 		peers: [
 			models.Peer{
-				subnet: '10.1.2.0/24'
-				wireguard_public_key: '4KTvZS2KPWYfMr+GbiUUly0ANVg8jBC7xP9Bl79Z8zM='
-				allowed_ips: ['10.1.2.0/24', '100.64.1.2/32']
+				subnet: '10.1.2.0/24' // auto-assign
+				wireguard_public_key: '4KTvZS2KPWYfMr+GbiUUly0ANVg8jBC7xP9Bl79Z8zM=' // get from autocreated private key
+				allowed_ips: ['10.1.2.0/24', '100.64.1.2/32'] // auto-assign
 			},
 		]
 	}
 	mut znet_workload := network.to_workload(name: network_name, description: 'test_network1')
 
+	ssh_key := os.getenv('SSH_KEY')
+	mut code_server_pass := os.getenv('CODE_SERVER_PASSWORD')
+	if ssh_key.len == 0{
+		panic('SSH_KEY env var must be set')
+	}
+
+	if code_server_pass.len == 0{
+		code_server_pass = 'password'
+	}
+
 	zmachine := models.Zmachine{
-		flist: 'https://hub.grid.tf/tf-official-apps/threefoldtech-ubuntu-22.04.flist'
+		flist: 'https://hub.grid.tf/mariobassem1.3bot/threefolddev-holochain-latest.flist' // from user or default to ubuntu
 		network: models.ZmachineNetwork{
 			public_ip: ''
 			interfaces: [
@@ -44,13 +55,15 @@ fn main() {
 			]
 			planetary: true
 		}
-		entrypoint: '/sbin/zinit init'
+		entrypoint: '/usr/local/bin/entrypoint.sh' // from user or default
 		compute_capacity: models.ComputeCapacity{
-			cpu: 1
-			memory: i64(1024) * 1024 * 1024 * 2
+			cpu: 4
+			memory: i64(8) * 1024 * 1024 * 1024
 		}
+		size: u64(30) * 1024 * 1024 * 1024
 		env: {
-			'SSH_KEY': 'ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDTwULSsUubOq3VPWL6cdrDvexDmjfznGydFPyaNcn7gAL9lRxwFbCDPMj7MbhNSpxxHV2+/iJPQOTVJu4oc1N7bPP3gBCnF51rPrhTpGCt5pBbTzeyNweanhedkKDsCO2mIEh/92Od5Hg512dX4j7Zw6ipRWYSaepapfyoRnNSriW/s3DH/uewezVtL5EuypMdfNngV/u2KZYWoeiwhrY/yEUykQVUwDysW/xUJNP5o+KSTAvNSJatr3FbuCFuCjBSvageOLHePTeUwu6qjqe+Xs4piF1ByO/6cOJ8bt5Vcx0bAtI8/MPApplUU/JWevsPNApvnA/ntffI+u8DCwgP'
+			'SSH_KEY': ssh_key,
+			'CODE_SERVER_PASSWORD': code_server_pass
 		}
 	}
 	mut zmachine_workload := zmachine.to_workload(name: 'vm2', description: 'zmachine_test')
