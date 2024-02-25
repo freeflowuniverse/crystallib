4aeb49fbb73e0b96122f318d9b93386d6a98195a
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2024-02-25 16:48:35 +0200

    adds process recurisve includes for doctree
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- crystallib/data/doctree/tree_processincludes.v
+++ crystallib/data/doctree/tree_processincludes.v
@@ -1,49 +1,60 @@
 module doctree
 
-import freeflowuniverse.crystallib.core.pathlib
 import freeflowuniverse.crystallib.ui.console
-import os
-import freeflowuniverse.crystallib.core.texttools
 
+fn (tree Tree) process_page_includes(mut page Page, col_name string) ! {
+	mut collection := tree.collection_get(col_name)!
+
+	mut mydoc := page.doc()!
+	mut res := mydoc.actionpointers(actor: 'wiki', name: 'include')
+	if res.len > 0 {
+		// means there is a wiki def defined
+		for mut action_element in res {
+			my_action := action_element.action
+			action_element.action_processed = true
+			name := my_action.params.get_default('page', '')!
+			if name == '' {
+				collection.error(
+					path: page.path
+					msg: "can't find 'page' param for include action: ${my_action}"
+					cat: .include
+				)
+			}
+			// handle includes from other collections
+			if collection.page_exists(name) {
+				mut mypage2 := collection.page_get(name)!
+				if mypage2.key() == page.key() {
+					collection.error(
+						path: page.path
+						msg: 'recursive include: ${name} for include action: ${my_action}'
+						cat: .include
+					)
+					continue
+				}
+				tree.process_page_includes(mut mypage2, col_name)!
+				mut mydoc2 := mypage2.doc()!
+				action_element.content = mydoc2.markdown()
+			} else {
+				collection.error(
+					path: page.path
+					msg: "can't find page: ${name} for include action: ${my_action}"
+					cat: .include
+				)
+			}
+		}
+	}
+}
 
 pub fn (mut tree Tree) process_includes() ! {
-
-	tree.process_defs()! //process definitions, always first definitions	
+	tree.process_defs()! // process definitions, always first definitions	
 
 	for collectionname, mut collection in tree.collections {
 		// console.print_green("get includes for collection:${collectionname}")		
 		for pagekey in collection.pages.keys() {
-			mut page := collection.pages[pagekey] or {panic('bug')}
-			console.print_green("Get includes for page ${page.key()}")
-			mut mydoc := page.doc()!
-			mut res:=mydoc.actionpointers(actor:"wiki",name:"include")
-			if res.len>0{
-				//means there is a wiki def defined
-				for mut action_element in res{
-					my_action:=action_element.action
-					action_element.action_processed=true
-					name := my_action.params.get_default("page","")!
-					if name==""{
-						collection.error(path:page.path,msg:"can't find 'page' param for include action: ${my_action}",cat:.include)
-					}
-					if collection.page_exists(name){
-						mut mypage2:=collection.page_get(name)!
-						if mypage2.key()==page.key(){
-							collection.error(path:page.path,msg:"recursive include: ${name} for include action: ${my_action}",cat:.include)						
-							continue
-						}
-						mut mydoc2 := mypage2.doc()!
-						// println("===\n${mydoc.markdown()}\n====\n")
-						// println("===\n${mydoc2.markdown()}\n====\n")
-						action_element.content = mydoc2.markdown()
-					}else{
-						collection.error(path:page.path,msg:"can't find page: ${name} for include action: ${my_action}",cat:.include)						
-					}
-
-				}
-			}
-			println(mydoc.markdown())
+			mut page := collection.pages[pagekey] or { panic('bug') }
+			console.print_green('Get includes for page ${page.key()}')
+			// process include recursivly
+			tree.process_page_includes(mut page, collectionname)!
 		}
 	}
-
 }
--- examples/data/doctree.vsh
+++ examples/data/doctree.vsh
@@ -10,18 +10,17 @@ mut tree := doctree.new()!
 tree.scan(path: testpath)!
 
 tree.process_defs()! // process includes
-// tree.process_includes()! //process includes
+tree.process_includes()! // process includes
 
 mut p := tree.page_get('riverlov:introduction.md')!
 // println(p)
-mut mydoc := p.doc()!
-println(mydoc)
-println(mydoc.markdown())
+// mut mydoc := p.doc()!
+// println(mydoc)
 
-// mut p2:=tree.page_get("riverlov:aboutus.md")!
-// mut mydoc2 := p2.doc()!
+mut p2 := tree.page_get('riverlov:aboutus.md')!
+mut mydoc2 := p2.doc()!
 // // println(mydoc2.defpointers())	
-// // println(mydoc2)
-// println(mydoc2.markdown())	
+// println(mydoc2)
+println(mydoc2.markdown())
 
 // tree.export(dest: '/tmp/remove')!
