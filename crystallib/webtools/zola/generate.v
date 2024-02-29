62e75edf12b826f0dd98aba2f7d2f170f8c23952
timurgordon <timurgordon@gmail.com>
2024-02-28 19:04:12 -0500

    add process step to site generation

--- crystallib/webtools/zola/generate.v
+++ crystallib/webtools/zola/generate.v
@@ -1,14 +1,59 @@
 module zola
 
+import freeflowuniverse.crystallib.clients.redisclient
 import freeflowuniverse.crystallib.webtools.tailwind
 import freeflowuniverse.crystallib.ui.console
 import freeflowuniverse.crystallib.osal
 import freeflowuniverse.crystallib.core.pathlib
 import freeflowuniverse.webcomponents.preprocessor
 import freeflowuniverse.crystallib.core.texttools
+import freeflowuniverse.crystallib.data.markdownparser
+import freeflowuniverse.crystallib.data.markdownparser.elements
+import freeflowuniverse.crystallib.data.doctree
 import os
 
+pub fn (mut site ZolaSite) process() ! {
+	// site.tree.process_defs()! // process includes
+	site.tree.process_includes()! // process includes
+	// site.tree.export(
+	// 	dest: '${site.path_build.path}', 
+	// 	reset: true
+	// 	keep_structure: true
+	// )!
+	
+	// export processed content to content dir of build dir
+	
+	// os.cp_all('${site.path_build.path}/src/content', '${site.path_build.path}/content', true)!
+	
+
+	mut content_dir := pathlib.get_dir(
+		path: '${site.path_build.path}/content'
+	)!
+
+	list := content_dir.list(
+		recursive: true
+		regex:[r'.*\.md$']
+	)!
+
+	for file in list.paths {
+		mut doc := markdownparser.new(path: file.path)!
+		// mut doc := elements.Doc{...doc}
+		// for mut element in doc.children {
+		// 	if mut element is elements.Include {
+		// 		page := site.tree.page_get(element.content)!
+		// 		element.doc = page.doc(mut doctree.DocArgs{})!
+		// 	}
+			
+		// }
+		// if file.path.ends_with('wealth.md') {
+		// 	panic(doc)
+		// }
+	}
+}
+
 pub fn (mut site ZolaSite) generate() ! {
+	site.process()!
+	
 	console.print_header(' website generate: ${site.name} on ${site.path_build.path}')
 
 	mut tw := tailwind.new(
@@ -35,6 +80,9 @@ pub fn (mut site ZolaSite) generate() ! {
 	preprocessor.preprocess('${site.path_build.path}/content')!
 
 	osal.exec(cmd: cmd)!
+	mut r := redisclient.core_get()!
+	r.set('zola:${site.name}:publish', site.path_publish.path)!
+	r.expire('zola:${site.name}:publish', 3600 * 12)!
 }
 
 fn (mut site ZolaSite) template_install() ! {
