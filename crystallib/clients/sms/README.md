4161893d3f95e07d96e054c9b5ce7a4c8a020c93
despiegk <kristof@incubaid.com>
2024-02-29 03:15:05 +0300

    UI

--- crystallib/clients/sms/README.md
+++ crystallib/clients/sms/README.md
@@ -1,7 +1,8 @@
 ## SMS Client
-it uses twilio api through cURL calls.
+
 
 ### Usage
+
 1. Create a twilio account and get your credentials. [here](https://www.twilio.com/try-twilio)
 2. Export your credentials in the shell
     ```bash
---
+++ crystallib/ui/generic/log.v
@@ -0,0 +1,30 @@
+module generic
+
+import freeflowuniverse.crystallib.ui.console { UIConsole }
+// import freeflowuniverse.crystallib.ui.telegram { UITelegram }
+import freeflowuniverse.crystallib.ui.uimodel
+
+// log content to the log panel (not every UI has this capability)
+// ```
+// args:
+// 	content 	string
+// 	clear       bool //means screen is reset for content above
+// 	lf_before int 	 //line feed before content
+// 	lf_after int 
+//  cat LogCat
+//defines colors as used in the representation layer
+// enum LogCat {
+// 	info
+// 	log
+// 	warning
+// 	header
+// 	debug
+// 	error
+// }
+// ```
+pub fn (mut c UserInterface) log(args uimodel.LogArgs) ! {
+	// match mut c.channel {
+	// 	UIConsole { return c.channel.log(args)! }
+	// 	else { panic("can't find channel") }
+	// }
+}
