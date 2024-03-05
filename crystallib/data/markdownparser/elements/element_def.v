4c0b113a9b884cb799a7def6b165b3a4afd5be40
Ashraf Fouda <ashraf.m.fouda@gmail.com>
2024-03-05 10:33:22 +0200

    fixes doctree defs/include (#375)
    Signed-off-by: Ashraf Fouda <ashraf.m.fouda@gmail.com>

--- crystallib/data/markdownparser/elements/element_def.v
+++ crystallib/data/markdownparser/elements/element_def.v
@@ -4,8 +4,8 @@ module elements
 pub struct Def {
 	DocBase
 pub mut:
-	pagekey  string
-	pagename string
+	pagekey   string
+	pagename  string
 	nameshort string
 }
 
@@ -13,9 +13,9 @@ pub fn (mut self Def) process() !int {
 	if self.processed {
 		return 0
 	}
-	if self.nameshort==""{
-		if self.content == ""{
-			return error("cannot get name content should not be empty.")
+	if self.nameshort == '' {
+		if self.content == '' {
+			return error('cannot get name content should not be empty.')
 		}
 		self.nameshort = self.content.to_lower().replace('_', '').trim('*')
 	}
@@ -25,23 +25,29 @@ pub fn (mut self Def) process() !int {
 
 pub fn (mut self Def) process_link() ! {
 	// self.trailing_lf = false
-	self.link_new(mut self.parent_doc(),'[${self.pagename}](${self.pagekey})')
+	self.link_new(mut self.parent_doc(), '[${self.pagename}](${self.pagekey})')
 	self.process_children()!
 	self.content = ''
-	self.processed = true	
+	self.processed = true
 }
 
 pub fn (self Def) markdown() !string {
-	if self.processed {return error("cannot do markdown for ${self} as long as not processed")}
+	if !self.processed {
+		return error('cannot do markdown for ${self} as long as not processed')
+	}
 	return self.DocBase.markdown() // for children
 }
 
 pub fn (self Def) html() !string {
-	if self.processed {return error("cannot do markdown for ${self} as long as not processed")}
+	if !self.processed {
+		return error('cannot do markdown for ${self} as long as not processed')
+	}
 	return self.DocBase.html() // for children
 }
 
 pub fn (self Def) pug() !string {
-	if self.processed {return error("cannot do markdown for ${self} as long as not processed")}
+	if !self.processed {
+		return error('cannot do markdown for ${self} as long as not processed')
+	}
 	return self.DocBase.pug() // for children
 }
