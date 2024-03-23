04e72a74153be4dc3aee9210d958a4b87f7e7d3d
timurgordon <timurgordon@gmail.com>
2024-03-23 13:27:29 -0400

    use heroscript in itenv_ourworld

--- examples/webtools/zola/threefold_heroscript/example.sh
+++ examples/webtools/zola/threefold_heroscript/example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hero zola -u https://git.ourworld.tf/tfgrid/info_tfgrid/src/branch/main/heroscript
+hero zola -u https://git.ourworld.tf/ourworld_holding/itenv_ourworld/src/branch/main/heroscript/web/www_threefold_io
