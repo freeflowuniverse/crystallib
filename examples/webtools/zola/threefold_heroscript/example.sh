9b63399735839ad0766b7a37151bf525729300c6
timurgordon <timurgordon@gmail.com>
2024-04-01 15:45:49 -0400

    example fix wip

--- examples/webtools/zola/threefold_heroscript/example.sh
+++ examples/webtools/zola/threefold_heroscript/example.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-hero zola -u https://git.ourworld.tf/ourworld_holding/itenv_ourworld/src/branch/main/heroscript/web/www_threefold_io
+hero zola -u https://github.com/threefoldfoundation/www_threefold_io/tree/development_zola/playbook
