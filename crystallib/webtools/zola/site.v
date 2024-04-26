28cb3cd58059a4e50cb790f5b2a9c8212e1d0392
despiegk <kristof@incubaid.com>
2024-04-26 08:44:32 +0200

    zola fix

--- crystallib/webtools/zola/site.v
+++ crystallib/webtools/zola/site.v
@@ -3,8 +3,8 @@ module zola
 import freeflowuniverse.crystallib.core.pathlib
 import freeflowuniverse.crystallib.data.doctree
 import freeflowuniverse.crystallib.develop.gittools
-// import freeflowuniverse.crystallib.data.markdownparser
-// import freeflowuniverse.crystallib.data.markdownparser.elements
+import freeflowuniverse.crystallib.data.markdownparser
+import freeflowuniverse.crystallib.data.markdownparser.elements
 import os
 import freeflowuniverse.crystallib.core.texttools
 
@@ -24,6 +24,11 @@ pub mut:
 	blog         Blog
 }
 
+pub struct Blog {
+pub mut:
+	posts map[string]elements.Doc
+}
+
 @[params]
 pub struct ZolaSiteArgs {
 pub mut:
@@ -32,6 +37,49 @@ pub mut:
 	description  string
 	path_publish string // optional
 	url          string = 'http://localhost:9998/' // base url of site
+}
+
+pub fn (mut self Zola) new(args_ ZolaSiteArgs) !&ZolaSite {
+	mut args := args_
+	args.name = texttools.name_fix(args.name)
+	mut site := ZolaSite{
+		name: args.name
+		title: args.title
+		description: args.description
+		url: args.url
+		zola: &self
+		tree: doctree.new(name: 'ws_${args.name}')!
+	}
+
+	self.sites[site.name] = &site
+
+	site.path_build = pathlib.get_dir(
+		path: '${self.path_build}/${args.name}'
+		create: true
+	)!
+	if args.path_publish == '' {
+		args.path_publish = '${self.path_publish}/${args.name}'
+	}
+	site.path_publish = pathlib.get_dir(
+		path: args.path_publish
+		create: true
+	)!
+	// make sure we have clean env
+	for i in ['css', 'static', 'templates', 'content'] {
+		if os.exists('${site.path_build.path}/${i}') {
+			os.rmdir_all('${site.path_build.path}/${i}')!
+		}
+	}
+	if os.exists('${site.path_build.path}/config.toml') {
+		os.rm('${site.path_build.path}/config.toml')!
+	}
+	site.template_install()!
+	return &site
+}
+
+pub fn (mut self Zola) get(name_ string) !&ZolaSite {
+	name := texttools.name_fix(name_)
+	return self.sites[name] or { error('cannot find zolasite with name: ${name}') }
 }
 
 // add template
@@ -52,93 +100,6 @@ pub fn (mut site ZolaSite) template_add(args gittools.GSCodeGetFromUrlArgs) ! {
 		os.cp_all('${mypath}/${i}', '${site.path_build.path}/${i}', true)!
 	}
 }
-
-@[params]
-pub struct BlogAddArgs {
-	Section
-}
-
-// adds a blog section to the zola site
-pub fn (mut site ZolaSite) blog_add(args BlogAddArgs) ! {
-	blog_section := Section{
-		...args.Section
-		name: 'blog'
-		title: if args.title != '' { args.title } else { 'Blog' }
-		sort_by: if args.sort_by != .@none { args.sort_by } else { .date }
-		template: if args.template != '' { args.template } else { 'layouts/blog.html' }
-		page_template: if args.page_template != '' {
-			args.page_template
-		} else {
-			'partials/postCard.html'
-		}
-		paginate_by: if args.paginate_by != 0 { args.paginate_by } else { 3 }
-	}
-	site.add_section(blog_section)!
-}
-
-// TIMUR: PLEASE CHECK
-
-// pub struct BlogAddArgs {
-// 	name       string
-// 	collection string @[required]
-// 	file       string @[required]
-// 	image      string
-// }
-
-// pub fn (mut site ZolaSite) blog_add(args BlogAddArgs) ! {
-// 	site.tree.process_includes()!
-// 	_ = site.tree.collection_get(args.collection) or {
-// 		println(err)
-// 		return err
-// 	}
-// 	mut page := site.tree.page_get('${args.collection}:${args.file}') or {
-// 		println(err)
-// 		return err
-// 	}
-// 	mut image := site.tree.image_get('${args.collection}:${args.image}') or {
-// 		println(err)
-// 		return err
-// 	}
-
-// 	mut blog_index := pathlib.get_file(
-// 		path: '${site.path_build.path}/content/blog/_index.md'
-// 	)!
-// 	if !blog_index.exists() {
-// 		blog_index.write('---
-// title: "Blog"
-// paginate_by: 9
-
-// # paginate_reversed: false
-
-// sort_by: "date"
-// insert_anchor_links: "left"
-// #base_url: "posts"
-// #first: "first"
-// #last: "last"
-// template: "layouts/blog.html"
-// page_template: "blogPage.html"
-// #transparent: true
-// generate_feed: true
-// extra:
-//   imgPath: images/threefold_img2.png
-// ---
-// ')!
-// 	}
-
-// 	blog_dir := pathlib.get_dir(
-// 		path: '${site.path_build.path}/content/blog'
-// 		create: true
-// 	)!
-// 	fixed_name := '${texttools.name_fix(args.name)}'
-// 	post_dir := pathlib.get_dir(
-// 		path: '${blog_dir.path}/${fixed_name}'
-// 		create: true
-// 	)!
-// 	page.export(dest: '${post_dir.path}/index.md')!
-// 	image.copy('${post_dir.path}/${image.file_name()}')!
-
-// 	site.blog.posts[args.name] = page.doc()!
-// }
 
 // add content from website, can be more than 1, will sync but not overwrite to the destination website
 //```args for getting the template
@@ -173,6 +134,68 @@ pub fn (mut site ZolaSite) content_add(args gittools.GSCodeGetFromUrlArgs) ! {
 		// }
 		// pointers := doc.action_pointers()
 	}
+}
+
+pub struct BlogAddArgs {
+	name       string
+	collection string @[required]
+	file       string @[required]
+	image      string
+}
+
+pub fn (mut site ZolaSite) blog_add(args BlogAddArgs) ! {
+	site.tree.process_includes()!
+	_ = site.tree.collection_get(args.collection) or {
+		println(err)
+		return err
+	}
+	mut page := site.tree.page_get('${args.collection}:${args.file}') or {
+		println(err)
+		return err
+	}
+	mut image := site.tree.image_get('${args.collection}:${args.image}') or {
+		println(err)
+		return err
+	}
+
+	mut blog_index := pathlib.get_file(
+		path: '${site.path_build.path}/content/blog/_index.md'
+	)!
+	if !blog_index.exists() {
+		blog_index.write('---
+title: "Blog"
+paginate_by: 9
+
+# paginate_reversed: false
+
+sort_by: "date"
+insert_anchor_links: "left"
+#base_url: "posts"
+#first: "first"
+#last: "last"
+template: "layouts/blog.html"
+page_template: "blogPage.html"
+#transparent: true
+generate_feed: true
+extra:
+  imgPath: images/threefold_img2.png
+---
+')!
+	}
+
+	blog_dir := pathlib.get_dir(
+		path: '${site.path_build.path}/content/blog'
+		create: true
+	)!
+	fixed_name := '${texttools.name_fix(args.name)}'
+	post_dir := pathlib.get_dir(
+		path: '${blog_dir.path}/${fixed_name}'
+		create: true
+	)!
+	page.export(dest: '${post_dir.path}/index.md')!
+	image.copy('${post_dir.path}/${image.file_name()}')!
+
+	site.blog.posts[args.name] = page.doc()!
 }
 
 pub fn (mut site ZolaSite) person_add(args BlogAddArgs) ! {
