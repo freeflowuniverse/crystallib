1b74e2c7284436a400a87d899e0ba18acae06a1f
timurgordon <timurgordon@gmail.com>
2024-04-30 04:22:37 +0300

    archive old actorgenerator

--- crystallib/core/actorgenerator/model.v
+++
@@ -1,9 +0,0 @@
-module actorgenerator
-
-import freeflowuniverse.crystallib.core.codemodel
-import freeflowuniverse.crystallib.core.codeparser
-import os
-
-pub struct ActorGenerator {
-	model_name string
-}
