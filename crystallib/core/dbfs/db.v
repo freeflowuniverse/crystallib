09bc9a667246f83abfc0e1f14aabcf7d17cb9577
timurgordon <timurgordon@gmail.com>
2024-05-22 13:08:02 +0300

    fmt

--- crystallib/core/dbfs/db.v
+++ crystallib/core/dbfs/db.v
@@ -10,56 +10,55 @@ pub struct DB {
 mut:
 	config DBConfig
 pub mut:
-	path      pathlib.Path
-	parent    &DBCollection @[skip; str: skip]
-	namedb 	  ?NameDB //optional namedb which is for hashed keys
+	path   pathlib.Path
+	parent &DBCollection @[skip; str: skip]
+	namedb ?NameDB // optional namedb which is for hashed keys
 }
 
 pub struct DBConfig {
 mut:
 	encrypted bool
 pub:
-	name string
-	withkeys bool //if set means we will use keys in stead of only u32
-	keyshashed bool //if its ok to hash the keys, which will generate id out of these keys and its more scalable
-	ext string //extension if we want to use it in DB e.g. 'json'
-	//base64 bool //if binary data will be base encoded, not used now
+	name       string
+	withkeys   bool   // if set means we will use keys in stead of only u32
+	keyshashed bool   // if its ok to hash the keys, which will generate id out of these keys and its more scalable
+	ext        string // extension if we want to use it in DB e.g. 'json'
+	// base64 bool //if binary data will be base encoded, not used now
 }
 
 @[params]
 pub struct GetArgs {
 pub mut:
-	key     string
-	id      u32
+	key string
+	id  u32
 }
 
 // get the value, if it doesn't exist then return empty string
 pub fn (mut db DB) get(args_ GetArgs) !string {
-	mut args:=args_
+	mut args := args_
 	args.key = texttools.name_fix(args.key)
-	mut pathsrc:=pathlib.Path{}
-	if args.key.len>0{
-		if args.id>0{
+	mut pathsrc := pathlib.Path{}
+	if args.key.len > 0 {
+		if args.id > 0 {
 			return error("cann't specify id and key")
 		}
-		if db.config.withkeys{
-			if db.config.keyshashed{
-				//means we use a namedb
-				mut ndb := db.namedb or {
-						panic("namedb should be available")
-					}
-				args.id,_ =ndb.get(args.key)!
-				pathsrc=db.path_get(args.id)!
-			}else{
-				//now we need to use the link as set
-				mut datapath0:="${db.path.path}/${args.key}"
-				if db.config.ext.len>0{
-					datapath0+=".${db.config.ext}"
+		if db.config.withkeys {
+			if db.config.keyshashed {
+				// means we use a namedb
+				mut ndb := db.namedb or { panic('namedb should be available') }
+
+				args.id, _ = ndb.get(args.key)!
+				pathsrc = db.path_get(args.id)!
+			} else {
+				// now we need to use the link as set
+				mut datapath0 := '${db.path.path}/${args.key}'
+				if db.config.ext.len > 0 {
+					datapath0 += '.${db.config.ext}'
 				}
-				pathsrc=pathlib.get_link(path:datapath0,create:false)!
+				pathsrc = pathlib.get_link(path: datapath0, create: false)!
 			}
-		}else{
-			pathsrc=db.path_get(args.id)!
+		} else {
+			pathsrc = db.path_get(args.id)!
 		}
 	} else if args.id > 0 {
 		pathsrc = db.path_get(args.id)!
@@ -80,167 +79,161 @@ pub fn (mut db DB) get(args_ GetArgs) !string {
 @[params]
 pub struct SetArgs {
 pub mut:
-	key     string
-	id      u32
-	value   string
-	valueb   []u8 //as bytes
+	key    string
+	id     u32
+	value  string
+	valueb []u8 // as bytes
 }
-
 
 // set the key/value will go to filesystem, is organzed per context and each db has a name
 pub fn (mut db DB) set(args_ SetArgs) !u32 {
-	//println(args_)
-	mut args:=args_
-	if args.value.len==0 && args.valueb.len==0 {
-		return error("specify for value or valueb, now both empty")
-	}		
-	if args.key.len>0{
+	// println(args_)
+	mut args := args_
+	if args.value.len == 0 && args.valueb.len == 0 {
+		return error('specify for value or valueb, now both empty')
+	}
+	if args.key.len > 0 {
 		args.key = texttools.name_fix(args.key)
 	}
 
-	if args.value.len>0  {
+	if args.value.len > 0 {
 		args.valueb = args.value.bytes()
-		args.value = ""
-	}	
+		args.value = ''
+	}
 
-	mut pathsrc:=pathlib.Path{}
-	
-	//lets deal with key
-	if args.key.len>0{
-		if args.id>0{
-			return error("cant have id and key at same time")
+	mut pathsrc := pathlib.Path{}
+
+	// lets deal with key
+	if args.key.len > 0 {
+		if args.id > 0 {
+			return error('cant have id and key at same time')
 		}
-		if !db.config.withkeys{
-			return error("db needs to be with keys")
-		}		
-		if db.config.keyshashed{
-			//means we use a namedb
-			mut ndb := db.namedb or {
-					panic("namedb should be available")
-				}
-			args.id = ndb.set(args.key,"")!
-			pathsrc=db.path_get(args.id)!
-		}else{
-			mut datapath0:="${db.path.path}/${args.key}"
-			if db.config.ext.len>0{
-				datapath0+=".${db.config.ext}"
+		if !db.config.withkeys {
+			return error('db needs to be with keys')
+		}
+		if db.config.keyshashed {
+			// means we use a namedb
+			mut ndb := db.namedb or { panic('namedb should be available') }
+
+			args.id = ndb.set(args.key, '')!
+			pathsrc = db.path_get(args.id)!
+		} else {
+			mut datapath0 := '${db.path.path}/${args.key}'
+			if db.config.ext.len > 0 {
+				datapath0 += '.${db.config.ext}'
 			}
-			pathsrc=pathlib.get_link(path:datapath0,create:false)!		
-			if !pathsrc.exists(){
+			pathsrc = pathlib.get_link(path: datapath0, create: false)!
+			if !pathsrc.exists() {
 				args.id = db.parent.incr()!
-				mut destname:="${db.path.path}/${args.key}"
-				if db.config.ext.len>0{
-					destname+=".${db.config.ext}"
+				mut destname := '${db.path.path}/${args.key}'
+				if db.config.ext.len > 0 {
+					destname += '.${db.config.ext}'
 				}
-				pathsrc=db.path_get(args.id)!
-				pathsrc.write("")!
-				pathsrc.link(destname,true)! //link the key to the right source info					
-			}else{
-				mut p3:=pathsrc.getlink()!
+				pathsrc = db.path_get(args.id)!
+				pathsrc.write('')!
+				pathsrc.link(destname, true)! // link the key to the right source info					
+			} else {
+				mut p3 := pathsrc.getlink()!
 				p3_name := p3.name()
 				args.id = p3_name.u32()
-			}		
+			}
 		}
-	}else if args.id > 0{
-		pathsrc=db.path_get(args.id)!
+	} else if args.id > 0 {
+		pathsrc = db.path_get(args.id)!
 	} else {
 		args.id = db.parent.incr()!
-		pathsrc=db.path_get(args.id)!
+		pathsrc = db.path_get(args.id)!
 	}
 	// println(pathsrc)
 	if db.config.encrypted {
 		args.valueb = aes_symmetric.encrypt(args.valueb, db.secret()!)
 		pathsrc.write(base64.encode(args.valueb))!
-	}else{
+	} else {
 		pathsrc.writeb(args.valueb)!
 	}
 
-	assert args.id>0
+	assert args.id > 0
 	return args.id
 }
 
-//get path based on int id in the DB
-fn (mut db DB) path_get(myid u32) !pathlib.Path {	
-	a,b,c:=namedb_dbid(myid)
-	mut destname:=c.str()
-	if db.config.ext.len>0{
-		destname+=".${db.config.ext}"
-	}	
+// get path based on int id in the DB
+fn (mut db DB) path_get(myid u32) !pathlib.Path {
+	a, b, c := namedb_dbid(myid)
+	mut destname := c.str()
+	if db.config.ext.len > 0 {
+		destname += '.${db.config.ext}'
+	}
 	mut mydatafile := pathlib.get_file(
-		path: "${db.path.path}/${a}/${b}/${destname}"
+		path: '${db.path.path}/${a}/${b}/${destname}'
 		create: false
 	)!
 	return mydatafile
 }
 
-
-
 // check if entry exists based on keyname
 pub fn (mut db DB) exists(args_ GetArgs) !bool {
-	mut args:=args_
+	mut args := args_
 	args.key = texttools.name_fix(args.key)
-	mut pathsrc:=pathlib.Path{}
-	if args.key.len>0{
-		if args.id>0{
+	mut pathsrc := pathlib.Path{}
+	if args.key.len > 0 {
+		if args.id > 0 {
 			return error("cann't specify id and key")
 		}
-		if !db.config.withkeys{
-			return error("db needs to be with keys")
+		if !db.config.withkeys {
+			return error('db needs to be with keys')
 		}
-		if db.config.keyshashed{
-			//means we use a namedb
-			mut ndb := db.namedb or {
-					panic("namedb should be available")
-				}
+		if db.config.keyshashed {
+			// means we use a namedb
+			mut ndb := db.namedb or { panic('namedb should be available') }
+
 			return ndb.exists(args.key)!
-		}else{
-			mut datapath0:="${db.path.path}/${args.key}"
-			if db.config.ext.len>0{
-				datapath0+=".${db.config.ext}"
+		} else {
+			mut datapath0 := '${db.path.path}/${args.key}'
+			if db.config.ext.len > 0 {
+				datapath0 += '.${db.config.ext}'
 			}
-			pathsrc=pathlib.get_link(path:datapath0,create:false)!
+			pathsrc = pathlib.get_link(path: datapath0, create: false)!
 		}
-	}else{
-		pathsrc=db.path_get(args.id)!
+	} else {
+		pathsrc = db.path_get(args.id)!
 	}
 	return pathsrc.exists()
 }
 
 // delete an entry
 pub fn (mut db DB) delete(args_ GetArgs) ! {
-	mut args:=args_
-	if args.key.len>0{
+	mut args := args_
+	if args.key.len > 0 {
 		args.key = texttools.name_fix(args.key)
 	}
-	mut pathsrc:=pathlib.Path{}
-	if args.key.len>0{
-		if args.id>0{
-			return error("cant have id and key at same time")
+	mut pathsrc := pathlib.Path{}
+	if args.key.len > 0 {
+		if args.id > 0 {
+			return error('cant have id and key at same time')
 		}
-		if !db.config.withkeys{
-			return error("db needs to be with keys")
-		}		
-		if db.config.keyshashed{
-			//means we use a namedb
-			mut ndb := db.namedb or {
-					panic("namedb should be available")
-				}
-			args.id,_ = ndb.get(args.key)!
-			pathsrc=db.path_get(args.id)!
+		if !db.config.withkeys {
+			return error('db needs to be with keys')
+		}
+		if db.config.keyshashed {
+			// means we use a namedb
+			mut ndb := db.namedb or { panic('namedb should be available') }
+
+			args.id, _ = ndb.get(args.key)!
+			pathsrc = db.path_get(args.id)!
 			ndb.delete(args.key)!
-		}else{
-			mut datapath0:="${db.path.path}/${args.key}"
-			if db.config.ext.len>0{
-				datapath0+=".${db.config.ext}"
+		} else {
+			mut datapath0 := '${db.path.path}/${args.key}'
+			if db.config.ext.len > 0 {
+				datapath0 += '.${db.config.ext}'
 			}
-			pathsrc=pathlib.get_link(path:datapath0,create:false)!		
-			if pathsrc.exists(){
-				mut p3:=pathsrc.getlink()!
-				p3.delete()!				
-			}		
+			pathsrc = pathlib.get_link(path: datapath0, create: false)!
+			if pathsrc.exists() {
+				mut p3 := pathsrc.getlink()!
+				p3.delete()!
+			}
 		}
-	}else{
-		pathsrc=db.path_get(args.id)!
+	} else {
+		pathsrc = db.path_get(args.id)!
 	}
 	pathsrc.delete()!
 }
@@ -252,8 +245,8 @@ pub fn (mut db DB) destroy() ! {
 
 // get all keys of the db (e.g. per session) can be with a prefix
 pub fn (mut db DB) keys(prefix_ string) ![]string {
-	//TODO: see get, to fix this one
-	panic("implement")
+	// TODO: see get, to fix this one
+	panic('implement')
 	prefix := texttools.name_fix(prefix_)
 	mut r := db.path.list(recursive: false)!
 	mut res := []string{}
@@ -268,8 +261,8 @@ pub fn (mut db DB) keys(prefix_ string) ![]string {
 
 // get all keys of the db (e.g. per session) can be with a prefix
 pub fn (mut db DB) ids() ![]u32 {
-	//TODO: see get, to fix this one
-	panic("implement")
+	// TODO: see get, to fix this one
+	panic('implement')
 	mut res := []u32{}
 	return res
 }
@@ -289,7 +282,7 @@ fn (mut db DB) secret() !string {
 // will mark db for encryption .
 // will go over all existing keys and encrypt
 pub fn (mut db DB) encrypt() ! {
-	//TODO: see get, to fix this one
+	// TODO: see get, to fix this one
 	if db.config.encrypted {
 		return
 	}
@@ -304,6 +297,6 @@ pub fn (mut db DB) encrypt() ! {
 	db.path.file_get_new('encrypted')!
 }
 
-pub fn (db DB) is_encrypted() bool{
+pub fn (db DB) is_encrypted() bool {
 	return db.config.encrypted
 }
