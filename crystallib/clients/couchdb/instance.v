38d458266d372290ec3c735cf2c61d4c3ceaa6bd
mariobassem <mariobassem12@gmail.com>
2024-06-24 19:01:21 +0300

    wip: add CouchDBInstace implementation

--- crystallib/clients/couchdb/instance.v
+++ crystallib/clients/couchdb/instance.v
@@ -1,27 +1,106 @@
 module couchdb
 
 import freeflowuniverse.crystallib.clients.httpconnection
+import json
 
 // one specific DB in the CouchDB server
 pub struct CouchDBInstance {
 pub mut:
+	// db name
+	name       string
 	connection &httpconnection.HTTPConnection
 }
 
-// see http://127.0.0.1:5984/_utils/docs/intro/api.html what to support
-
-pub fn (mut cl CouchDBInstance) get() ! {
-	panic('implement')
+@[params]
+pub struct DBSecurity {
+pub mut:
+	admins  DBUsers
+	members DBUsers
 }
 
-pub fn (mut cl CouchDBInstance) set() ! {
-	panic('implement')
+@[params]
+pub struct DBUsers {
+pub mut:
+	names []string
+	roles []string
 }
 
-pub fn (mut cl CouchDBInstance) delete() ! {
-	panic('implement')
+pub struct DB {
+pub mut:
+	instance_start_time u64
+	db_name             string
+	purge_seq           string
+	update_seq          string
+	sizes               Sizes
+	props               Props
+	doc_del_count       u64
+	doc_count           u64
+	disk_format_version u64
+	compact_running     bool
+	cluster             Cluster
 }
 
-pub fn (mut cl CouchDBInstance) query() ! {
-	panic('implement')
+pub struct Sizes {
+pub mut:
+	file     u64
+	external u64
+	active   u64
 }
+
+pub struct Props {
+pub mut:
+	partitioned bool
+}
+
+pub struct Cluster {
+pub mut:
+	n u64
+	q u64
+	r u64
+	w u64
+}
+
+pub fn (mut cl CouchDBInstance) get_db() !DB {
+	res := cl.connection.send(method: .get, prefix: cl.name)!
+	if !res.is_ok() {
+		return error('failed to get db: (${res.code}) ${res.data}')
+	}
+
+	db := json.decode(DB, res.data)!
+	return db
+}
+
+pub fn (mut cl CouchDBInstance) get_document[T](doc_id string) !T {
+	res := cl.connection.send(method: .get, prefix: '${cl.name}/${doc_id}')!
+	if !res.is_ok() {
+		return error('failed to get document: (${res.code}) ${res.data}')
+	}
+
+	doc := json.decode(T, res.data)!
+	return doc
+}
+
+pub fn (mut cl CouchDBInstance) create_docuemnt[T](doc T) ! {
+	res := cl.connection.send(method: .post, prefix: cl.name, data: json.encode(doc))!
+	if !res.is_ok() {
+		return error('failed to create document: (${res.code}) ${res.data}')
+	}
+}
+
+pub fn (mut cl CouchDBInstance) create_or_update_document[T](doc_id string, doc T) ! {
+	res := cl.connection.send(method: .put, prefix: '${cl.name}/${doc_id}', data: json.encode(doc))!
+	if !res.is_ok() {
+		return error('failed to create/update document: (${res.code}) ${res.data}')
+	}
+}
+
+pub fn (mut cl CouchDBInstance) delete(doc_id string) ! {
+	res := cl.connection.send(method: .delete, prefix: '${cl.name}/${doc_id}')!
+	if !res.is_ok() {
+		return error('failed to delete document: (${res.code}) ${res.data}')
+	}
+}
+
+// pub fn (mut cl CouchDBInstance) query(...)!{
+
+// }
--- crystallib/clients/couchdb/instance_attachments.v
+++ crystallib/clients/couchdb/instance_attachments.v
@@ -6,18 +6,48 @@ module couchdb
 
 // QUESTION: how should we deal with big documents
 
-pub fn (mut cl CouchDBInstance) attachment_get() ! {
-	panic('implement')
+// pub struct Attachment {
+// pub mut:
+// 	content_type   string
+// 	data           ?string
+// 	digest         string
+// 	encoded_length ?u64
+// 	encoding       ?string
+// 	length         ?u64
+// 	revpos         u64
+// 	stub           ?bool
+// }
+
+pub fn (mut cl CouchDBInstance) attachment_get(doc_id string, attachment_name string) ![]u8 {
+	res := cl.connection.send(method: .get, prefix: '${cl.name}/${doc_id}/${attachment_name}')!
+	if !res.is_ok() {
+		return error('failed to get attachment: (${res.code}) ${res.data}')
+	}
+
+	return res.data.bytes()
 }
 
-pub fn (mut cl CouchDBInstance) attachment_add() ! {
-	panic('implement')
+pub fn (mut cl CouchDBInstance) attachment_add(doc_id string, attachment_name string, data []u8) ! {
+	res := cl.connection.send(
+		method: .put
+		prefix: '${cl.name}/${doc_id}/${attachment_name}'
+		data: data.str()
+	)!
+	if !res.is_ok() {
+		return error('failed to add attachment: (${res.code}) ${res.data}')
+	}
 }
 
-pub fn (mut cl CouchDBInstance) attachment_delete() ! {
-	panic('implement')
+pub fn (mut cl CouchDBInstance) attachment_delete(doc_id string, attachment_name string) ! {
+	res := cl.connection.send(
+		method: .delete
+		prefix: '${cl.name}/${doc_id}/${attachment_name}'
+	)!
+	if !res.is_ok() {
+		return error('failed to delete attachment: (${res.code}) ${res.data}')
+	}
 }
 
-pub fn (mut cl CouchDBInstance) attachment_list() ! {
-	panic('implement')
-}
+// pub fn (mut cl CouchDBInstance) attachment_list(...)!{
+
+// }
