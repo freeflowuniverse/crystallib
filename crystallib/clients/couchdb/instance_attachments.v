29b88da5dafc50e85675f550d80ee6ab81403b8a
timurgordon <timurgordon@gmail.com>
2024-06-24 11:10:30 +0300

    fix unimplemented code causing ci to fail

--- crystallib/clients/couchdb/instance_attachments.v
+++ crystallib/clients/couchdb/instance_attachments.v
@@ -1,24 +1,23 @@
 module couchdb
 
+// see http://127.0.0.1:5984/_utils/docs/intro/api.html#documents attachments
 
-//see http://127.0.0.1:5984/_utils/docs/intro/api.html#documents attachments
+// make sure we return as binary
 
-//make sure we return as binary
+// QUESTION: how should we deal with big documents
 
-//QUESTION: how should we deal with big documents
-
-pub fn (mut cl CouchDBInstance) attachment_get(...)!{
-
+pub fn (mut cl CouchDBInstance) attachment_get() ! {
+	panic('implement')
 }
 
-pub fn (mut cl CouchDBInstance) attachment_add(...)!{
-
+pub fn (mut cl CouchDBInstance) attachment_add() ! {
+	panic('implement')
 }
 
-pub fn (mut cl CouchDBInstance) attachment_delete(...)!{
-
+pub fn (mut cl CouchDBInstance) attachment_delete() ! {
+	panic('implement')
 }
 
-pub fn (mut cl CouchDBInstance) attachment_list(...)!{
-
+pub fn (mut cl CouchDBInstance) attachment_list() ! {
+	panic('implement')
 }
