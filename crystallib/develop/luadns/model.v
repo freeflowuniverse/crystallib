3e3816e9e033fdffcd8e50aa500608d26fc84eb7
timurgordon <timurgordon@gmail.com>
2024-07-05 16:45:58 +0300

    add luadns module

---
+++ crystallib/develop/luadns/model.v
@@ -0,0 +1,30 @@
+module luadns
+
+import freeflowuniverse.crystallib.core.pathlib
+
+struct ARecord {
+mut:
+    name string
+    ip   string
+}
+
+struct CNAMERecord {
+mut:
+    name  string
+    alias string
+}
+
+struct CAARecord {
+mut:
+    name  string
+    value string
+    tag   string
+}
+
+struct DNSConfig {
+mut:
+    domain        string
+    a_records     []ARecord
+    cname_records []CNAMERecord
+    caa_records   []CAARecord
+}
