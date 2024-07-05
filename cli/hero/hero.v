dae2aaac43a165a647bbdeff70aaa2049d956970
timurgordon <timurgordon@gmail.com>
2024-07-05 16:48:55 +0300

    add luadns to hero cli

--- cli/hero/hero.v
+++ cli/hero/hero.v
@@ -76,6 +76,7 @@ fn do() ! {
 	// herocmds.cmd_configure(mut cmd)
 	// herocmds.cmd_postgres(mut cmd)
 	herocmds.cmd_mdbook(mut cmd)
+	herocmds.cmd_luadns(mut cmd)
 	herocmds.cmd_caddy(mut cmd)
 	herocmds.cmd_zola(mut cmd)
 	herocmds.cmd_juggler(mut cmd)
