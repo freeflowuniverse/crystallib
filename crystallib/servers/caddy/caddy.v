e9db20047a0ac65a04cba6b49547598364e049be
timurgordon <timurgordon@gmail.com>
2024-07-08 11:27:07 +0300

    implement basic auth, use json caddyfile model

--- crystallib/servers/caddy/caddy.v
+++ crystallib/servers/caddy/caddy.v
@@ -3,34 +3,13 @@ module caddy
 import freeflowuniverse.crystallib.core.pathlib
 import freeflowuniverse.crystallib.ui.console
 import freeflowuniverse.crystallib.sysadmin.startupmanager
+import freeflowuniverse.crystallib.servers.caddy.security
 import os
 
 // Restart the Caddy
 pub fn (mut self Caddy[Config]) restart() ! {
 	self.stop()!
 	self.start()!
-}
-
-pub fn (mut self Caddy[Config]) reverse_proxy(address Address, args ReverseProxy) ! {
-	mut cfg := self.config()!
-	cfg.file.reverse_proxy(address, args)!
-}
-
-pub fn (mut self Caddy[Config]) file_server(addresses []Address, args FileServer) ! {
-	mut cfg := self.config()!
-	cfg.file.file_server(addresses, args)!
-}
-
-pub fn (mut self Caddy[Config]) add_block(block SiteBlock) ! {
-	mut cfg := self.config()!
-	cfg.file.add_site_block(block)
-}
-
-pub fn (mut self Caddy[Config]) generate() ! {
-	mut cfg := self.config()!
-	content := cfg.file.export()!
-	mut file := pathlib.get_file(path: '${cfg.homedir}/Caddyfile')!
-	file.write(content) or { panic('failed to write ${err}') }
 }
 
 pub fn (mut self Caddy[Config]) start() ! {
---
+++ crystallib/servers/caddy/http.v
@@ -0,0 +1,134 @@
+module caddy
+
+pub struct HTTP {
+mut:
+	servers map[string]Server
+}
+
+pub struct Server {
+mut:
+	listen []string //ports
+	routes []Route
+	
+}
+
+pub struct Route {
+mut:
+	@match []Match
+	handle []Handle
+
+}
+
+pub struct Match {
+mut:
+	host []string
+	path []string
+}
+
+pub struct Handle {
+mut:
+	handler string
+	routes []Route
+	providers Providers
+	root string
+}
+
+pub struct Providers {
+mut:
+	http_basic HTTPBasic
+}
+
+pub struct HTTPBasic {
+mut:
+	hash string = 'bcrypt'
+	accounts []Account
+}
+
+pub struct Account {
+	username string
+	password string
+}
+
+pub fn (mut http HTTP) add_file_server(args FileServer) ! {
+	if 'srv0' !in http.servers {
+		http.servers['srv0'] = Server{}
+	}
+
+	http.servers['srv0'].routes << Route {
+		@match: [Match{
+			host: [args.domain]
+		}]
+		handle: [Handle{
+			handler:'subroute'
+			routes: [Route {
+				handle: [Handle{
+					handler:'vars'
+					root: args.root
+				}]
+			}]
+		}]
+	}
+}
+
+pub fn (mut http HTTP) add_basic_auth(domain string, username string, password string) ! {
+	for key, mut server in http.servers {
+		server.add_basic_auth(domain, username, password)
+	}
+}
+
+pub fn (mut server Server) add_basic_auth(domain string, username string, password string) {
+		for mut route in server.routes {
+			if route.@match.any(domain in it.host) {
+				route.add_basic_auth(username, password)
+			}
+		}
+}
+
+pub fn (mut route Route) add_basic_auth(username string, password string) {
+
+	mut found := false
+	for mut handle in route.handle {
+		if handle.handler == 'subroute' {
+			found = true
+			handle.routes << basic_auth_route(username, password)
+		}
+	}
+	if !found {
+		route.handle << Handle {
+			handler: 'subroute'
+			routes: [basic_auth_route(username, password)]
+		}
+	}
+}
+
+fn basic_auth_route(username string, password string) Route {
+	return Route {
+		handle: [
+			Handle {
+				handler:'authentication'
+				providers: Providers {
+					http_basic: HTTPBasic {
+						accounts: [
+							Account{
+								username: username
+								password: password
+							}
+						]
+					}
+				}
+			}
+		]
+	}
+}
+
+// internal function to find and return the route object in which a domain is defined
+fn (http HTTP) get_domain_route(domain string) ?Route {
+	for key, server in http.servers {
+		for route in server.routes {
+			if route.@match.any(domain in it.host) {
+				return route
+			}
+		}
+	}
+	return none
+}
