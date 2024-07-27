da831a34272e4b1b581bfd4ad356ac8b0bd0459c
despiegk <kristof@incubaid.com>
2024-07-27 09:28:02 +0200

    ...

--- crystallib/installers/base/uninstaller.v
+++ crystallib/installers/base/uninstaller.v
@@ -25,18 +25,18 @@ pub fn osx_uninstall_containers() ! {
 }
 
 
-pub fn osx_uninstall_brew() ! {
-	console.print_header('Uninstall Brew')
-	pl := osal.platform()
-	if pl != .osx {
-		return error("only support OSX")
-	}
-	error('cannot uninstall containers, something went wrong.\n${err}')
+// pub fn osx_uninstall_brew() ! {
+// 	console.print_header('Uninstall Brew')
+// 	pl := osal.platform()
+// 	if pl != .osx {
+// 		return error("only support OSX")
+// 	}
+// 	return error('cannot uninstall containers, something went wrong.\n${err}')
 
-	brew_uninstall_file:=$embed_file('templates/brew_uninstall.sh')
-	osal.exec(cmd: brew_uninstall_file.to_string(), stdout: true, ignore_error:false, shell: true
-	) or { return error('cannot uninstall brew, something went wrong.\n${err}') }
+// 	brew_uninstall_file:=$embed_file('templates/brew_uninstall.sh')
+// 	osal.exec(cmd: brew_uninstall_file.to_string(), stdout: true, ignore_error:false, shell: true
+// 	) or { return error('cannot uninstall brew, something went wrong.\n${err}') }
 	
-	//will never come here because of shell: true
-	console.print_header('Removed brew and docker ...')
-}
+// 	//will never come here because of shell: true
+// 	console.print_header('Removed brew and docker ...')
+// }
---
+++ scripts/nerdctl/container_crystal_shell.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -eu -o pipefail
+export MYPATH=$(dirname "$(realpath "$0")")
+cd $MYPATH
+
+export NAME="crystal"
+export BASENR=7
+
+ssh -p ${BASENR}022 root@127.0.0.1 -A 
+
+
+
