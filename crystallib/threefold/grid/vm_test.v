16be05f03b2179851a7ac6e9e8d0c7aff87bbeca
root <kristof@incubaid.com>
2024-08-13 08:52:10 +0200

    remove test data

--- crystallib/threefold/grid/vm_test.v
+++ crystallib/threefold/grid/vm_test.v
@@ -1,7 +1,7 @@
 module grid
 
 fn test_vm_deploy() ! {
-	deployer := new_deployer('license carry boss sand squeeze forum wide try reform embrace chimney mimic brass wreck wing dove tiger admit jelly quit water twist face pull')!
+	deployer := new_deployer('')!
 	deployer.vm_deploy(
 		name: 'test_vm'
 		deployment_name: 'test_deployment'
