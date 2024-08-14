6b9216aeb6616ab5cb426000b18da6b6d3b510a8
timurgordon <timurgordon@gmail.com>
2024-08-14 15:29:42 +0300

    get tfrobot working again

---
+++ crystallib/threefold/tfrobot/vm_deploy_test.v
@@ -0,0 +1,35 @@
+module tfrobot
+
+import os
+import freeflowuniverse.crystallib.osal
+
+
+const testdata_dir = '${os.dir(@FILE)}/testdata'
+
+fn testsuite_begin() ! {
+	osal.load_env_file('${testdata_dir}/.env')!
+}
+
+fn test_vm_deploy() ! {
+	mneumonics := os.getenv('MNEUMONICS')
+	ssh_key := os.getenv('SSH_KEY')
+
+	println('debbb ${ssh_key}')
+
+	mut robot := configure('testrobot', 
+		mnemonics: mneumonics
+		network: 'main'
+	)!
+	result := robot.vm_deploy(
+		deployment_name: 'test_deployment'
+		name: 'test_vm'
+		cores: 1
+		memory: 256
+		pub_sshkeys: [ssh_key]
+	)!
+	panic(result)
+
+	assert result.name.starts_with('test_vm')
+	assert result.yggdrasil_ip.len > 0
+	assert result.mycelium_ip.len > 0
+}
