75e951de0d6f48333edb7163ceefa0bb71f1a314
root <kristof@incubaid.com>
2024-08-29 10:02:42 +0200

    fix

--- scripts/lib/8_osupdate.sh
+++ scripts/lib/8_osupdate.sh
@@ -2,7 +2,7 @@
 function os_update {
     echo ' - os update'
     if [[ "${OSNAME}" == "ubuntu" ]]; then
-        if [ -n "$GITHUB_ACTIONS" ]; then
+        if is_github_actions; then
             echo "github actions"
         else
             rm -f /var/lib/apt/lists/lock
@@ -13,7 +13,7 @@ function os_update {
         export DEBIAN_FRONTEND=noninteractive
         dpkg --configure -a
         apt update -y
-        if [ -n "$GITHUB_ACTIONS" ]; then
+        if is_github_actions; then
             echo "** IN GITHUB ACTIONS, DON'T DO UPDATE"
         else
             set +e
