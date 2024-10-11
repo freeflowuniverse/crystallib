c6bfe7d4a4da2eaa9e3b5a4b89c215dee9e67f5b
timurgordon <timurgordon@gmail.com>
2024-10-11 14:41:56 +0200

    components wip

--- crystallib/webserver/components/components.v
+++ crystallib/webserver/components/components.v
@@ -19,6 +19,8 @@ pub mut:
     items []NavItem
 }
 
+pub struct Sidebar {}
+
 pub struct MarkdownContent {
 pub mut:
     nav string
@@ -26,12 +28,40 @@ pub mut:
     title string
 }
 
+pub interface IComponent {
+    html() string
+}
+
 pub struct View {
 pub mut:
     navbar Navbar
+    sidebar Sidebar
+    main IComponent
     content elements.Doc
     markdown MarkdownContent
     title string
+}
+
+// pub struct Navbar {
+//     // brand: NavItem{href: '#', text: 'MyWebsite', class_name: 'brand'},
+//     // items: [
+//     //     NavItem{href: '#home', text: 'Home'},
+//     //     NavItem{href: '#about', text: 'About'},
+//     //     NavItem{href: '#services', text: 'Services'},
+//     //     NavItem{href: '#contact', text: 'Contact'}
+//     // ]
+// }
+
+pub fn (navbar Navbar) html() string {
+    return $tmpl('./templates/navbar.html')
+}
+
+pub fn (navbar Sidebar) html() string {
+    return $tmpl('./templates/sidebar.html')
+}
+
+pub fn (view View) html2() string {
+    return $tmpl('./templates/view2.html')
 }
 
 pub fn (view View) html() string {
@@ -39,7 +69,6 @@ pub fn (view View) html() string {
 }
 
 fn model_web_example() View {
-    
     example_main:=$tmpl("templates/example_main.md")
     example_nav:=$tmpl("templates/example_nav.md")
 
--- crystallib/webserver/components/table.v
+++ crystallib/webserver/components/table.v
@@ -12,7 +12,14 @@ pub:
 
 pub struct Table {
 pub:
+	headers []string
 	rows []Row
+}
+
+pub fn (table Table) html() string {
+	headers := table.headers.map('<th>${it}</th>').join('')
+	rows := table.rows.map(it.html()).join('\n')
+	return '<table id="table">\n\t<thead>\n\t\t<tr>${headers}</tr>\n\t</thead>\n\t<tbody>\n${rows}\n\t</tbody>\n</table>'
 }
 
 pub fn (component Row) html() string {
